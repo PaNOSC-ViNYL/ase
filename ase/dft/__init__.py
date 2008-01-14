7214bc66ada3d573ab82d3e63a4024f0b0244878
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2008-01-14 21:05:26 +0000

    Improved error message

--- ase/dft/__init__.py
+++ ase/dft/__init__.py
@@ -4,5 +4,7 @@ from ase.dft.dos import DOS
 
 
 def monkhorst_pack(size):
+    if npy.less_equal(size, 0).any():
+        raise ValueError('Illegal size: %s' % list(size))
     kpts = npy.indices(size).transpose((1, 2, 3, 0)).reshape((-1, 3))
     return (kpts + 0.5) / size - 0.5
