6288477fb2755c3f233edcf7d977b13920c2f302
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2008-01-31 08:32:16 +0000

    Don't do an energy/force calculation

--- ase/gui/__init__.py
+++ ase/gui/__init__.py
@@ -11,5 +11,8 @@ def gui(atoms):
     if not isinstance(atoms, list):
         atoms = [atoms]
     filename = tempfile.mktemp('.traj', 'ag-')
+    calc = atoms.get_calculator()
+    atoms.set_calculator(None)
     write_trajectory(filename, atoms)
+    atoms.set_calculator(calc)
     os.system('(ag %s &); (sleep 15; rm %s) &' % (filename, filename))
