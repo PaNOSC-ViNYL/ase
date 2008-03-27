c71c4c6480b6009ec28d999019a423e21b93dabf
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2008-03-27 14:34:19 +0000

    Neighborlist class added.  Useful for Python implementation of EMT potential and for GPAW's LCAO calculator.

--- ase/calculators/neighborlist.py
+++ ase/calculators/neighborlist.py
@@ -1,23 +1,34 @@
+from math import sqrt
+
+import numpy as npy
+
+
 class NeighborList:
     def __init__(self, cutoffs, skin=0.3):
-        self.cutoffs = dict([(Z, rc + skin) for Z, rc in cutoffs.items()])
+        self.cutoffs = cutoffs + skin
         self.skin = skin
         self.nupdates = 0
 
     def update(self, atoms):
-        if (self.positions is None or
-            ((atoms.positions - self.positions)**2).max() > self.skin**2):
+        if self.nupdates == 0:
             self.build(atoms)
+            return True
+        
+        assert (self.cell == atoms.get_pbc()).all()
 
+        if ((atoms.positions - self.positions)**2).sum(1).max() > self.skin**2:
+            self.build(atoms)
+            return True
+
+        return False
+    
     def build(self, atoms):
         self.positions = atoms.get_positions()
         pbc = atoms.get_pbc()
-        cell = atoms.get_cell()
-        rc2 = npy.array([self.cutoffs[Z]
-                         for Z in atoms.get_atomic_numbers()])**2
-        rcmax = max(self.cutoffs.values())
+        self.cell = atoms.get_cell()
+        rcmax = self.cutoffs.max()
         
-        icell = npy.linalg.inv(cell)
+        icell = npy.linalg.inv(self.cell)
         scaled = npy.dot(self.positions, icell)
         scaled0 = scaled.copy()
 
@@ -27,28 +38,36 @@ class NeighborList:
                 scaled0[:, i] %= 1.0
                 v = icell[:, i]
                 h = 1 / sqrt(npy.dot(v, v))
-                N.append(int(rcmax / h) + 1)
+                n =  int(2 * rcmax / h) + 1
             else:
-                N.append(0)
-
+                n = 0
+            N.append(n)
+            
         offsets = npy.empty((len(atoms), 3), int)
-        (scaled - scaled0).round(out=offsets)
-        positions0 = npy.dot(scaled0, cell)
+        (scaled0 - scaled).round(out=offsets)
+        positions0 = npy.dot(scaled0, self.cell)
+        natoms = len(atoms)
         indices = npy.arange(natoms)
 
         self.neighbors = [npy.empty(0, int) for a in range(natoms)]
         self.displacements = [npy.empty((0, 3), int) for a in range(natoms)]
-        for n1 in range(-N[0], N[0] + 1):
+        for n1 in range(0, N[0] + 1):
             for n2 in range(-N[1], N[1] + 1):
                 for n3 in range(-N[2], N[2] + 1):
-                    displacement = npy.dot((n1, n2, n3), cell)
+                    if n1 == 0 and (n2 < 0 or n2 == 0 and n3 < 0):
+                        continue
+                    displacement = npy.dot((n1, n2, n3), self.cell)
                     for a in range(natoms):
                         d = positions0 + displacement - positions0[a]
-                        i = indices[(d**2).sum(1) < rc2]
-                        self.neighbors[i] = npy.concatenate(
+                        i = indices[(d**2).sum(1) <
+                                    (self.cutoffs + self.cutoffs[a])**2]
+                        if n1 == 0 and n2 == 0 and n3 == 0:
+                            i = i[i >= a]
+                        self.neighbors[a] = npy.concatenate(
                             (self.neighbors[a], i))
                         disp = npy.empty((len(i), 3), int)
                         disp[:] = (n1, n2, n3)
+                        disp += offsets[i] - offsets[a]
                         self.displacements[a] = npy.concatenate(
                             (self.displacements[a], disp))
 
---
+++ ase/test/neighbor.py
@@ -0,0 +1,41 @@
+from ase import *
+from ase.calculators.neighborlist import NeighborList
+
+atoms = Atoms(numbers=range(10),
+              cell=[(0.2, 1.2, 1.4),
+                    (1.4, 0.1, 1.6),
+                    (1.3, 2.0, -0.1)])
+atoms.set_scaled_positions(3 * random.random((10, 3)) - 1)
+
+def count(nl, atoms):
+    c = zeros(len(atoms), int)
+    R = atoms.get_positions()
+    cell = atoms.get_cell()
+    d = 0.0
+    for a in range(len(atoms)):
+        i, offsets = nl.get_neighbors(a)
+        for j in i:
+            c[j] += 1
+        c[a] += len(i)
+        d += (((R[i] + dot(offsets, cell) - R[a])**2).sum(1)**0.5).sum()
+    return d, c
+
+for p1 in range(2):
+    for p2 in range(2):
+        for p3 in range(2):
+            print p1, p2, p3
+            atoms.set_pbc((p1, p2, p3))
+            nl = NeighborList(atoms.numbers * 0.2 + 0.5, 0.0)
+            nl.update(atoms)
+            d, c = count(nl, atoms)
+            i, o = nl.get_neighbors(0)
+            atoms2 = atoms.repeat((p1 + 1, p2 + 1, p3 + 1))
+            nl2 = NeighborList(atoms2.numbers * 0.2 + 0.5, 0.0)
+            nl2.update(atoms2)
+            d2, c2 = count(nl2, atoms2)
+            c2.shape = (-1, 10)
+            dd = d * (p1 + 1) * (p2 + 1) * (p3 + 1) - d2
+            print dd
+            print c2 - c
+            assert abs(dd) < 1e-11
+            assert not (c2 - c).any()
