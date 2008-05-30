1a07a24d8f5cb29de9d4fd62c6a9bbda5fdd8887
carstenr <carstenr@5af997e4-4c3d-0410-961e-ac0512e437d9>
2008-05-30 08:46:38 +0000

    Update tutorial

--- doc/tutorials/saving_graphics.py
+++ doc/tutorials/saving_graphics.py
@@ -4,7 +4,7 @@ atoms = Atoms('Ag', cell=(2.7, 2.7, 2.7), pbc=True) * (18, 8, 8)
 
 # view with ag
 #view(atoms)
-rotation = '-70x, -20y, -2z' # found using 'view -> rotate'
+rotation = '-70x, -20y, -2z' # found using ag menu 'view -> rotate'
 
 #Make colors
 from ase.utils import hsv
@@ -13,18 +13,17 @@ colors = hsv(atoms.positions[:, 0])
 # Textures
 tex = ['jmol',] * 432 + ['vmd',] * 432+ ['ase3',] * 288
 
-# Make graphics files
-write('flat.png', atoms, rotation=rotation, colors=colors, show_unit_cell=2,
-      radii=None)
-write('nice.pov', atoms, rotation=rotation, colors=colors, show_unit_cell=2,
-      radii=None, textures=tex, display=False)
 
-import os; os.system('povray nice.ini')
-
-extra_keywords = { # For povray files only
-'showunitcell' : 2,
-'display'      : True,  # Display while rendering
-'pause'        : True,  # Pause when done rendering (only if display)
+# keywords
+kwargs = { # Keywords that exist for eps, png, and pov
+'rotation': rotation,
+'show_unit_cell': 2,
+'colors': colors,
+'radii': None,
+}
+extra_kwargs = { # For povray files only
+'display'      : False, # Display while rendering
+'pause'        : False, # Pause when done rendering (only if display)
 'transparent'  : True,  # Transparent background
 'canvas_width' : None,  # Width of canvas in pixels
 'canvas_height': None,  # Height of canvas in pixels 
@@ -35,5 +34,13 @@ extra_keywords = { # For povray files only
                   'White',       # color
                   .7, .7, 3, 3], # width, height, Nlamps_x, Nlamps_y
 'background'   : 'White',        # color
-'textures'     : None, # Length of atoms list of texture names
+'textures'     : tex, # Length of atoms list of texture names
 }
+
+# Make graphics files
+write('flat.png', atoms, **kwargs)
+kwargs.update(extra_kwargs)
+write('nice.pov', atoms, **kwargs)
+
+import os; os.system('povray nice.ini')
+
