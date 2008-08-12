af26581cc43ae37ad5930a018f7fcff3258980f9
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2008-08-12 07:20:38 +0000

    Move to correct directory

--- doc/images.py
+++ doc/images.py
@@ -1,4 +1,5 @@
 # creates: ag.png water_divide_surf.png
 from urllib import urlretrieve
-for name in ['ag.png', 'water_divide_surf.png']:
-    urlretrieve('http://wiki.fysik.dtu.dk/ase-files/' + name, name)
+urlretrieve('http://wiki.fysik.dtu.dk/ase-files/ag.png', 'ase/ag.png')
+urlretrieve('http://wiki.fysik.dtu.dk/ase-files/water_divide_surf.png',
+            'ase/dft/water_divide_surf.png')
