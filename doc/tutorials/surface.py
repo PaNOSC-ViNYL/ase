8bd0c3317b289752318b2183b64f02394c39e19b
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2008-08-12 16:39:28 +0000

    Using povpng function

--- doc/tutorials/surface.py
+++ doc/tutorials/surface.py
@@ -2,7 +2,7 @@
 # creates:  surface.png
 import os
 from ase import *
+from ase.io.pov import povpng
 execfile('N2Cu.py')
 image = read('N2Cu.traj@-1')
-write('surface.pov', image, pause=False)
-os.system('povray surface.ini')
+povpng('surface.pov', image)
