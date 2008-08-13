5cec4f53f338b427d699111949df3e6f793437e8
carstenr <carstenr@5af997e4-4c3d-0410-961e-ac0512e437d9>
2008-08-13 12:38:43 +0000

    Units explained in doc

--- ase/data/molecules.py
+++ ase/data/molecules.py
@@ -2224,6 +2224,12 @@ def get_atomization_energy(name):
     The atomization energy is extrapolated from experimental heats of
     formation at room temperature, using calculated zero-point energies
     and thermal corrections.
+
+    The atomization energy is returned in kcal/mol = 43.36 meV:
+
+    >>> from ase import *; print kcal / mol
+    0.0433641146392
+
     """
     assert name in extra or name in g2
     d = data[name]
