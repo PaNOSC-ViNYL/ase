a605b862d54bc462e66e7bad57a3e7390e64562c
carstenr <carstenr@5af997e4-4c3d-0410-961e-ac0512e437d9>
2008-09-15 11:56:22 +0000

    Merged TransportCalculator and Transmission classes

--- ase/transport/calculators.py
+++ ase/transport/calculators.py
@@ -1,8 +1,10 @@
-from ase.transport.transmission import Transmission
+import numpy as npy
+
+from numpy import linalg
 from ase.transport.selfenergy import LeadSelfEnergy
 from ase.transport.greenfunction import GreenFunction
 from ase.transport.tools import subdiagonalize, cutcoupling, tri2full, dagger
-import numpy as npy
+
 
 class TransportCalculator:
     """Determine transport properties of device sandwiched between
@@ -10,30 +12,33 @@ class TransportCalculator:
     """
 
     def __init__(self, **kwargs):
-        """args=(energies, h, h1, h2, s=None, s1=None, s2=None, align_bf=None)
+        """Bla Bla XXX
         
-        energies is the energy grid on which the transport properties should
-        be determined.
+        energies is the energy grid on which the transport properties
+        should be determined.
         
         h1 (h2) is a matrix representation of the Hamiltonian of two
-        principal layers of the left (right) lead, and the coupling between
-        such layers.
+        principal layers of the left (right) lead, and the coupling
+        between such layers.
         
-        h is a matrix representation of the Hamiltonian of the scattering
-        region. This must include at least on lead principal layer on each
-        side. The coupling in (out) of the scattering region is by 
-        default assumed to be identical to the coupling between left (right) 
-        principal layers. However, these couplings can also be
-        specified explicitly through hc1 and hc2. 
+        h is a matrix representation of the Hamiltonian of the
+        scattering region. This must include at least on lead
+        principal layer on each side. The coupling in (out) of the
+        scattering region is by default assumed to be identical to the
+        coupling between left (right) principal layers.  However,
+        these couplings can also be specified explicitly through hc1
+        and hc2.
         
-        s, s1, and s2 are the overlap matrices corresponding to h, h1, and
-        h2. Default is the identity operator. sc1 and sc2 are the overlap
-        matrices corresponding to the optional couplings hc1 and hc2.
+        s, s1, and s2 are the overlap matrices corresponding to h, h1,
+        and h2. Default is the identity operator. sc1 and sc2 are the
+        overlap matrices corresponding to the optional couplings hc1
+        and hc2.
         
         align_bf specifies the principal layer basis index used to
         align the fermi levels of the lead and scattering regions.
         """
         
+        # The default values for all extra keywords
         self.input_parameters = {'energies': None,
                                  'h': None,
                                  'h1': None,
@@ -41,42 +46,56 @@ class TransportCalculator:
                                  's': None,
                                  's1': None,
                                  's2': None,
-                                 'hc1' : None,
-                                 'hc2' : None,
-                                 'sc1' : None,
-                                 'sc2' :None,
+                                 'hc1': None,
+                                 'hc2': None,
+                                 'sc1': None,
+                                 'sc2': None,
                                  'align_bf': None,
                                  'eta1': 1e-3,
                                  'eta2': 1e-3,
                                  'eta': 1e-3,
-                                 'logfile': None,
-                                 'verbose': False}
-
-        self.trans = Transmission()
-        self.initialized =  False
+                                 'logfile': None, # '-',
+                                 'eigenchannels': 0,
+                                 'dos': False,
+                                 'pdos': [],
+                                 }
+        self.initialized = False # Changed Hamiltonians?
+        self.uptodate = False # Changed energy grid?
         self.set(**kwargs)
 
     def set(self, **kwargs):
-        p = self.input_parameters
-        p.update(kwargs)
+        for key in kwargs:
+            if key in ['h', 'h1', 'h2', 'hc1', 'hc2',
+                       's', 's1', 's2', 'sc1', 'sc2',
+                       'eta', 'eta1', 'eta2', 'align_bf']:
+                self.initialized = False
+                self.uptodate = False
+                break
+            if key in ['energies', 'eigenchannels', 'dos', 'pdos']:
+                self.uptodate = False
 
-        self.pl1 = len(p['h1']) / 2
-        self.pl2 = len(p['h2']) / 2
-        assert p['h'] is not None
-        
-        if 'pdos' in p:
-            self.trans.set(pdos=p['pdos'])
-        
-        self.initialize()
+        self.input_parameters.update(kwargs)
+        log = self.input_parameters['logfile']
+        if log is None:
+            class Trash:
+                def write(self, s):
+                    pass
+                def flush(self):
+                    pass
+            self.log = Trash()
+        elif log == '-':
+            from sys import stdout
+            self.log = stdout
+        elif 'logfile' in kwargs:
+            self.log = open(log, 'w')
 
     def initialize(self):
+        if self.initialized:
+            return
+
+        print >> self.log, '# Initializing calculator...'
+
         p = self.input_parameters
-        self.verbose = p['verbose']
-        if self.verbose:
-            print "initializing calculator..."
-        self.energies = p['energies']
-        pl1 = self.pl1
-        pl2 = self.pl2
         if p['s1'] == None:
             p['s1'] = npy.identity(len(p['h1']))
         if p['s2'] == None:
@@ -84,27 +103,18 @@ class TransportCalculator:
         if p['s'] == None:
             p['s'] = npy.identity(len(p['h']))
             
-        h1_ii = p['h1'][:pl1, :pl1]
-        h1_ij = p['h1'][:pl1, pl1:2*pl1]
-        s1_ii = p['s1'][:pl1, :pl1]
-        s1_ij = p['s1'][:pl1, pl1:2*pl1]
-
-        h2_ii = p['h2'][:pl2,:pl2]
-        h2_ij = p['h2'][pl2:2*pl2,:pl2]
-        s2_ii = p['s2'][:pl2,:pl2]
-        s2_ij = p['s2'][pl2:2*pl2,:pl2]
-
-        #h1_im = p['h'][:pl1,pl1:-pl2]
-        #s1_im = p['s'][:pl1,pl1:-pl2]
-
-        #h2_im = p['h'][-pl1:,pl1:-pl2]
-        #s2_im = p['s'][-pl1:,pl1:-pl2]
-
-        #h_mm = p['h'][pl1:-pl2,pl1:-pl2]
-        #s_mm = p['s'][pl1:-pl2,pl1:-pl2]
-        
         h_mm = p['h']
         s_mm = p['s']
+        pl1 = len(p['h1']) / 2
+        pl2 = len(p['h2']) / 2
+        h1_ii = p['h1'][:pl1, :pl1]
+        h1_ij = p['h1'][:pl1, pl1:2 * pl1]
+        s1_ii = p['s1'][:pl1, :pl1]
+        s1_ij = p['s1'][:pl1, pl1:2 * pl1]
+        h2_ii = p['h2'][:pl2, :pl2]
+        h2_ij = p['h2'][pl2: 2 * pl2, :pl2]
+        s2_ii = p['s2'][:pl2, :pl2]
+        s2_ij = p['s2'][pl2: 2 * pl2, :pl2]
         
         if p['hc1'] is None:
             nbf = len(h_mm)
@@ -135,79 +145,109 @@ class TransportCalculator:
         if align_bf != None:
             diff = (h_mm[align_bf, align_bf] - h1_ii[align_bf, align_bf]) \
                    / s_mm[align_bf, align_bf]
-            if self.verbose:
-                print "Alligning scattering H to left lead H. diff=", diff
+            print >> self.log, '# Aligning scat. H to left lead H. diff=', diff
             h_mm -= diff * s_mm
 
-        self.h_pp = p['h']
-        self.s_pp = p['s']
         #setup lead self-energies
-        sigma1 = LeadSelfEnergy((h1_ii, s1_ii), 
-                                (h1_ij, s1_ij),
-                                (h1_im, s1_im),
-                                p['eta1'])
-        
-        sigma2 = LeadSelfEnergy((h2_ii, s2_ii), 
-                                (h2_ij, s2_ij),
-                                (h2_im, s2_im),
-                                p['eta2'])
+        self.selfenergies = [LeadSelfEnergy((h1_ii, s1_ii), 
+                                            (h1_ij, s1_ij),
+                                            (h1_im, s1_im),
+                                            p['eta1']),
+                             LeadSelfEnergy((h2_ii, s2_ii), 
+                                            (h2_ij, s2_ij),
+                                            (h2_im, s2_im),
+                                            p['eta2'])]
 
-        self.selfenergies = [sigma1, sigma2]
         #setup scattering green function
-        self.gf = GreenFunction(selfenergies=self.selfenergies,
-                                H=h_mm,
-                                S=s_mm,
-                                eta = p['eta'])
+        self.greenfunction = GreenFunction(selfenergies=self.selfenergies,
+                                           H=h_mm,
+                                           S=s_mm,
+                                           eta=p['eta'])
 
-        #setup the basic transmission calculator 
-        self.trans.set(energies=self.energies,
-                       greenfunction=self.gf,
-                       selfenergies=self.selfenergies,
-                       logfile=p['logfile'],
-                       verbose=p['verbose'])
-        
         self.initialized = True
+    
+    def update(self):
+        if self.uptodate:
+            return
+        
+        p = self.input_parameters
+        self.energies = p['energies']
+        nepts = len(self.energies)
+        nchan = p['eigenchannels']
+        pdos = p['pdos']
+        self.T_e = npy.empty(nepts)
+        if p['dos']:
+            self.dos_e = npy.empty(nepts)
+        if pdos != []:
+            self.pdos_ne = npy.empty((len(pdos), nepts))
+        if nchan > 0:
+            self.eigenchannels_ne = npy.empty((nchan, nepts))
+
+        for e, energy in enumerate(self.energies):
+            Ginv_mm = self.greenfunction(energy, inverse=True)
+            lambda1_mm = self.selfenergies[0].get_lambda(energy)
+            lambda2_mm = self.selfenergies[1].get_lambda(energy)
+            a_mm = linalg.solve(Ginv_mm, lambda1_mm)
+            b_mm = linalg.solve(dagger(Ginv_mm), lambda2_mm)
+            T_mm = npy.dot(a_mm, b_mm)
+            if nchan > 0:
+                t_n = linalg.eigvals(T_mm).real
+                self.eigenchannels_ne[:, e] = npy.sort(t_n)[-nchan:]
+                self.T_e[e] = npy.sum(t_n)
+            else:
+                self.T_e[e] = npy.trace(T_mm).real
+
+            print >> self.log, e, self.T_e[e]
+            self.log.flush()
+
+            if p['dos']:
+                self.dos_e[e] = self.greenfunction.dos(energy)
+
+            if pdos != []:
+                self.pdos_ne[:, e] = npy.take(self.greenfunction.pdos(energy),
+                                              pdos)
+        
+        self.uptodate = True
 
     def print_pl_convergence(self):
-        p = self.input_parameters
-        pl1 = self.pl1
+        self.initialize()
+        pl1 = len(self.input_parameters['h1']) / 2
         
         h_ii = self.selfenergies[0].h_ii
         s_ii = self.selfenergies[0].s_ii
-        ha_ii = self.gf.H[:pl1, :pl1]
-        sa_ii = self.gf.S[:pl1, :pl1]
+        ha_ii = self.greenfunction.H[:pl1, :pl1]
+        sa_ii = self.greenfunction.S[:pl1, :pl1]
         c1 = npy.abs(h_ii - ha_ii).max()
         c2 = npy.abs(s_ii - sa_ii).max()
-        print "Conv (h,s)=%.2e, %2.e" % (c1, c2)
+        print 'Conv (h,s)=%.2e, %2.e' % (c1, c2)
 
     def get_transmission(self):
-        if not self.trans.uptodate:
-            self.trans.update()
-        return self.trans.T_e
+        self.initialize()
+        self.update()
+        return self.T_e
 
     def get_dos(self):
-        if not self.trans.uptodate:
-            self.set(dos=True)
-            self.trans.update()
-        return self.trans.dos_e
+        self.initialize()
+        self.update()
+        return self.dos_e
 
-    def get_eigenchannels(self,n=0):
-        if n > self.trans.input_parameters['eigenchannels']:
-            self.trans.set(eigenchannels=n)
-            self.trans.initialize()
-            self.trans.update()
-        elif not self.trans.uptodate:
-            self.trans.update()
-        if n==0:
-            n = self.trans.input_parameters['eigenchannels']
-        return self.trans.eigenchannels_ne[:n]
+    def get_eigenchannels(self, n=None):
+        """Get ``n`` first eigenchannels."""
+        self.initialize()
+        self.update()
+        if n > self.input_parameters['eigenchannels']:
+            self.set(eigenchannels=n)
+        if n is None:
+            n = self.input_parameters['eigenchannels']
+        return self.eigenchannels_ne[:n]
 
     def get_pdos(self):
-        if not self.trans.uptodate:
-            self.trans.update()
-        return self.trans.pdos_ne
+        self.initialize()
+        self.update()
+        return self.pdos_ne
 
     def subdiagonalize_bfs(self, bfs):
+        self.initialize()
         bfs = npy.array(bfs)
         p = self.input_parameters
         h_pp = p['h']
@@ -218,6 +258,7 @@ class TransportCalculator:
         return ht_pp, st_pp, e_p, c_pp
 
     def cutcoupling_bfs(self, bfs):
+        self.initialize()
         bfs = npy.array(bfs)
         p = self.input_parameters
         h_pp = p['h'].copy()
@@ -225,5 +266,34 @@ class TransportCalculator:
         cutcoupling(h_pp, s_pp, bfs)
         return h_pp, s_pp
         
-    def get_left_channels(self, energy, n):
-        return self.trans.get_left_channels(energy, n)
+    def get_left_channels(self, energy, nchan=1):
+        self.initialize()
+        g_s_ii = self.greenfunction(energy)
+        lambda_l_ii = self.selfenergies[0].get_lambda(energy)
+        lambda_r_ii = self.selfenergies[1].get_lambda(energy)
+
+        if self.greenfunction.S is None:
+            s_s_qsrt_ii = s_s_isqrt = npy.identity(len(g_s_ii))
+        else:
+            s_mm = self.greenfunction.S
+            s_s_i, s_s_ii = linalg.eig(s_mm)
+            s_s_i = npy.abs(s_s_i)
+            s_s_sqrt_i = npy.sqrt(s_s_i) # sqrt of eigenvalues  
+            s_s_sqrt_ii = npy.dot(s_s_ii * s_s_sqrt_i, dagger(s_s_ii))
+            s_s_isqrt_ii = npy.dot(s_s_ii / s_s_sqrt_i, dagger(s_s_ii))
+
+        lambdab_r_ii = npy.dot(npy.dot(s_s_isqrt_ii, lambda_r_ii),s_s_isqrt_ii)
+        a_l_ii = npy.dot(npy.dot(g_s_ii, lambda_l_ii), dagger(g_s_ii))
+        ab_l_ii = npy.dot(npy.dot(s_s_sqrt_ii, a_l_ii), s_s_sqrt_ii)
+        lambda_i, u_ii = linalg.eig(ab_l_ii)
+        ut_ii = npy.sqrt(lambda_i / (2.0 * npy.pi)) * u_ii
+        m_ii = 2 * npy.pi * npy.dot(npy.dot(dagger(ut_ii), lambdab_r_ii),ut_ii)
+        T_i,c_in = linalg.eig(m_ii)
+        T_i = npy.abs(T_i)
+        
+        channels = npy.argsort(-T_i)[:nchan]
+        c_in = npy.take(c_in, channels, axis=1)
+        T_n = npy.take(T_i, channels)
+        v_in = npy.dot(npy.dot(s_s_isqrt_ii, ut_ii), c_in)
+
+        return T_n, v_in
