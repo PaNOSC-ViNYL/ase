a97b0e3528c06314621eac0cd2574ae00664e668
bjork <bjork@5af997e4-4c3d-0410-961e-ac0512e437d9>
2008-10-19 13:36:44 +0000

    Update of io/vasp.py made by Janne Blomqvist.

--- ase/io/vasp.py
+++ ase/io/vasp.py
@@ -4,27 +4,9 @@ to handle VASP simulations. It will only work with ASE-3.0 and later
 versions.
 """
 
-from string import atoi,atof,split
 import sys
 import os
 import re
-
-def nlist(x):
-    return range(len(x))
-
-# Define 'all' and 'any' methods to make the code compatible
-# with more versions of Python.
-def all(iterable):
-    for element in iterable:
-        if not element:
-            return False
-    return True
-
-def any(iterable):
-    for element in iterable:
-        if element:
-            return True
-    return False
 
 def read_vasp(filename='CONTCAR'):
     """Import POSCAR/CONTCAR type file.
@@ -38,260 +20,199 @@ def read_vasp(filename='CONTCAR'):
     from ase import Atoms, Atom
     from ase.constraints import FixAtoms, fix_scaled
     from ase.data import chemical_symbols
+    import numpy as np
 
-    dir, filename = os.path.split(filename)
+    if type(filename) == str:
+        f = open(filename)
+    elif type(filename) == file:
+        f = filename
+    else:
+        raise TypeError("filename argument must be a string or a file object.")
 
-    # Read from file
-    file_in = ReadPOSCAR(filename, dir)
-    unitcell = file_in.unit_cell()
-    poscon = file_in.positions_constraints()
-    cart=file_in.cartesian()
-    # Read atomic species
+    # First line should contain the atom symbols , eg. "Ag Ge" in
+    # the same order
+    # as later in the file (and POTCAR for the full vasp run)
+    atomtypes = f.readline().split()
     try:
-        # Try to read chemical symbols from label in POSCAR/CONTCAR.
-        atomtypes = file_in.atom_types()
+        for atype in atomtypes:
+           if not atype in chemical_symbols:
+              raise KeyError
     except KeyError:
-        # If this failes, first try to find OUTCAR, then POTCAR.
         try:
             file_outcar = ReadOUTCAR(dir)
-            atomtypes=file_outcar.atom_types()
+            atomtypes = file_outcar.atom_types()
         except IOError:
             file_potcar = ReadPOTCAR(dir)
-            atomtypes=file_potcar.atom_types()
-    # Set atomic positions
-    atoms=Atoms()
+            atomtypes = file_potcar.atom_types()
 
-    for ikind in nlist(poscon[0]):
-        for nb in nlist(poscon[0][ikind][0:])[::3]:
-            atoms.append(Atom(atomtypes[ikind], poscon[0][ikind][nb:nb+3]))
-    # Set unitcell
-    atoms.set_cell(unitcell, not(cart))
-    # Set constraints
-    constraints=[]
-    indices=[]
-    i=0
-    for ikind in nlist(poscon[0]):
-        for nb in nlist(poscon[0][ikind][0:])[::3]:
-            if poscon[1][ikind][0:]:
-                tmp = poscon[1][ikind][nb:nb+3]
-                if any(tmp) and not all(tmp):
-                    constraints.append(fix_scaled(atoms.get_cell(),i,tmp))
-                elif all(tmp):
-                    indices.append(i)
-                i=i+1
-    if indices:
-        constraints.append(FixAtoms(indices=indices))
-    if constraints:
-        atoms.set_constraint(constraints)
+    lattice_constant = float(f.readline())
+
+    # Now the lattice vectors
+    a = []
+    for ii in range(3):
+        s = f.readline().split()
+        floatvect = float(s[0]), float(s[1]), float(s[2])
+        a.append(floatvect)
+
+    basis_vectors = np.array(a) * lattice_constant
+
+    # Number of atoms. Again this must be in the same order as
+    # in the first line
+    # or in the POTCAR or OUTCAR file
+    atom_symbols = []
+    numofatoms = f.readline().split()
+    for i, num in enumerate(numofatoms):
+        numofatoms[i] = int(num)
+        if (len(atomtypes) < i + 1):
+            atomtypes.append("Unknown")
+        [atom_symbols.append(atomtypes[i]) for na in xrange(numofatoms[i])]
+
+    # Check if Selective dynamics is switched on
+    sdyn = f.readline()
+    selective_dynamics = sdyn[0].lower() == "s"
+
+    # Check if atom coordinates are cartesian or direct
+    if selective_dynamics:
+        ac_type = f.readline()
+    else:
+        ac_type = sdyn
+    cartesian = ac_type[0].lower() == "c" or ac_type[0].lower() == "k"
+    tot_natoms = sum(numofatoms)
+    atoms_pos = np.empty((tot_natoms, 3))
+    if selective_dynamics:
+        selective_flags = np.empty((tot_natoms, 3), dtype=bool)
+    for atom in xrange(tot_natoms):
+        ac = f.readline().split()
+        atoms_pos[atom] = (float(ac[0]), float(ac[1]), float(ac[2]))
+        if selective_dynamics:
+            curflag = []
+            for flag in ac[3:6]:
+                curflag.append(flag == 'F')
+            selective_flags[atom] = curflag
+    # Done with all reading
+    if type(filename) == str:
+        f.close()
+    if cartesian:
+        atoms_pos *= lattice_constant
+    atoms = Atoms(symbols = atom_symbols, cell = basis_vectors, pbc = True)
+    if cartesian:
+        atoms.set_positions(atoms_pos)
+    else:
+        atoms.set_scaled_positions(atoms_pos)
+    if selective_dynamics:
+        constraints = []
+        indices = []
+        for ind, sflags in enumerate(selective_flags):
+            if sflags.any() and not sflags.all():
+                constraints.append(fix_scaled(atoms.get_cell(), ind, sflags))
+            elif sflags.all():
+                indices.append(ind)
+        if indices:
+            constraints.append(FixAtoms(indices))
+        if constraints:
+            atoms.set_constraint(constraints)
     return atoms
 
-def write_vasp(filename, atoms, label='', direct=None, dir='', sort=None):
-        """Method to write VASP position (POSCAR/CONTCAR) files.
+def write_vasp(filename, atoms, label='', direct=False, sort=None):
+    """Method to write VASP position (POSCAR/CONTCAR) files.
 
-        Writes label, scalefactor, unitcell, # of various kinds of atoms,
-        positions in cartesian or scaled coordinates (Direct), and constraints
-        to file. Cartesian coordiantes is default and default label is the 
-        atomic species, e.g. 'C N H Cu'.
-        """
-        
-        import numpy as np
-
-        dir, filename = os.path.split(filename)
-        
-        # Read number of atoms of various kinds
-        symbols = atoms.get_atomic_numbers()
-        if sort:
-            symbols.sort()
-        atom_num=[[symbols[0],1]]
-        for m in range(1,len(symbols)):
-            if symbols[m]==symbols[m-1]:
-                atom_num[-1][1]+=1
-            else:
-                atom_num.append([symbols[m], 1])
-        atomlist=[]
-        atomsort=[]
-        for n in nlist(atom_num):
-            atomlist.append(atom_num[n][1])
-            atomsort.append(atom_num[n][0])
-      
-        # Create the label
-        if label == '':
-            label = element_string(atomsort)
-
-        # Write unitcell in real coordinates and adapt to VASP convention 
-        # for unit cell
-        ucell=atoms.get_cell()
-        
-        # Write atom positions in scaled or cartesian coordinates
-        if direct:
-            coord = atoms.get_scaled_positions()
-        else:
-            coord = atoms.get_positions()
-        con=[]
-        constr=atoms.constraints
-        if constr:
-            con=np.zeros([len(atoms),3])
-            for n_constr in nlist(constr):
-                b=str(constr[n_constr])
-                if b.startswith('FixScaled'):
-                    print 1
-                    con[constr[n_constr].a]=constr[n_constr].mask
-                elif b.startswith('FixAtoms'):
-                    con[constr[n_constr].index]=[1,1,1]
-        # Write to file
-        outfile = WritePOSCAR(filename, dir)
-        outfile.label(label)
-        outfile.unit_cell(ucell)
-        outfile.atom_types(atomlist)
-        outfile.coordinates_constraints(coord, con, direct)
-        outfile.close()
-
-class ReadPOSCAR:
-    """Class to read data from CONTCAR and POSCAR files. 
-
-    All read methods opens and closes POSCAR/CONTCAR. 
+    Writes label, scalefactor, unitcell, # of various kinds of atoms,
+    positions in cartesian or scaled coordinates (Direct), and constraints
+    to file. Cartesian coordiantes is default and default label is the 
+    atomic species, e.g. 'C N H Cu'.
     """
-
-    def __init__(self,filename='CONTCAR', dir='./'):
-
-        self._file_= os.path.join(dir,filename)
-        if not os.path.isfile(self._file_):
-            print ' The file ' +self._file_+ ' does not exist. Please specify a valid filename.'
-            sys.exit()
     
-    def atom_types(self):
-        from ase.data import chemical_symbols
-        file=open(self._file_,'r')
-        input = file.readline()
-        str = split(input)
-        atomtypes=[]
-        for n in range(len(str)):
-            atomtypes.append(str[n])
-        for i in nlist(atomtypes):
-           issymbol=False
-           for symbol in chemical_symbols:
-              if symbol==atomtypes[i]:
-                 issymbol=True
-           if not issymbol:
-              raise KeyError
-        return atomtypes
+    import numpy as np
+    from ase.constraints import FixAtoms, fix_scaled
 
-    def unit_cell(self):
-        """Method that returns unitcell as a list of list of numbers"""
-        file=open(self._file_,'r')
-        input=file.readlines()
-        file.close()
-        scale= atof(input[1])
-        unitcell=[]
-        for i in range(2,5):
-            unitcell.append([scale*float(x) for x in input[i].split()])
-        return unitcell
+    if type(filename) == str:
+        f = open(filename, 'w')
+    elif type(filename) == file:
+        f = file
+    else:
+        raise TypeError('filename argument must be either a string or a file \
+object.')
+    
+    # Write atom positions in scaled or cartesian coordinates
+    if direct:
+        coord = atoms.get_scaled_positions()
+    else:
+        coord = atoms.get_positions()
 
-    def cartesian(self):
-        """Method the check if the coordinates are given in direct or carteisna
-        coordinates
-        """
-        file=open(self._file_,'r')
-        for i in range(6):
-            skip=file.readline()
-        str=file.readline()
-        if str[0]=='S':
-            str=file.readline()
-        if str[0]=='D' or str[0]=='d':
-            cartesian=False
-        elif str[0]=='C' or str[0]=='c' or str[0]=='K' or str[0]=='k':
-            cartesian=True
-        return cartesian
-        
-    def positions_constraints(self):
-        """Method that returns a list of list of atom positions and list of
-           constraints. Atom positions is a list of the coordinates
-           and the constraints is a string.
-        """
+    if atoms.constraints:
+        sflags = np.zeros((len(atoms), 3), dtype=bool)
+        for constr in atoms.constraints:
+            if isinstance(constr, fix_scaled):
+                sflags[constr.a] = constr.mask
+            elif isinstance(constr, FixAtoms):
+                sflags[constr.index] = [True, True, True]
 
-        file=open(self._file_, 'r')
-        input=file.readlines()
-        str=split(input[5])
-        natoms=[]
-        for i in nlist(str):
-            natoms.append(atoi(str[i]))
-        if(input[6][0] == 'S'):
-            nline=8
+    if sort:
+        ind = np.argsort(atoms.get_chemical_symbols())
+        symbols = np.array(atoms.get_chemical_symbols())[ind]
+        coord = coord[ind]
+        if atoms.constraints:
+            sflags = sflags[ind]
+    else:
+        symbols = atoms.get_chemical_symbols()
+
+    # Create a list of (symbol, count) pairs
+    sc = []
+    psym = symbols[0]
+    count = 0
+    for sym in symbols:
+        if sym != psym:
+            sc.append((psym, count))
+            psym = sym
+            count = 1
         else:
-            nline=7
-        pos=[]
-        con=[]
-        for ikind in nlist(natoms):
-            posatom=[]
-            conatom=[]
-            for i in range(natoms[ikind]):
-                str=split(input[nline])
-                vec=[]
-                for j in range(3):
-                    posatom.append(atof(str[j]))
-                if len(str) > 3:
-                    for k in range(3):
-                        conatom.append(-int(str[3+k]=='T')+1)
-                nline+=1
-            pos.append(posatom)
-            con.append(conatom)
-        file.close()
-        return [pos,con]
+            count += 1
+    sc.append((psym, count))
 
-class WritePOSCAR:
-    """Class that generates POSCAR files.
+    # Create the label
+    if label == '':
+        for sym, c in sc:
+            label += '%3s' % sym
+    f.write(label + '\n')
 
-    Directory can be specified, default is current directory.
-    """
+    # Write unitcell in real coordinates and adapt to VASP convention 
+    # for unit cell
+    # ase Atoms doesn't store the lattice constant separately, so always
+    # write 1.0.
+    f.write('%19.16f\n' % 1.0)
+    for vec in atoms.get_cell():
+        f.write(' ')
+        for el in vec:
+            f.write('%22.16f' % el)
+        f.write('\n')
 
-    def __init__(self,filename='POSCAR', dir='./'):
-        self._file_ = open(os.path.join(dir,filename),'w')
+    # Numbers of each atom
+    for sym, count in sc:
+        f.write('%4i' % count)
+    f.write('\n')
 
-    def close(self):
-        """Method that closes POSCAR file"""
-        self._file_.close()        
+    if atoms.constraints:
+        f.write('Selective dynamics\n')
 
-    def label(self,label):
-        """Method that writes label on first line"""
-        self._file_.write(label+'\n')
+    if direct:
+        f.write('Direct\n')
+    else:
+        f.write('Cartesian\n')
 
-    def unit_cell(self,unitcell):
-        """Method that writes scalefactor and unitcell with
-          16 decimal places specifie in fix.
-        """
-        string = '    %.16f\n' % 1.0
-        self._file_.write(string)
-        for ivec in range(3):
-            self._file_.write('    %.16f %.16f %.16f\n' %  tuple(unitcell[ivec]))
+    for iatom, atom in enumerate(coord):
+        for dcoord in atom:
+            f.write('%20.16f' % dcoord)
+        if atoms.constraints:
+            for flag in sflags[iatom]:
+                if flag:
+                    s = 'F'
+                else:
+                    s = 'T'
+                f.write('%4s' % s)
+        f.write('\n')
 
-    def atom_types(self,atomlist):
-        """Method that writes number of various atomtypes
-           from 'atomlist'
-        """
-        str=''
-        for ikind in range(len(atomlist)):
-           str = str+'  %i' % atomlist[ikind]
-        self._file_.write(str+'\n')
-
-    def coordinates_constraints(self, coord, con, direct):
-        """Method that writes the atom coordinates from the list 'coord' 
-           with 16 decimal places and the constraints from the 
-           list 'con'.
-        """
-        if con != []:
-           self._file_.write('Selective dynamics\n')
-        if direct :
-            self._file_.write('Direct\n')
-        else:
-            self._file_.write('Cartesian\n')
-        for iatom in nlist(coord):
-           string='  %.16f %.16f %.16f' %  tuple(coord[iatom])
-           if con != []:
-               str_ = ''
-               for c in range(3):
-                   str_+=str(bool(-con[iatom][c]+1))[0]+' '
-               string += '  ' + str_
-           self._file_.write(string+'\n')
+    if type(filename) == str:
+        f.close()
 
 class ReadPOTCAR:
     """Class that read data from POTCAR file.
@@ -309,7 +230,7 @@ class ReadPOTCAR:
         atomtypes=[]
         for line in lines:
             if re.search('TITEL',line):
-                atomtypes.append(split(split(line.split()[3],'_')[0],'.')[0])
+                atomtypes.append(line.split()[3].split('_')[0].split('.')[0])
         return atomtypes
 
 class ReadOUTCAR:
@@ -329,12 +250,6 @@ class ReadOUTCAR:
         atomtypes=[]
         for line in lines:
             if re.search('TITEL',line):
-                atomtypes.append(split(split(line.split()[3],'_')[0],'.')[0])
+                atomtypes.append(line.split()[3].split('_')[0].split('.')[0])
         return atomtypes
 
-def element_string(list):
-    from ase.data import chemical_symbols
-    elements = ''
-    for n in range(len(list)):
-        elements += chemical_symbols[list[n]]+' '
-    return elements
