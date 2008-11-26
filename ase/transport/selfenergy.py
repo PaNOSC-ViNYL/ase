a0d11c1846ae14deed8b490be6c194a344a47528
jingzhe <jingzhe@5af997e4-4c3d-0410-961e-ac0512e437d9>
2008-11-26 19:15:47 +0000

    do some preparation for integral

--- ase/transport/selfenergy.py
+++ ase/transport/selfenergy.py
@@ -1,49 +1,88 @@
 import numpy as npy
 from ase.transport.tools import dagger
 
+# ## add ended by ---- if no ,oneline, if modify, comment the line before
+
 class LeadSelfEnergy:
+    #---implemented by Jingzhe Chen-------
+    #nume    // number of energy points 
+    #energy     // energy point in integral path,list
+    #wgp     // integral weight factor in integral path,list
+    #fgp     // the Fermi Distribution factor in integral path,list
+    #nres    // the num of residure in the integral countour,list
+    #sigma_mm     // the stored leadselfenergy cooresponding to Egp, list of matrix
+    #bias          // the shift value for hamiltonian
+    #kpts         //kpoints,list
+        
+    
     conv = 1e-8 # Convergence criteria for surface Green function
     
-    def __init__(self, hs_dii, hs_dij, hs_dim, eta=1e-4):
-        self.h_ii, self.s_ii = hs_dii # onsite principal layer
-        self.h_ij, self.s_ij = hs_dij # coupling between principal layers
-        self.h_im, self.s_im = hs_dim # coupling to the central region
-        self.nbf = self.h_im.shape[1] # nbf for the scattering region
-        self.eta = eta
-        self.energy = None
-        self.sigma_mm = npy.empty((self.nbf, self.nbf), complex)
-
-    def __call__(self, energy):
-        """Return self-energy (sigma)"""
-        if energy != self.energy:
-            self.energy = energy
-            z = energy + self.eta * 1.j           
-            tau_im = z * self.s_im - self.h_im
-            a_im = npy.linalg.solve(self.get_sgfinv(energy), tau_im)
-            tau_mi = z * dagger(self.s_im) - dagger(self.h_im)
-            self.sigma_mm[:] = npy.dot(tau_mi, a_im)
-
-        return self.sigma_mm
     
-    def get_lambda(self, energy):
+    def __init__(self, hsk_dii, hsk_dij, eta=1e-4):
+        self.hk_ii, self.sk_ii = hsk_dii # onsite principal layer
+        self.hk_ij, self.sk_ij = hsk_dij # coupling between principal layers
+                                        # coupling to the central region
+        self.nbf = self.hk_ii.shape[-1] # nbf for the scattering region
+        self.eta = eta
+        
+        self.nkpts = self.hk_ii.shape[1]
+        self.sigmak_mm = [npy.empty((self.nbf, self.nbf), complex)] * \
+                                                                    self.nkpts
+        self.sigma = npy.empty([self.nbf, self.nbf], complex)
+        self.nume = [0] * self.nkpts
+        self.nres = [0] * self.nkpts
+        self.egp = []
+        self.wgp = []
+        self.fgp = []
+        for i in range(self.nkpts):
+            self.egp.append([])
+            self.wgp.append([])
+            self.fgp.append([])
+        self.bias = 0
+
+
+    def __call__(self, energy, pkpt=0):
+        """Return self-energy (sigma)"""
+        if energy != None:
+            z = energy - self.bias 
+            self.sigma = npy.empty([self.nbf, self.nbf], complex)
+            tau_im = npy.empty([1, self.nbf, self.nbf], complex )
+            tau_mi = npy.empty([1, self.nbf, self.nbf], complex )            
+            a_im = npy.empty([1, self.nbf, self.nbf], complex )            
+
+            tau_im = z * self.sk_ij[pkpt] - self.hk_ij[pkpt]                
+            sgf = self.get_sgfinv(energy, pkpt)
+            a_im = npy.linalg.solve(sgf, tau_im)
+            tau_mi = z * dagger(self.sk_ij[pkpt]) - dagger(self.hk_ij[pkpt])
+            self.sigma = npy.dot(tau_mi, a_im)
+        return self.sigma
+
+    def get_lambda(self, energy, pkpt=0):
         """Return the lambda (aka Gamma) defined by i(S-S^d).
 
         Here S is the retarded selfenergy, and d denotes the hermitian
         conjugate.
         """
-        sigma_mm = self(energy)
-        return 1.j * (sigma_mm - dagger(sigma_mm))
-        
-    def get_sgfinv(self, energy):
-        """The inverse of the retarded surface Green function""" 
-        z = energy + self.eta * 1.0j
-        
-        v_00 = z * dagger(self.s_ii) - dagger(self.h_ii)
-        v_11 = v_00.copy()
-        v_10 = z * self.s_ij - self.h_ij
-        v_01 = z * dagger(self.s_ij) - dagger(self.h_ij)
+        ##
+        energy = npy.array(energy)
+        sigma_mm = self(energy, pkpt)
+        return 1.j * (sigma_mm - npy.resize(dagger(sigma_mm), sigma_mm.shape))
 
+    def get_sgfinv(self, energy, pkpt=0):
+        """The inverse of the retarded surface Green function""" 
+        z = energy - self.bias 
         delta = self.conv + 1
+       
+
+        v_00 = npy.empty([self.nbf, self.nbf], complex)
+        v_01 = npy.empty([self.nbf, self.nbf], complex)
+        v_10 = npy.empty([self.nbf, self.nbf], complex)
+        v_11 = npy.empty([self.nbf, self.nbf], complex)        
+        
+        v_00 = z * dagger(self.sk_ii[pkpt]) - dagger(self.hk_ii[pkpt])
+        v_11 = v_00.copy()
+        v_10 = z * self.sk_ij[pkpt] - self.hk_ij[pkpt]
+        v_01 = z * dagger(self.sk_ij[pkpt]) - dagger(self.hk_ij[pkpt])
         n = 0
         while delta > self.conv:
             a = npy.linalg.solve(v_11, v_01)
@@ -54,8 +93,13 @@ class LeadSelfEnergy:
             v_11 -= v_01_dot_b
             v_01 = -npy.dot(v_01, a)
             v_10 = -npy.dot(v_10, b)
-        
             delta = npy.abs(v_01).max()
             n += 1
+        return v_00  
+    
+    
+    def set_bias(self, bias):
+        self.bias = bias
 
-        return v_00
+        
+    
