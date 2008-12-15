e4b98f6ea80aa9ede8db58ea385daebb2b9f0d2a
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2008-12-15 07:47:30 +0000

    We don't really need the 'role' and 'directive' directives.

--- doc/conf.py
+++ doc/conf.py
@@ -144,33 +144,3 @@ latex_preamble = '\usepackage{amsmath}'
 # Extension interface
 # -------------------
 
-import re
-from sphinx import addnodes
-
-dir_sig_re = re.compile(r'\.\. ([^:]+)::(.*)$')
-
-def parse_directive(env, sig, signode):
-    if not sig.startswith('.'):
-        dec_sig = '.. %s::' % sig
-        signode += addnodes.desc_name(dec_sig, dec_sig)
-        return sig
-    m = dir_sig_re.match(sig)
-    if not m:
-        signode += addnodes.desc_name(sig, sig)
-        return sig
-    name, args = m.groups()
-    dec_name = '.. %s::' % name
-    signode += addnodes.desc_name(dec_name, dec_name)
-    signode += addnodes.desc_classname(args, args)
-    return name
-
-
-def parse_role(env, sig, signode):
-    signode += addnodes.desc_name(':%s:' % sig, ':%s:' % sig)
-    return sig
-
-
-def setup(app):
-    app.add_description_unit('directive', 'dir', 'pair: %s; directive', parse_directive)
-    app.add_description_unit('role', 'role', 'pair: %s; role', parse_role)
-    app.add_description_unit('confval', 'confval', 'pair: %s; configuration value')
