abff7019c2956403aef6a9dad18a1c22a92f762f
jblomqvist <jblomqvist@5af997e4-4c3d-0410-961e-ac0512e437d9>
2008-12-29 13:14:25 +0000

    Move VASP charge density stuff into own class.
    Also store the augmentation charges for CHGCAR files.

--- ase/calculators/vasp.py
+++ ase/calculators/vasp.py
@@ -731,23 +731,70 @@ class Vasp:
         else:
             raise NotImplementedError('Only Monkhorst-Pack and gamma centered grid supported for restart.')
 
-    def read_charge_density(self, filename='CHG'):
+
+class VaspChargeDensity(object):
+    """Class for representing VASP charge density"""
+
+    def __init__(self, filename='CHG'):
+        # Instance variables
+        self.atoms = []   # List of Atoms objects
+        self.chg = []     # Charge density
+        self.chgdiff = [] # Charge density difference, if spin polarized
+        self.aug = ''     # Augmentation charges, not parsed just a big string
+        
+        # Note that the augmentation charge is not a list, since they
+        # are needed only for CHGCAR files which store only a single
+        # image.
+        
+        self.read(filename)
+
+    def is_spin_polarized(self):
+        if len(self.chgdiff) > 0:
+            return True
+        return False
+
+    def _read_chg(self, fobj, chg, volume):
+        """Read charge from file object
+
+        Utility method for reading the actual charge density (or
+        charge density difference) from a file object. On input, the
+        file object must be at the beginning of the charge block, on
+        output the file position will be left at the end of the
+        block. The chg array must be of the correct dimensions.
+
+        """
+        # VASP writes charge density as
+        # WRITE(IU,FORM) (((C(NX,NY,NZ),NX=1,NGXC),NY=1,NGYZ),NZ=1,NGZC)
+        # Fortran nested implied do loops; innermost index fastest
+        # First, just read it in
+        for zz in range(chg.shape[2]):
+            for yy in range(chg.shape[1]):
+                chg[:, yy, zz] = np.fromfile(fobj, count = chg.shape[0],
+                                             sep=' ')
+        chg /= volume
+
+    def read(self, filename='CHG'):
         """Read CHG or CHGCAR file.
 
-        Returns list of (atoms, chg, chgdiff*) tuples.
-        
         If CHG contains charge density from multiple steps all the
-        steps are read and returned. By default VASP writes out the
-        charge density every 10 steps.
+        steps are read and stored in the object. By default VASP
+        writes out the charge density every 10 steps.
 
-        chgdiff is the difference between the spin up charge density and
-        the spin up charge density and is thus only returned for a spin-
-        polarized calculation.
+        chgdiff is the difference between the spin up charge density
+        and the spin up charge density and is thus only read for a
+        spin-polarized calculation.
+
+        aug is the PAW augmentation charges found in CHGCAR. These are
+        not parsed, they are just stored as a string so that they can
+        be written again to a CHGCAR format file.
 
         """
         import ase.io.vasp as aiv
         f = open(filename)
-        nchg = []
+        self.atoms = []
+        self.chg = []
+        self.chgdiff = []
+        self.aug = ''
         while True:
             try:
                 atoms = aiv.read_vasp(f)
@@ -759,83 +806,78 @@ class Vasp:
             ngr = f.readline().split()
             ng = (int(ngr[0]), int(ngr[1]), int(ngr[2]))
             chg = np.empty(ng)
-            # VASP writes charge density as
-            # WRITE(IU,FORM) (((C(NX,NY,NZ),NX=1,NGXC),NY=1,NGYZ),NZ=1,NGZC)
-            # Fortran nested implied do loops; innermost index fastest
-            # First, just read it in
-            for zz in range(chg.shape[2]):
-                for yy in range(chg.shape[1]):
-                    chg[:, yy, zz] = np.fromfile(f, count = chg.shape[0],
-                                                 sep=' ')
-            chg /= atoms.get_volume()
+            self._read_chg(f, chg, atoms.get_volume())
+            self.chg.append(chg)
+            self.atoms.append(atoms)
             # Check if the file has a spin-polarized charge density part, and
             # if so, read it in.
             fl = f.tell()
             # First check if the file has an augmentation charge part (CHGCAR file.)
             line1 = f.readline()
             if line1=='':
-                nchg.append((atoms, chg))
                 break
-            elif line1.split()[0]=='augmentation':
-                while True:
-                    line2 = f.readline()
-                    if line2.split()==ngr:
-                        chgdiff = np.empty(ng)
-                        for zz in range(chg.shape[2]):
-                            for yy in range(chg.shape[1]):
-                                chgdiff[:, yy, zz] = np.fromfile(f, count = chgdiff.shape[0],
-                                                                 sep=' ')
-                        chgdiff /= atoms.get_volume()
-                        nchg.append((atoms, chg, chgdiff))
-                    elif line2=='':
-                        nchg.append((atoms, chg))
-                        break
-            elif line1.split()==ngr:
+            elif line1.find('augmentation') != -1:
+                self.aug += line1
+                for line2 in f.readlines():
+                    self.aug += line2
+                break
+            elif line1.split() == ngr:
                 chgdiff = np.empty(ng)
-                for zz in range(chgdiff.shape[2]):
-                    for yy in range(chgdiff.shape[1]):
-                        chgdiff[:, yy, zz] = np.fromfile(f, count = chgdiff.shape[0],
-                                                         sep=' ')
-                chgdiff /= atoms.get_volume()
-                nchg.append((atoms, chg, chgdiff))
+                self._read_chg(f, chgdiff, atoms.get_volume())
+                self.chgdiff.append(chgdiff)
             else:
                 f.seek(fl)
-                nchg.append((atoms, chg))
         f.close()
-        return nchg
 
-    def write_charge_density(self, chgs, filename='CHG'):
+    def _write_chg(self, fobj, chg, volume, format='chg'):
+        """Write charge density
+
+        Utility function similar to _read_chg but for writing.
+
+        """
+        n = 0
+        for zz in range(chg.shape[2]):
+            for yy in range(chg.shape[1]):
+                for xx in range(chg.shape[0]):
+                    if format.lower() == 'chg':
+                        fobj.write(' %#11.5G' % (chg[xx, yy, zz] * volume))
+                        n += 1
+                    else:
+                        fobj.write(' %17.10E' % (chg[xx, yy, zz] * volume))
+                        n += 2
+                    if n % 10 == 0:
+                        # Write 5 or 10 values per line
+                        fobj.write('\n')
+
+    def write(self, filename='CHG', format=None):
         """Write VASP charge density in CHG format
 
-        Note that the CHGCAR format is not supported, since the PAW
-        1-center occupancies in that file are not handled.
-
-        chgs -- list of (atoms, chg) tuples, where the atoms object
-        specifies the atomic positions for that charge density image.
+        filename -- Name of file to write to
+        format -- String specifying whether to write in CHGCAR or CHG
+                  format
 
         """
         import ase.io.vasp as aiv
+        if format == None:
+            if filename.lower().find('chgcar') != -1 or len(self.chg) == 1:
+                format = "chgcar"
+            else:
+                format = "chg"
         f = open(filename, 'w')
-        if hasattr(chgs[0], 'cell'):
-            chgs = [chgs]
-        for atoms_chgs in chgs:
-            aiv.write_vasp(f, atoms_chgs[0], direct=True)
+        for ii, chg in enumerate(self.chg):
+            if format == 'chgcar' and ii != len(self.chg) - 1:
+                continue # Write only the last image for CHGCAR
+            aiv.write_vasp(f, self.atoms[ii], direct=True)
             f.write('\n')
-            for m in range(1, len(atoms_chgs)):
-                for dim in atoms_chgs[m].shape:
-                    f.write(' %4i' % dim)
-                f.write('\n')
-                atoms_chgs[m][:] *= atoms_chgs[0].get_volume()
-                n = 0
-                for zz in range(atoms_chgs[m].shape[2]):
-                    for yy in range(atoms_chgs[m].shape[1]):
-                        for xx in range(atoms_chgs[m].shape[0]):
-                            f.write(' %#11.5G' % atoms_chgs[m][xx, yy, zz])
-                            n += 1
-                            if n % 10 == 0:
-                                # Write 10 values per line
-                                f.write('\n')
-                atoms_chgs[m][:] /= atoms_chgs[0].get_volume()
+            for dim in chg.shape:
+                f.write(' %4i' % dim)
+            f.write('\n')
+            vol = self.atoms[ii].get_volume()
+            self._write_chg(f, chg, vol, format)
+            if self.is_spin_polarized():
+                self._write_chg(f, self.chgdiff[ii], vol, format)
+            if format == 'chgcar':
+                f.write(self.aug)
         f.close()
 
 
