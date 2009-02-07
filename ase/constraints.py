a7fd20db18064b73847a1573d018e624548d55fe
jkitchin <jkitchin@5af997e4-4c3d-0410-961e-ac0512e437d9>
2009-02-07 19:14:31 +0000

    added UnitCellFilter which can be used to relax internal and unit cell degrees of freedom

--- ase/constraints.py
+++ ase/constraints.py
@@ -369,3 +369,135 @@ class StrainFilter:
 
     def __len__(self):
         return 2
+
+class UnitCellFilter:
+    """Modify the supercell and the atom positions. """
+    def __init__(self, atoms, mask=None):
+        """Create a filter that returns the atomic forces and unit
+        cell stresses together, so they can simultaneously be
+        minimized.
+
+        The first argument, atoms, is the atoms object.
+
+        The optional second argument, mask, is a list of booleans,
+        indicating which of the six independent
+        components of the strain are relaxed.
+        1, True = relax to zero
+        0, False = fixed, ignore this component
+        
+        use atom Constraints, e.g. FixAtoms, to control relaxation of
+        the atoms.
+
+        #this should be equivalent to the StrainFilter
+        >>> atoms = Atoms(...)
+        >>> atoms.set_constraint(FixAtoms(mask=[True for atom in atoms]))
+        >>> ucf = UCFilter(atoms)
+
+        You should not attach this UCFilter object to a
+        trajectory. Instead, create a trajectory for the atoms, and
+        attach it to an optimizer like this:
+
+        >>> atoms = Atoms(...)
+        >>> ucf = UCFilter(atoms)
+        >>> qn = QuasiNewton(ucf)
+        >>> traj = PickleTrajectory('TiO2.traj','w',atoms)
+        >>> qn.attach(traj)
+        >>> qn.run(fmax=0.05)
+
+        Helpful conversion table
+        ========================
+        0.05 eV/A^3   = 8 GPA
+        0.003 eV/A^3  = 0.48 GPa
+        0.0006 eV/A^3 = 0.096 GPa
+        0.0003 eV/A^3 = 0.048 GPa
+        0.0001 eV/A^3 = 0.02 GPa
+        """
+        
+        self.atoms = atoms
+        self.strain = np.zeros(6)
+                
+        if mask is None:
+            self.mask = np.ones(6)
+        else:
+            self.mask = np.array(mask)
+
+        self.origcell = atoms.get_cell()
+        
+    def get_positions(self):
+        '''
+        this returns an array with shape (natoms+2,3).
+
+        the first natoms rows are the positions of the atoms, the last
+        two rows are the strains associated with the unit cell
+        '''
+
+        atom_positions = self.atoms.get_positions()
+        strains = self.strain.reshape((2, 3))
+
+        natoms = len(self.atoms)
+        all_pos = np.zeros((natoms+2,3),np.float)
+        all_pos[0:natoms,:] = atom_positions
+        all_pos[natoms:,:] = strains
+
+        return all_pos
+
+    def set_positions(self, new):
+        '''
+        new is an array with shape (natoms+2,3).
+        
+        the first natoms rows are the positions of the atoms, the last
+        two rows are the strains used to change the cell shape.
+
+        The atom positions are set first, then the unit cell is
+        changed keeping the atoms in their scaled positions.
+        '''        
+
+        natoms = len(self.atoms)
+        
+        atom_positions = new[0:natoms,:]
+        self.atoms.set_positions(atom_positions)
+
+        new = new[natoms:,:] #this is only the strains
+        new = new.ravel() * self.mask
+        eps = np.array([[1.0 + new[0], 0.5 * new[5], 0.5 * new[4]],
+                        [0.5 * new[5], 1.0 + new[1], 0.5 * new[3]],
+                        [0.5 * new[4], 0.5 * new[3], 1.0 + new[2]]])
+
+        self.atoms.set_cell(np.dot(self.origcell, eps), scale_atoms=True)
+        self.strain[:] = new
+
+    def get_forces(self,apply_constraint=False):
+        '''
+        returns an array with shape (natoms+2,3) of the atomic forces
+        and unit cell stresses.
+
+        the first natoms rows are the forces on the atoms, the last
+        two rows are the stresses on the unit cell, which have been
+        reshaped to look like "atomic forces". i.e.,
+
+        f[-2] = -vol*[sxx,syy,szz]*mask[0:3]
+        f[-1] = -vol*[syz, sxz, sxy]*mask[3:]
+
+        apply_constraint is an argument expected by ase
+        '''
+        
+        stress = self.atoms.get_stress()
+        atom_forces = self.atoms.get_forces()
+
+        natoms = len(self.atoms)
+        all_forces = np.zeros((natoms+2,3),np.float)
+        all_forces[0:natoms,:] = atom_forces
+
+        vol = self.atoms.get_volume()
+        stress_forces = -vol * (stress * self.mask).reshape((2, 3))
+        all_forces[natoms:,:] = stress_forces 
+        return all_forces
+
+    def get_potential_energy(self):
+        return self.atoms.get_potential_energy()
+
+    def has(self, x):
+        return self.atoms.has(x)
+
+    def __len__(self):
+        return (2 + len(self.atoms))
