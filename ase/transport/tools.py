59bf9abb76de7a45b734978b4429b5552b6b1d20
jingzhe <jingzhe@5af997e4-4c3d-0410-961e-ac0512e437d9>
2009-02-09 12:37:37 +0000

    decrease the integral points

--- ase/transport/tools.py
+++ ase/transport/tools.py
@@ -81,12 +81,34 @@ def fliplr(a):
         b[i] = a[length - i - 1]
     return b
 
-def function_integral(function, intrange, tol = 1.e-6, trace = 0,
-                      arg1=None, arg2=None):
+def plot_path(energy):
+    import pylab
+    pylab.plot(npy.real(energy), npy.imag(energy), 'b--o')
+    pylab.show()
+    
+
+def function_integral(function, calcutype):
     #return the integral of the 'function' on 'intrange'    
     #the function can be a value or a matrix, arg1,arg2 are the possible
     #parameters of the function
-
+    
+    intctrl = function.intctrl
+    if calcutype == 'eqInt':
+        intrange = intctrl.eqintpath
+        tol = intctrl.eqinttol
+        radius = function.intctrl.eqpath_radius
+        origin = function.intctrl.eqpath_origin
+    elif calcutype == 'neInt':
+        intrange = intctrl.neintpath
+        tol = intctrl.neinttol
+        radius = function.intctrl.eqpath_radius
+        origin = function.intctrl.eqpath_origin  
+    elif calcutype == 'locInt':
+        intrange = intctrl.locintpath
+        tol = intctrl.locinttol
+        radius = function.intctrl.locpath_radius
+        origin = function.intctrl.locpath_origin
+    trace = 0    
     a = 0.
     b = 1.
 
@@ -112,25 +134,44 @@ def function_integral(function, intrange, tol = 1.e-6, trace = 0,
     for i in range(len(w1)):
         w1[i] = w1[i] / 6.0
         w2[i] = w2[i] / 1470.0
-                                                                            
+                                                        
     dZ = [intrange[:len(intrange) - 1], intrange[1:]]
     hmin = [0] * len(dZ[1])
+
+    path_type = []
+    for i in range(len(intrange) - 1):
+        rs = npy.abs(dZ[0][i] - origin)
+        re = npy.abs(dZ[1][i] - origin)
+        if abs(rs - radius) < 1.0e-8 and abs(re - radius) < 1.0e-8:
+            path_type.append('half_circle')
+        else:
+            path_type.append('line')
+   
     for i in range(len(dZ[1])):
-        dZ[1][i] = dZ[1][i]-dZ[0][i]
+        if path_type[i] == 'half_circle':
+            dZ[0][i] = 0
+            dZ[1][i] = npy.pi
+    for i in range(len(dZ[1])):
+        dZ[1][i] = dZ[1][i] - dZ[0][i]
         hmin[i] = realmin / 1024 * abs(dZ[1][i])
+
+
     temp = npy.array([[1] * 13, x0]).transpose()
+    
     Zx = npy.dot(temp, npy.array(dZ))
       
-    num = 0
-    Zxx = [Zx[0][0]]
+    Zxx = []
     for i in range(len(intrange) - 1):
-        for j in range(12):
-            num += 1
-            Zxx.append(Zx[j + 1][i])
+        for j in range(13):
+            Zxx.append(Zx[j][i])
 
     ns = 0
     ne = 12
-    yns = function.calgfunc(Zxx[ns], arg1, arg2)
+    if path_type[0] == 'line':
+        yns = function.calgfunc(Zxx[ns], calcutype)
+    elif path_type[0] == 'half_circle':
+        energy = origin + radius * npy.exp((npy.pi - Zxx[ns + i]) * 1.j)
+        yns = -1.j * radius * npy.exp(-1.j* Zxx[ns +i])* function.calgfunc(energy, calcutype)        
     fcnt = 0
     
 
@@ -140,9 +181,13 @@ def function_integral(function, intrange, tol = 1.e-6, trace = 0,
         Q2pQ0 = yns * (w2[0] - w0[0])
         fcnt = fcnt + 12
         for i in range(1,12):
-            yne = function.calgfunc(Zxx[ns + i], arg1, arg2)
-            Q1pQ0 = Q1pQ0 + yne * (w1[i] - w0[i])
-            Q2pQ0 = Q2pQ0 + yne * (w2[i] - w0[i])
+            if path_type[n] == 'line':
+                yne = function.calgfunc(Zxx[ns + i], calcutype)
+            elif path_type[n] == 'half_circle':
+                energy = origin + radius * npy.exp((npy.pi -Zxx[ns + i]) * 1.j)
+                yne = -1.j * radius * npy.exp(-1.j * Zxx[ns + i])* function.calgfunc(energy, calcutype)
+            Q1pQ0 += yne * (w1[i] - w0[i])
+            Q2pQ0 += yne * (w2[i] - w0[i])
 
         # Increase the tolerance if refinement appears to be effective
         r = npy.abs(Q2pQ0) / npy.abs(Q1pQ0 + realmin)
@@ -152,13 +197,18 @@ def function_integral(function, intrange, tol = 1.e-6, trace = 0,
             thistol = tol / r
         else:
             thistol = tol
-
-        yne = function.calgfunc(Zxx[ne], arg1, arg2)
+        if path_type[n] == 'line':
+            yne = function.calgfunc(Zxx[ne], calcutype)
+        elif path_type[n] == 'half_circle':
+            energy = origin + radius * npy.exp((npy.pi -Zxx[ne]) * 1.j)
+            yne = -1.j * radius * npy.exp(-1.j * Zxx[ne])* function.calgfunc(energy, calcutype)
         #Call the recursive core integrator
+       
         Qk, xpk, wpk, fcnt, warn = quadlstep(function, Zxx[ns],
                                             Zxx[ne], yns, yne,
                                             thistol, trace, fcnt,
-                                            hmin[n], arg1, arg2)
+                                            hmin[n], calcutype, path_type[n],
+                                            origin, radius)
         if n == 0:
             Q = npy.copy(Qk)
             Xp = xpk[:]
@@ -176,13 +226,14 @@ def function_integral(function, intrange, tol = 1.e-6, trace = 0,
         else:
             pass
         
-        ns += 12
-        ne += 12
+        ns += 13
+        ne += 13
         yns = npy.copy(yne)
       
     return Q,Xp,Wp,fcnt
 
-def quadlstep(f, Za, Zb, fa, fb, tol, trace, fcnt, hmin, arg1, arg2):
+def quadlstep(f, Za, Zb, fa, fb, tol, trace, fcnt, hmin, calcutype,
+                                                   path_type, origin, radius):
     #Gaussian-Lobatto and Kronrod method
     #QUADLSTEP Recursive core routine for integral
     #input parameters:
@@ -205,7 +256,11 @@ def quadlstep(f, Za, Zb, fa, fb, tol, trace, fcnt, hmin, arg1, arg2):
     if abs(Zh) < hmin:
         # Minimun step size reached; singularity possible
         Q = Zh * (fa + fb)
-        Xp = [Za, Zb]
+        if path_type == 'line':
+            Xp = [Za, Zb]
+        elif path_type == 'half_circle':
+            Xp = [origin + radius * npy.exp((npy.pi - Za) * 1.j),
+                                      origin + radius * npy.exp((npy.pi - Zb) * 1.j)]
         Wp = [Zh, Zh]
         warn = 1
         return Q, Xp, Wp, fcnt, warn
@@ -213,7 +268,11 @@ def quadlstep(f, Za, Zb, fa, fb, tol, trace, fcnt, hmin, arg1, arg2):
     if fcnt > maxfcnt:
         #Maximum function count exceed; singularity likely
         Q = Zh * (fa + fb)
-        Xp = [Za, Zb]
+        if path_type == 'line':
+            Xp = [Za, Zb]
+        elif path_type == 'half_circle':
+            Xp = [origin + radius * npy.exp((npy.pi - Za) * 1.j),
+                                      origin + radius * npy.exp((npy.pi - Zb) * 1.j)]
         Wp = [Zh, Zh]
         warn = 2
         return Q, Xp, Wp, fcnt, warn
@@ -223,8 +282,12 @@ def quadlstep(f, Za, Zb, fa, fb, tol, trace, fcnt, hmin, arg1, arg2):
     y = [0] * len(x)
     for i in range(len(x)):
         x[i] *= 0.5
-        Zx[i] = Za + (Zb-Za) * x[i]
-        y[i] = f.calgfunc(Zx[i], arg1, arg2)
+        Zx[i] = Za + (Zb - Za) * x[i]
+        if path_type == 'line':
+            y[i] = f.calgfunc(Zx[i], calcutype)
+        elif path_type == 'half_circle':
+            energy = origin + radius * npy.exp((npy.pi - Zx[i]) * 1.j)
+            y[i] = f.calgfunc(energy, calcutype)
     #Four point Lobatto quadrature
     s1 = [1.0, 0.0, 5.0, 0.0, 5.0, 0.0, 1.0]
     s2 = [77.0, 432.0, 625.0, 672.0, 625.0, 432.0, 77.0]
@@ -233,8 +296,15 @@ def quadlstep(f, Za, Zb, fa, fb, tol, trace, fcnt, hmin, arg1, arg2):
     for i in range(7):
         Wk[i] = (Zh / 6.0) * s1[i]
         Wp[i] = (Zh / 1470.0) * s2[i]
-    Xp = [Za] + Zx + [Zb]
-
+    if path_type == 'line':
+        Xp = [Za] + Zx + [Zb]
+    elif path_type == 'half_circle':
+        Xp = [Za] + Zx + [Zb] 
+        for i in range(7):
+            factor = -1.j * radius * npy.exp(1.j * (npy.pi - Xp[i]))
+            Wk[i] *= factor
+            Wp[i] *= factor
+            Xp[i] = origin + radius * npy.exp((npy.pi - Xp[i]) * 1.j)
     Qk = fa * Wk[0] + fb * Wk[6]
     Q = fa * Wp[0] + fb * Wp[6]
     for i in range(1, 6):
@@ -242,7 +312,11 @@ def quadlstep(f, Za, Zb, fa, fb, tol, trace, fcnt, hmin, arg1, arg2):
         Q  += y[i-1] * Wp[i]
     if npy.isinf(npy.max(npy.abs(Q))):
         Q = Zh * (fa + fb)
-        Xp = [Za, Zb]
+        if path_type == 'line':
+            Xp = [Za, Zb]
+        elif path_type == 'half_circle':
+            Xp = [origin + radius * npy.exp((npy.pi - Za) * 1.j),
+                                      origin + radius * npy.exp((npy.pi - Zb) * 1.j)]
         Wp = [Zh, Zh]
         warn = 3
         return Qk, Xp, Wp, fcnt, warn
@@ -251,30 +325,37 @@ def quadlstep(f, Za, Zb, fa, fb, tol, trace, fcnt, hmin, arg1, arg2):
     if trace:
         print fcnt, real(Za), imag(Za), abs(Zh)
     #Check accurancy of integral over this subinterval
+    XXk = [Xp[0], Xp[2], Xp[4], Xp[6]]
+    WWk = [Wk[0], Wk[2], Wk[4], Wk[6]]
+    YYk = [fa, y[1], y[3], fb]
     if npy.max(npy.abs(Qk - Q)) <= tol:
         warn = 0
-        return Q, Xp, Wp, fcnt, warn
+        return Q, XXk, WWk, fcnt, warn
     #Subdivide into six subintevals
     else:
-        Q, Xp, Wp, fcnt, warn = quadlstep(f, Za, Zx[0], fa, y[0],
+        Q, Xk, Wk, fcnt, warn = quadlstep(f, Za, Zx[1], fa, YYk[1],
                                            tol, trace, fcnt, hmin,
-                                               arg1, arg2)
-        for k in range(1, 5):
-            Qk, xpk, wpk, fcnt, warnk = quadlstep(f, Zx[k - 1],
-                    Zx[k], y[k - 1], y[k], tol, trace, fcnt, hmin,
-                                             arg1, arg2)
-            Q += Qk
-            Xp = Xp[:-1] + xpk
-            Wp = Wp[:-1] + [Wp[-1] + wpk[0]] + wpk[1:]
-            warn = max(warn, warnk)
-        Qk, xpk, wpk, fcnt, warnk = quadlstep(f, Zx[4], Zb, y[4], fb,
-                                           tol, trace, fcnt, hmin,
-                                                   arg1, arg2)
+                                               calcutype, path_type,
+                                                origin, radius)
+
+        Qk, xkk, wkk, fcnt, warnk = quadlstep(f, Zx[1],
+                          Zx[3], YYk[1], YYk[2], tol, trace, fcnt, hmin,
+                                             calcutype, path_type,
+                                             origin, radius)
         Q += Qk
-        Xp = Xp[:-1] + xpk
-        Wp = Wp[:-1] + [Wp[-1] + wpk[0]] + wpk[1:]
+        Xk = Xk[:-1] + xkk
+        Wk = Wk[:-1] + [Wk[-1] + wkk[0]] + wkk[1:]
         warn = max(warn, warnk)
-    return Q, Xp, Wp, fcnt, warn
+        
+        Qk, xkk, wkk, fcnt, warnk = quadlstep(f, Zx[3], Zb, YYk[2], fb,
+                                           tol, trace, fcnt, hmin,
+                                                   calcutype, path_type,
+                                                   origin, radius)
+        Q += Qk
+        Xk = Xk[:-1] + xkk
+        Wk = Wk[:-1] + [Wk[-1] + wkk[0]] + wkk[1:]
+        warn = max(warn, warnk)
+    return Q, Xk, Wk, fcnt, warn
 
 def mytextread0(filename):
     num = 0
