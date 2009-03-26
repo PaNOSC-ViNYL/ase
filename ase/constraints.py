b900b4eb9ba3936485c69e5e0fb1630d87fe67ea
schiotz <schiotz@5af997e4-4c3d-0410-961e-ac0512e437d9>
2009-03-26 13:47:36 +0000

    Undoing commit r841

--- ase/constraints.py
+++ ase/constraints.py
@@ -292,10 +292,8 @@ class Filter:
 
         if mask is not None:
             self.index = np.asarray(mask, bool)
-            self.n = self.index.sum()
         else:
             self.index = np.asarray(indices, int)
-            self.n = len(self.index)
 
     def get_cell(self):
         """Returns the computational cell.
@@ -370,7 +368,7 @@ class Filter:
 
     def __len__(self):
         "Return the number of movable atoms."
-        return self.n
+        return len(self.index)
 
     def __getitem__(self, i):
         "Return an atom."
