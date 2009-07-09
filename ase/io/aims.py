918dde7075e7e46931854a1b5dd0cabfe133af4b
bjork <bjork@5af997e4-4c3d-0410-961e-ac0512e437d9>
2009-07-09 23:49:38 +0000

    Aims read/write methods now also support the FixCartesian constraint.

--- ase/io/aims.py
+++ ase/io/aims.py
@@ -6,7 +6,8 @@ def read_aims(filename):
     a geometry.in file.
     """
 
-    from ase import Atoms, FixAtoms
+    from ase import Atoms, FixAtoms, FixCartesian
+    import numpy as np
 
     atoms = Atoms()
     fd = open(filename, 'r')
@@ -16,29 +17,47 @@ def read_aims(filename):
     cell = []
     symbols = []
     fix = []
+    fix_cart = []
+    xyz = np.array([0, 0, 0])
     i = -1
     for n, line in enumerate(lines):
         inp = line.split()
         if inp == []:
             continue
         if inp[0] == 'atom':
+            if xyz.all():
+                fix.append(i)
+            elif xyz.any():
+                print 1
+                fix_cart.append(FixCartesian(i, xyz))
             floatvect = float(inp[1]), float(inp[2]), float(inp[3])
             positions.append(floatvect)
             symbols.append(inp[-1])
             i += 1
+            xyz = np.array([0, 0, 0])
         elif inp[0] == 'lattice_vector':
             floatvect = float(inp[1]), float(inp[2]), float(inp[3])
             cell.append(floatvect)
-        try:
-            if lines[n+1].split()[0] == 'constraint_relaxation':
+        if inp[0] == 'constraint_relaxation':
+            if inp[1] == '.true.':
                 fix.append(i)
-        except IndexError:
-            continue
+            elif inp[1] == 'x':
+                xyz[0] = 1
+            elif inp[1] == 'y':
+                xyz[1] = 1
+            elif inp[1] == 'z':
+                xyz[2] = 1
+    if xyz.all():
+        fix.append(i)
+    elif xyz.any():
+        fix_cart.append(FixCartesian(i, xyz))
     atoms = Atoms(symbols, positions)
     if len(cell)==3:
         atoms.set_cell(cell)
     if len(fix):
-        atoms.set_constraint(FixAtoms(indices=fix))
+        atoms.set_constraint([FixAtoms(indices=fix)]+fix_cart)
+    else:
+        atoms.set_constraint(fix_cart)
     return atoms
 
 def write_aims(filename, atoms, cell=False):
@@ -49,7 +68,7 @@ def write_aims(filename, atoms, cell=False):
     written out.
     """
 
-    from ase.constraints import FixAtoms
+    from ase.constraints import FixAtoms, FixCartesian
     import numpy as np
 
     fd = open(filename, 'w')
@@ -61,10 +80,13 @@ def write_aims(filename, atoms, cell=False):
             fd.write('\n')
         fd.write('\n')
     fix = []
+    fix_cart = {}
     if atoms.constraints:
         for constr in atoms.constraints:
             if isinstance(constr, FixAtoms):
                 fix = constr.index
+            elif isinstance(constr, FixCartesian):
+                fix_cart[str(constr.a)] = -constr.mask+1
     fix = np.array(fix)%len(atoms)
     for i, atom in enumerate(atoms):
         fd.write('atom ')
@@ -74,7 +96,14 @@ def write_aims(filename, atoms, cell=False):
         fd.write('\n')
         if i in fix:
             fd.write('constraint_relaxation .true.\n')
-
+        else:
+            try:
+                xyz = fix_cart[str(i)]
+                for n in range(3):
+                    if xyz[n]:
+                        fd.write('constraint_relaxation %s\n' % 'xyz'[n])
+            except KeyError:
+                continue
 
 
 def read_energy(filename):
