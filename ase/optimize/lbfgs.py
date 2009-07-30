d88d509b1f3d607bf4da065a626ac42b444d1c4a
tjiang <tjiang@5af997e4-4c3d-0410-961e-ac0512e437d9>
2009-07-30 14:49:14 +0000

    slightly change of the classes in lbfgs

--- ase/optimize/lbfgs.py
+++ ase/optimize/lbfgs.py
@@ -19,7 +19,7 @@ class BaseLBFGS(Optimizer):
         Number of steps to be stored. Default value is 25.
 
     method: string
-        Two methods for determing atomic movement are available. If
+        Two methods for determine atomic movement are available. If
         method = 'line', a line search will be performed to determine
         the atomic movement. An extra scf loop for the trail step in
         each atomic step. And if method = 'hess', the atomic step will
@@ -27,8 +27,7 @@ class BaseLBFGS(Optimizer):
         include only one scf loop.  """
 
     def __init__(self, atoms, restart=None, logfile='-', trajectory=None,
-                 maxstep=0.2, dR=0.1,
-                 memory=25, alpha=0.05, method=None, damping=1.):
+                 maxstep=0.2, dR=0.1, memory=25, damping=1.):
         Optimizer.__init__(self, atoms, restart, logfile, trajectory)
 
         if maxstep > 1.0:
@@ -42,19 +41,13 @@ class BaseLBFGS(Optimizer):
         self.maxstep = maxstep
         self.dR = dR
         self.memory = memory
-        self.alpha = alpha
-        self.method = method
         self.damping = damping
 
     def initialize(self):
         self.ITR = None
         self.f_old = None
         self.r_old = None
-
-    def sign(self,w):
-        if(w < 0.0):
-            return -1.0
-        return 1.0
+        self.Ho = 1.0
 
     def read(self):
         (self.ITR, self.s, self.y, self.rho, self.r_old, 
@@ -63,37 +56,8 @@ class BaseLBFGS(Optimizer):
     def step(self, f):
         r = self.atoms.get_positions()
         self.update(r, f, self.r_old, self.f_old)
-
         du = self.d / np.sqrt(np.vdot(self.d, self.d))
-        if(self.method == 'hess'): 
-        # use the Hessian Matrix to predict the min
-            dr = self.d
-            if(abs(np.sqrt(np.vdot(dr, dr).sum())) > self.maxstep):
-                dr = du * self.maxstep
-
-        elif(self.method == 'line'):
-        # Finite difference step using temporary point
-            tmp_r = r.copy()
-            tmp_r += (du * self.dR)
-            self.tmp.set_positions(tmp_r)
-        # Decide how much to move along the line du
-            Fp1 = np.vdot(f, du)
-            Fp2 = np.vdot(self.tmp.get_forces(), du)
-            CR = (Fp1 - Fp2) / self.dR
-            #RdR = Fp1*0.1
-            if(CR < 0.0):
-                print "negcurve"
-                RdR = self.maxstep
-                #if(abs(RdR) > self.maxstep):
-                #    RdR = self.sign(RdR) * self.maxstep
-            else:
-                Fp = (Fp1 + Fp2) * 0.5
-                RdR = Fp / CR 
-                if(abs(RdR) > self.maxstep):
-                    RdR = self.sign(RdR) * self.maxstep
-                else:
-                    RdR += self.dR * 0.5
-            dr = du * RdR
+        dr = self.determine_step(r, f, du)        
         self.r_old = r.copy()
         self.f_old = f.copy()
         r += dr * self.damping
@@ -102,10 +66,7 @@ class BaseLBFGS(Optimizer):
     def update(self, r, f, r_old, f_old):
         a = np.zeros(self.memory + 1, 'd')
         self.tmp = self.atoms
-        self.Ho = np.ones((np.shape(r)[0], 3), 'd')
-        if (self.method == 'hess'):
-            self.Ho = self.Ho * self.alpha
-        if(not self.ITR):
+        if not self.ITR:
             self.ITR = 1
             self.s = [1.] # The 0'th element is not actually used
             # The point is to use 1-indexation
@@ -114,14 +75,8 @@ class BaseLBFGS(Optimizer):
         else:
             a1 = abs (np.vdot(f, f_old))
             a2 = np.vdot(f_old, f_old)
-            if(self.method == 'line'):
-                if(a1 <= 0.5 * a2 and a2 != 0):
-                    reset_flag = 0
-                else:
-                    reset_flag = 1
-            else:
-                reset_flag = 0
-            if(reset_flag == 0):
+            reset_flag = self.check_for_reset(a1, a2)
+            if not reset_flag:
                 ITR = self.ITR
                 if(ITR > self.memory):
                     self.s.pop(1)
@@ -141,7 +96,7 @@ class BaseLBFGS(Optimizer):
 
         r_old = r.copy()
         f_old = f.copy()
-        if(self.ITR <= self.memory):
+        if self.ITR <= self.memory:
             BOUND = self.ITR
         else:
             BOUND = self.memory
@@ -150,10 +105,10 @@ class BaseLBFGS(Optimizer):
             k = (BOUND - j)
             a[k] = self.rho[k] * np.vdot(self.s[k], q)
             q -= a[k] * self.y[k]
-        d = self.Ho * q 
+        d = self.Ho * q
         for j in range(1,BOUND):
             B = self.rho[j] * np.vdot(self.y[j], d)
-            d= d + self.s[j] * (a[j] - B)
+            d = d + self.s[j] * (a[j] - B)
         self.d = -1.0 * d
 
     def replay_trajectory(self, traj):
@@ -164,7 +119,7 @@ class BaseLBFGS(Optimizer):
         atoms = traj[0]
         r_old = atoms.get_positions()
         f_old = atoms.get_forces()
-        for i in range(0,len(traj)-1):
+        for i in range(0, len(traj) - 1):
             r = traj[i].get_positions()
             f = traj[i].get_forces()
             self.update(r, f, r_old, f_old)
@@ -173,19 +128,60 @@ class BaseLBFGS(Optimizer):
         self.r_old = traj[-2].get_positions()
         self.f_old = traj[-2].get_forces()
 
-class LineMinimizerLBFGS(BaseLBFGS):
-    def __init__(self, *args, **kwargs):
-        kwargs['method'] = 'line'
-        BaseLBFGS.__init__(self, *args, **kwargs)
-
 class LBFGS(BaseLBFGS):
-    def __init__(self, *args, **kwargs):
-        kwargs['method'] = 'hess'
-        BaseLBFGS.__init__(self, *args, **kwargs)
+    def initialize(self):
+        BaseLBFGS.initialize(self)
+        self.Ho = 0.05
+    
+    def determine_step(self, r, f, du):
+        # use the Hessian Matrix to predict the min
+        dr = self.d
+        if(abs(np.sqrt(np.vdot(dr, dr).sum())) > self.maxstep):
+            dr = du * self.maxstep
+        return dr
 
-def LineLBFGS(*args, **kwargs):
-    sys.stderr.write('Please use LineMinimizerLBFGS instead of LineLBFGS!')
-    return LineMinimizerLBFGS(*args, **kwargs)
-def HessLBFGS(*args, **kwargs):
-    sys.stderr.write('Please use LBFGS instead of HessLBFGS!')
-    return LBFGS(*args, **kwargs)
+    def check_for_reset(self, a1, a2):
+        return False
+
+class LineSearchLBFGS(BaseLBFGS):
+    """"""
+    def determine_step(self, r, f, du):
+        # Finite difference step using temporary point
+        tmp_r = r.copy()
+        tmp_r += (du * self.dR)
+        self.tmp.set_positions(tmp_r)
+        # Decide how much to move along the line du
+        Fp1 = np.vdot(f, du)
+        Fp2 = np.vdot(self.tmp.get_forces(), du)
+        CR = (Fp1 - Fp2) / self.dR
+        #RdR = Fp1*0.1
+        if CR < 0.0:
+            #print "negcurve"
+            RdR = self.maxstep
+            #if(abs(RdR) > self.maxstep):
+            #    RdR = self.sign(RdR) * self.maxstep
+        else:
+            Fp = (Fp1 + Fp2) * 0.5
+            RdR = Fp / CR 
+            if abs(RdR) > self.maxstep:
+                RdR = np.sign(RdR) * self.maxstep
+            else:
+                RdR += self.dR * 0.5
+        return du * RdR
+
+    def check_for_reset(self, a1, a2):
+        return not (a1 <= 0.5 * a2 and a2 != 0)
+
+class LineLBFGS(LineSearchLBFGS):
+    def __init__(self, *args, **kwargs):
+        if 'method' in kwargs:
+            del kwargs['method']
+        sys.stderr.write('Please use LineMinimizerLBFGS instead of LineLBFGS!')
+        LineSearchLBFGS.__init__(self, *args, **kwargs)
+
+class HessLBFGS(LBFGS):
+    def __init__(self, *args, **kwargs):
+        if 'method' in kwargs:
+            del kwargs['method']
+        sys.stderr.write('Please use LBFGS instead of HessLBFGS!')
+        LBFGS.__init__(self, *args, **kwargs)
