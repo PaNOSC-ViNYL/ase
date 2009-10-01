dda88284e367fbdf9b5033f0f8029f0cca7898ac
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2009-10-01 08:07:11 +0000

    Silence Sphinx warning: 'html_favicon is not an .ico file'

--- doc/conf.py
+++ doc/conf.py
@@ -114,12 +114,12 @@ html_style = 'ase.css'
 
 # The name of an image file (within the static path) to place at the top of
 # the sidebar.
-html_logo = '_static/ase.png'
+html_logo = '_static/ase.ico'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = '_static/ase.png'
+html_favicon = '_static/ase.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
