26f383697e92927416c5888ccd589e15188e827f
tjiang <tjiang@5af997e4-4c3d-0410-961e-ac0512e437d9>
2009-10-20 11:53:10 +0000

    adding help for total energy plot in ag

--- ase/gui/languages/en.py
+++ ase/gui/languages/en.py
@@ -4,18 +4,19 @@ translation = {
 
 'Help for plot ...':
 """Symbols:
-<c>e</c>:\t\tpotential energy
-<c>ekin</c>:\tkinetic energy
-<c>fmax</c>:\tmaximum force
-<c>fave</c>:\taverage force
-<c>R[n,0-2]</c>:\tposition of atom number <c>n</c> 
-<c>d(n<sub>1</sub>,n<sub>1</sub>)</c>:\tdistance between two atoms
-<c>i</c>:\t\tcurrent image number
-<c>E[i]</c>:\t\tenergy of image number <c>i</c>
-<c>F[n,0-2]</c>:\tforce on atom number <c>n</c>
-<c>M[n]</c>:\tmagnetic moment of atom number <c>n</c> 
+<c>e</c>:\t\t\ttotal energy
+<c>epot</c>:\t\tpotential energy
+<c>ekin</c>:\t\tkinetic energy
+<c>fmax</c>:\t\tmaximum force
+<c>fave</c>:\t\taverage force
+<c>R[n,0-2]</c>:\t\tposition of atom number <c>n</c> 
+<c>d(n<sub>1</sub>,n<sub>1</sub>)</c>:\t\tdistance between two atoms
+<c>i</c>:\t\t\tcurrent image number
+<c>E[i]</c>:\t\t\tenergy of image number <c>i</c>
+<c>F[n,0-2]</c>:\t\tforce on atom number <c>n</c>
+<c>M[n]</c>:\t\tmagnetic moment of atom number <c>n</c> 
 <c>A[0-2,0-2]</c>:\tunit-cell basis vectors 
-<c>s</c>:\t\tpath length""",
+<c>s</c>:\t\t\tpath length""",
 
 'Help for slice ...':
 """Use Python slice syntax: "start:stop:step" or "start:stop":
