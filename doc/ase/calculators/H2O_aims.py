f4f507d4f35da3862783ae283d02eb0293c78b66
hanke <hanke@5af997e4-4c3d-0410-961e-ac0512e437d9>
2009-10-26 23:07:54 +0000

    make example as simple as possible

--- doc/ase/calculators/H2O_aims.py
+++ doc/ase/calculators/H2O_aims.py
@@ -1,17 +1,8 @@
 from ase import *
 import os
 
-# calculating H2O from scratch using ASE and FHI-aims
-
-# definitions and variables
-species='~/codes/fhi-aims/fhi-aims.workshop/species_defaults/light/' # location of the species defaults
-run_command = 'aims.workshop.serial.x' # aims run command 
-outputname = 'output.water.relax'      # output file 
-
-# make a rectangular water
 water = Atoms([Atom('H',(1,0,0)),Atom('O',(0,0,0)),Atom('H',(0,1,0))])
 
-# make an FHI-aims calculator and set it for the water molecule
 calc=Aims(xc='pbe',
           sc_accuracy_etot=1e-6,
           sc_accuracy_eev=1e-3,
@@ -20,10 +11,8 @@ calc=Aims(xc='pbe',
           species_dir='/home/hanke/codes/fhi-aims/fhi-aims.workshop/species_defaults/light/',
           run_command='aims.workshop.serial.x')
 
-# relax using ASE procedure ... possibly not very efficient but a good start for the moment
 water.set_calculator(calc)
 dynamics = QuasiNewton(water,trajectory='square_water.traj')
 dynamics.run(fmax=0.01)
 
-# read data and display
 view(water)
