593f898bd337047df0759ab97b0b32f8e9c6632c
miwalter <miwalter@5af997e4-4c3d-0410-961e-ac0512e437d9>
2009-12-07 10:01:47 +0000

    Python 2.3 compability and better io test - by Lars Pastewka

--- ase/test/io.py
+++ ase/test/io.py
@@ -7,10 +7,12 @@ atoms = Atoms('AuH',
               positions=[(c, c, 0), (c, c, d)],
               cell=(a, a, 2 * d),
               pbc=(0, 0, 1))
+extra = np.array([ 2.3, 4.2 ])
+atoms.set_array("extra", extra)
 atoms *= (1, 1, 2)
 images = [atoms.copy(), atoms.copy()]
 
-r = ['xyz', 'traj', 'cube', 'pdb']
+r = ['xyz', 'traj', 'cube', 'pdb', 'cfg']
 w = r + ['xsf']
 try:
     import matplotlib
@@ -24,13 +26,18 @@ for format in w:
     fname1 = 'io-test.1.' + format
     fname2 = 'io-test.2.' + format
     write(fname1, atoms, format=format)
-    if format not in ['cube', 'png', 'eps']:
+    if format not in ['cube', 'png', 'eps', 'cfg']:
         write(fname2, images, format=format)
 
     if format in r:
         print 'I'
         a1 = read(fname1)
-        if format != 'cube':
+        assert np.all(np.abs(a1.get_positions() - atoms.get_positions()) < 1e-6)
+        if format in ['traj', 'cube', 'cfg']:
+            assert np.all(np.abs(a1.get_cell() - atoms.get_cell()) < 1e-6)
+        if format in ['cfg']:
+            assert np.all(np.abs(a1.get_array("extra") - atoms.get_array("extra")) < 1e-6)
+        if format not in ['cube', 'png', 'eps', 'cfg']:
             a2 = read(fname2)
             a3 = read(fname2, index=0)
             a4 = read(fname2, index=slice(None))
