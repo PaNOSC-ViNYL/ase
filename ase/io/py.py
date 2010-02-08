4201c7dcad23cf71b35947715baac4a47a885ce9
carstenr <carstenr@5af997e4-4c3d-0410-961e-ac0512e437d9>
2010-02-08 10:51:33 +0000

    The io module for saving trajectory as python script did not produce valid code. It seemed just a copy of write_ascii module.
    Bug fixed.

--- ase/io/py.py
+++ ase/io/py.py
@@ -1,21 +1,24 @@
 from ase.atoms import Atoms
 
 
-def write_py(fileobj, images):
+def write_py(fileobj, images, **kwargs):
     if isinstance(fileobj, str):
         fileobj = open(fileobj, 'w')
 
-    fileobj.write('from ase import *\n')
+    fileobj.write('from ase import *\n\n')
     
     if not isinstance(images, (list, tuple)):
         images = [images]
-        fileobj.write('atoms = ')
-    else:
-        fileobj.write('images = [')
+    fileobj.write('images = [\n')
 
-    symbols = images[0].get_chemical_symbols()
-    natoms = len(symbols)
-    for atoms in images:
-        fileobj.write('%d\n\n' % natoms)
-        for s, (x, y, z) in zip(symbols, atoms.get_positions()):
-            fileobj.write('%-2s %22.15f %22.15f %22.15f\n' % (s, x, y, z))
+    for image in images:
+        fileobj.write("    Atoms(symbols='%s',\n"
+                      "          pbc=np.%s,\n"
+                      "          cell=np.array(\n      %s,\n"
+                      "          positions=np.array(\n      %s),\n" % (
+            image.get_chemical_symbols(reduce=True),
+            repr(image.pbc),
+            repr(image.cell)[6:],
+            repr(image.positions)[6:]))
+        
+    fileobj.write(']')
