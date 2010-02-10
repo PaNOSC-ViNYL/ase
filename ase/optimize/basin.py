fdbdd33b60b0f5355e5088c7ef1537c37ac1c195
miwalter <miwalter@5af997e4-4c3d-0410-961e-ac0512e437d9>
2010-02-10 09:02:41 +0000

    the first relaxed conformation is the lowest by default -> write it out

--- ase/optimize/basin.py
+++ ase/optimize/basin.py
@@ -44,6 +44,7 @@ class BasinHopping(Dynamics):
         self.Emin = self.get_energy(self.atoms.get_positions())
         self.rmin = self.atoms.get_positions()
         self.positions = self.atoms.get_positions()
+        self.call_observers()
         self.log(-1, self.Emin, self.Emin)
                 
     def run(self, steps):
