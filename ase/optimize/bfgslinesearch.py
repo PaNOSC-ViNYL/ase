52cd269655c6e42f17998d6de8186c1fccf51aae
tjiang <tjiang@5af997e4-4c3d-0410-961e-ac0512e437d9>
2010-03-29 12:17:29 +0000

    change indentation

--- ase/optimize/bfgslinesearch.py
+++ ase/optimize/bfgslinesearch.py
@@ -8,8 +8,8 @@
 
 import numpy as np
 from numpy import atleast_1d, eye, mgrid, argmin, zeros, shape, empty, \
- squeeze, vectorize, asarray, absolute, sqrt, Inf, asfarray, isinf
-from ase.utils.linesearch_ana import LineSearch
+     squeeze, vectorize, asarray, absolute, sqrt, Inf, asfarray, isinf
+from ase.utils.linesearch import LineSearch
 from ase.optimize import Optimizer
 from numpy import arange
 
@@ -25,115 +25,115 @@ pymax = __builtin__.max
 __version__="0.1"
 
 class BFGSLineSearch(Optimizer):
-def __init__(self, atoms, restart=None, logfile='-', maxstep=.2,
-			 trajectory=None, c1=.2, c2=0.6, alpha=10,stpmax=50.):
-	"""Minimize a function using the BFGS algorithm.
+    def __init__(self, atoms, restart=None, logfile='-', maxstep=.2,
+                 trajectory=None, c1=1e-4, c2=0.9, alpha=10,stpmax=50.):
+        """Minimize a function using the BFGS algorithm.
 
-	Notes:
+        Notes:
 
-		Optimize the function, f, whose gradient is given by fprime
-		using the quasi-Newton method of Broyden, Fletcher, Goldfarb,
-		and Shanno (BFGS) See Wright, and Nocedal 'Numerical
-		Optimization', 1999, pg. 198.
+            Optimize the function, f, whose gradient is given by fprime
+            using the quasi-Newton method of Broyden, Fletcher, Goldfarb,
+            and Shanno (BFGS) See Wright, and Nocedal 'Numerical
+            Optimization', 1999, pg. 198.
 
-	*See Also*:
+        *See Also*:
 
-	  scikits.openopt : SciKit which offers a unified syntax to call
-						this and other solvers.
+          scikits.openopt : SciKit which offers a unified syntax to call
+                            this and other solvers.
 
-	"""
-	self.maxstep = maxstep
-	self.stpmax = stpmax
-	self.alpha = alpha
-	self.H = None
-	self.c1 = c1
-	self.c2 = c2
-	self.force_calls = 0
-	self.function_calls = 0
-	self.r0 = None
-	self.g0 = None
-	self.e0 = None
-	self.load_restart = False
-	self.task = 'START'
-	self.rep_count = 0
-	self.p = None
-	self.alpha_k = None
-	self.no_update = False
+        """
+        self.maxstep = maxstep
+        self.stpmax = stpmax
+        self.alpha = alpha
+        self.H = None
+        self.c1 = c1
+        self.c2 = c2
+        self.force_calls = 0
+        self.function_calls = 0
+        self.r0 = None
+        self.g0 = None
+        self.e0 = None
+        self.load_restart = False
+        self.task = 'START'
+        self.rep_count = 0
+        self.p = None
+        self.alpha_k = None
+        self.no_update = False
 
-	Optimizer.__init__(self, atoms, restart, logfile, trajectory)
+        Optimizer.__init__(self, atoms, restart, logfile, trajectory)
 
-def read(self):
-	self.r0, self.g0, self.e0, self.task, self.H = self.load()
-	self.load_restart = True    
+    def read(self):
+        self.r0, self.g0, self.e0, self.task, self.H = self.load()
+        self.load_restart = True    
 
-def reset(self):
-	print 'reset'
-	self.H = None
-	self.r0 = None
-	self.g0 = None
-	self.e0 = None
-	self.rep_count = 0
-	  
+    def reset(self):
+        print 'reset'
+        self.H = None
+        self.r0 = None
+        self.g0 = None
+        self.e0 = None
+        self.rep_count = 0
+          
 
-def step(self, f):
-	atoms = self.atoms
-	r = atoms.get_positions()
-	r = r.reshape(-1)
-	g = -f.reshape(-1) / self.alpha
-	#g = -f.reshape(-1) 
-	p0 = self.p
-	self.update(r, g, self.r0, self.g0, p0)
-	e = atoms.get_potential_energy() / self.alpha
-	#e = self.func(r)
+    def step(self, f):
+        atoms = self.atoms
+        r = atoms.get_positions()
+        r = r.reshape(-1)
+        g = -f.reshape(-1) / self.alpha
+        #g = -f.reshape(-1) 
+        p0 = self.p
+        self.update(r, g, self.r0, self.g0, p0)
+        e = atoms.get_potential_energy() / self.alpha
+        #e = self.func(r)
 
-	self.p = -np.dot(self.H,g)
-	p_size = np.sqrt((self.p **2).sum())
-	if self.nsteps != 0:
-		p0_size = np.sqrt((p0 **2).sum())
-		delta_p = self.p/p_size + p0/p0_size
-	if p_size <= np.sqrt(len(atoms) * 1e-10):
-		self.p /= (p_size / np.sqrt(len(atoms)*1e-10))
-	fp=[]
-	gp=[]
-	ls = LineSearch()
-	self.alpha_k, fc, gc, e, self.e0, fkp1, self.no_update = \
-	   ls._line_search(self.func, self.fprime, r, self.p, g, e, self.e0,
-					   maxstep=self.maxstep, c1=self.c1,
-					   c2=self.c2, stpmax=self.stpmax)
-	#if alpha_k is None:  # line search failed try different one.
-	#    alpha_k, fc, gc, e, e0, gfkp1 = \
-	#             line_search(self.func, self.fprime,r,p,g,
-	#                         e,self.e0)
-	#if abs(e - self.e0) < 0.000001:
-	#    self.rep_count += 1
-	#else:
-	#    self.rep_count = 0
+        self.p = -np.dot(self.H,g)
+        p_size = np.sqrt((self.p **2).sum())
+        if self.nsteps != 0:
+            p0_size = np.sqrt((p0 **2).sum())
+            delta_p = self.p/p_size + p0/p0_size
+        if p_size <= np.sqrt(len(atoms) * 1e-10):
+            self.p /= (p_size / np.sqrt(len(atoms)*1e-10))
+        fp=[]
+        gp=[]
+        ls = LineSearch()
+        self.alpha_k, fc, gc, e, self.e0, fkp1, self.no_update = \
+           ls._line_search(self.func, self.fprime, r, self.p, g, e, self.e0,
+                           maxstep=self.maxstep, c1=self.c1,
+                           c2=self.c2, stpmax=self.stpmax)
+        #if alpha_k is None:  # line search failed try different one.
+        #    alpha_k, fc, gc, e, e0, gfkp1 = \
+        #             line_search(self.func, self.fprime,r,p,g,
+        #                         e,self.e0)
+        #if abs(e - self.e0) < 0.000001:
+        #    self.rep_count += 1
+        #else:
+        #    self.rep_count = 0
 
-	#if (alpha_k is None) or (self.rep_count >= 3):
-	#    # If the line search fails, reset the Hessian matrix and
-	#    # start a new line search.
-	#    self.reset()
-	#    return
+        #if (alpha_k is None) or (self.rep_count >= 3):
+        #    # If the line search fails, reset the Hessian matrix and
+        #    # start a new line search.
+        #    self.reset()
+        #    return
 
-	dr = self.alpha_k * self.p
-	atoms.set_positions((r+dr).reshape(len(atoms),-1))
-	self.r0 = r
-	self.g0 = g
-	self.dump((self.r0, self.g0, self.e0, self.task, self.H))
+        dr = self.alpha_k * self.p
+        atoms.set_positions((r+dr).reshape(len(atoms),-1))
+        self.r0 = r
+        self.g0 = g
+        self.dump((self.r0, self.g0, self.e0, self.task, self.H))
 
-def update(self, r, g, r0, g0, p0):
-	self.I = eye(len(self.atoms) * 3, dtype=int)
-	if self.H is None:
-		self.H = eye(3 * len(self.atoms))
-		#self.H = eye(3 * len(self.atoms)) / self.alpha
-		return
-	else:
-		dr = r - r0
-		dg = g - g0 
-		if not (self.alpha_k > 0 and abs(np.dot(g,p0))-abs(np.dot(g0,p0)) < 0):
-			return
-		if self.no_update == True:
-			print 'skip update'
+    def update(self, r, g, r0, g0, p0):
+        self.I = eye(len(self.atoms) * 3, dtype=int)
+        if self.H is None:
+            self.H = eye(3 * len(self.atoms))
+            #self.H = eye(3 * len(self.atoms)) / self.alpha
+            return
+        else:
+            dr = r - r0
+            dg = g - g0 
+            if not (self.alpha_k > 0 and abs(np.dot(g,p0))-abs(np.dot(g0,p0)) < 0):
+                return
+            if self.no_update == True:
+                print 'skip update'
                 return
 
             try: # this was handled in numeric, let it remaines for more safety
