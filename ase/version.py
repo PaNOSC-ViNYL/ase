c05061e4627abfcf8101d16d27ffcb810e797f86
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2010-04-23 08:17:29 +0000

    Update version number.  This should have been done after the 3.3 release, but I forgot.

--- ase/version.py
+++ ase/version.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2003  CAMP
 # Please see the accompanying LICENSE file for further information.
 
-version_base = '3.3.1'
+version_base = '3.4.0'
 
 try:
     from ase.svnversion import svnversion
