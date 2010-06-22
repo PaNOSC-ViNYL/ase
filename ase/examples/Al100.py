e93b40e4a6387e1c6de71d8c55e21c4730396558
s032082 <s032082@5af997e4-4c3d-0410-961e-ac0512e437d9>
2010-06-22 16:29:02 +0000

    Fixed wildcard imports and updated very very old code

--- ase/examples/Al100.py
+++ ase/examples/Al100.py
@@ -1,10 +1,10 @@
-from gpaw import *
-from ASE import *
+from ase import Atom, Atoms
+from gpaw import GPAW
 a = 4.0
 b = a / 2**.5
 L = 7.0
-al = ListOfAtoms([Atom('Al')], cell=(b, b, L), periodic=True)
-calc = Calculator(kpts=(4, 4, 1))
-al.SetCalculator(calc)
-al.GetPotentialEnergy()
+al = Atoms([Atom('Al')], cell=(b, b, L), pbc=True)
+calc = GPAW(kpts=(4, 4, 1))
+al.set_calculator(calc)
+al.get_potential_energy()
 calc.write('Al100.gpw', 'all')
