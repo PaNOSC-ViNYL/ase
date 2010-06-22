2fe6877fc259f089934765bdeb1d78c7a360c395
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2010-06-22 19:25:15 +0000

    Fix import.

--- doc/tutorials/spacegroup/spacegroup.py
+++ doc/tutorials/spacegroup/spacegroup.py
@@ -1,6 +1,6 @@
 # creates: spacegroup-*.png 
 
-import ase
+import ase.io
 
 execfile('spacegroup-al.py')
 execfile('spacegroup-mg.py')
@@ -12,17 +12,17 @@ execfile('spacegroup-skutterudite.py')
 
 for name in ['al', 'mg', 'fe', 'diamond', 'nacl', 'rutile', 'skutterudite']:
     atoms = globals()[name]
-    ase.write('spacegroup-%s.pov'%name, 
-              atoms, 
-              transparent=False, 
-              display=False, 
-              run_povray=True,
-              #canvas_width=128,
-              show_unit_cell=2,
-              rotation='10x,-10y', 
-              #celllinewidth=0.02,
-              celllinewidth=0.05,
-              )
+    ase.io.write('spacegroup-%s.pov'%name, 
+                 atoms, 
+                 transparent=False, 
+                 display=False, 
+                 run_povray=True,
+                 #canvas_width=128,
+                 show_unit_cell=2,
+                 rotation='10x,-10y', 
+                 #celllinewidth=0.02,
+                 celllinewidth=0.05,
+                 )
 
 execfile('spacegroup-cosb3.py')
     
