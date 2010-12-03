f4f5c0b22eb2352a1980228475bf39b379154e20
jber <jber@5af997e4-4c3d-0410-961e-ac0512e437d9>
2010-12-03 12:57:50 +0000

    Add a curvature check to the general optimization convergence criteria for usage with minimum mode following methods (The Dimer method).

--- ase/optimize/optimize.py
+++ ase/optimize/optimize.py
@@ -124,6 +124,9 @@ class Optimizer(Dynamics):
         """Did the optimization converge?"""
         if forces is None:
             forces = self.atoms.get_forces()
+        if hasattr(self.atoms, 'get_curvature'):
+            return (forces**2).sum(axis=1).max() < self.fmax**2 and \
+                   self.atoms.get_curvature() < 0.0
         return (forces**2).sum(axis=1).max() < self.fmax**2
 
     def log(self, forces):
