0a57f0f879fa88826c11e1ad83348e0c117d87b3
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2011-03-18 16:00:33 +0000

    Fix bug for non-orthorhombic cell.

--- ase/dft/wannier.py
+++ ase/dft/wannier.py
@@ -70,8 +70,8 @@ def calculate_weights(cell_cc):
     weight_d = w[:3]
     for d in range(3, 6):
         if abs(w[d]) > 1e-5:
-            Gdir_dc = np.concatenate(Gdir_dc, alldirs_dc[d])
-            weight_d = np.concatenate(weight_d, w[d])
+            Gdir_dc = np.concatenate((Gdir_dc, alldirs_dc[d:d + 1]))
+            weight_d = np.concatenate((weight_d, w[d:d + 1]))
     weight_d /= max(abs(weight_d))
     return weight_d, Gdir_dc
 
