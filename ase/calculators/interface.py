302faf986638a6332e4878074b32f6b9c7ea9812
strange <strange@5af997e4-4c3d-0410-961e-ac0512e437d9>
2011-03-24 21:06:00 +0000

    only consider nbands when making initialguess for wannier functions

--- ase/calculators/interface.py
+++ ase/calculators/interface.py
@@ -122,7 +122,7 @@ class DFTCalculator(Calculator):
         return 0.0
 
     def initial_wannier(self, initialwannier, kpointgrid, fixedstates,
-                        edf, spin):
+                        edf, spin, nbands):
         """Initial guess for the shape of wannier functions.
 
         Use initial guess for wannier orbitals to determine rotation
