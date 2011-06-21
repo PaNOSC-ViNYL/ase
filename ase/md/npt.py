3ba8f001ade89ddc2d7f788724c2d095123e76e7
schiotz <schiotz@5af997e4-4c3d-0410-961e-ac0512e437d9>
2011-06-21 13:40:01 +0000

    Add read_from_trajectory class method to NPT dynamics.  Needed to restart calculations.

--- ase/md/npt.py
+++ ase/md/npt.py
@@ -474,7 +474,53 @@ class NPT(MolecularDynamics):
                 'h': self.h,
                 'h_past': self.h_past,
                 'timeelapsed': self.timeelapsed}
-    
+        
+    @classmethod
+    def read_from_trajectory(cls, trajectory, frame=-1, atoms=None):
+        """Read dynamics and atoms from trajectory (Class method).
+        
+        Simultaneously reads the atoms and the dynamics from a BundleTrajectory,
+        including the internal data of the NPT dynamics object (automatically
+        saved when attaching a BundleTrajectory to an NPT object).
+        
+        Arguments::
+        
+        trajectory 
+            The filename or an open BundleTrajectory object.
+        
+        frame (optional)
+            Which frame to read.  Default: the last.
+            
+        atoms (optional, internal use only)
+            Pre-read atoms.  Do not use. 
+        """
+        if isinstance(trajectory, str):
+            if trajectory.endswith('/'):
+                trajectory = trajectory[:-1]
+            if trajectory.endswith('.bundle'):
+                from ase.io.bundletrajectory import BundleTrajectory
+                trajectory = BundleTrajectory(trajectory)
+            else:
+                raise ValueError("Cannot open '%': unsupported file format" % trajectory)
+        # trajectory is now a BundleTrajectory object (or compatible)
+        if atoms is None:
+            atoms = trajectory[frame]
+        init_data = trajectory.read_special('npt_init')
+        frame_data = trajectory.read_special('npt_dynamics', frame)
+        dyn = cls(atoms, timestep=init_data['dt'], 
+                  temperature=init_data['temperature'],
+                  externalstress=init_data['externalstress'],
+                  ttime=init_data['ttime'],
+                  pfactor=init_data['pfactor_given'],
+                  mask=init_data['mask'])
+        dyn.desiredEkin = init_data['desiredEkin']
+        dyn.tfact = init_data['tfact']
+        dyn.pfact = init_data['pfact']
+        dyn.frac_traceless = init_data['frac_traceless']
+        for k, v in frame_data.items():
+            setattr(dyn, k, v)
+        return (dyn, atoms)
+        
     def _getbox(self):
         "Get the computational box."
         return self.atoms.get_cell()
