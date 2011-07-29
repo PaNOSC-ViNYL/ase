9b0e36b7744fec540f1693c70ab8873eb3ca470b
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2011-07-29 12:27:09 +0000

    Remove LGPLv3+ copyright so that it defaults to LGPLv2.1+ as for all other files.

--- ase/calculators/castep.py
+++ ase/calculators/castep.py
@@ -7,20 +7,6 @@ Authors:
     Max Hoffmann, max.hoffmann@ch.tum.de
     Jörg Meyer, joerg.meyer@ch.tum.de
 """
-# Copyright 2011, Max Hoffmann, Jörg Meyer
-
-# The CASTEP-ASE interface is free software: you can redistribute
-# it and/or modify it under the terms of the GNU Lesser General
-# Public License as published by the Free Software Foundation, either
-# version 3 of the License, or (at your option) any later version.
-#
-# The CASTEP-ASE interface is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 __all__ = [
     'Castep',
--- ase/io/castep.py
+++ ase/io/castep.py
@@ -1,30 +1,9 @@
-#!/usr/bin/ipython
 # -*- coding: utf-8 -*-
 """This module defines I/O routines with CASTEP files.
 The key idea is that all function accept or return  atoms objects.
 CASTEP specific parameters will be returned through the <atoms>.calc
 attribute.
 """
-# Copyright 2011, Max Hoffmann, Jörg Meyer
-
-# This file is part of the CASTEP-ASE interface
-
-# The CASTEP-ASE interface is free software: you can redistribute
-# it and/or modify it under the terms of the GNU Lesser General
-# Public License as published by the Free Software Foundation, either
-# version 3 of the License, or (at your option) any later version.
-#
-# The CASTEP-ASE interface is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# The CASTEP-ASE interface is free software: you can redistribute
-# it and/or modify it under the terms of the GNU Lesser General
-# Public License as published by the Free Software Foundation, either
-# version 3 of the License, or (at your option) any later version.
 
 from numpy import sqrt, radians, sin, cos, matrix, array, cross, float32, dot
 import ase
