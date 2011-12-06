c231868f5687bd9df35cf33affd8134d65ca3335
dlandis <dlandis@5af997e4-4c3d-0410-961e-ac0512e437d9>
2011-12-06 10:36:22 +0000

    cmr: db-files now support splice and return an Atoms object even when not all ase_* information is available

--- ase/io/db.py
+++ ase/io/db.py
@@ -1,28 +1,28 @@
 import numpy as np
 from ase.atoms import Atoms
 from ase.calculators.singlepoint import SinglePointCalculator
+from ase.data import atomic_numbers
 
-def read_db(filename, index):
-    import cmr
-    r = cmr.read(filename)
-    if r.is_group():
-       hashes = r.get_member_hashes()
-       hash = hashes[index]
-       d = r.get_xmldata(hash)
-    else:
-       d = r
-    if type(d) == str or len(d.keys("ase")) == 0:
-        raise RuntimeError('db-file: there is no ase readable data available in the specified file!')
-    positions = d.get('ase_positions')
-    numbers = d.get('ase_atomic_numbers')
-    cell = d.get('ase_cell')
-    pbc = d.get('ase_pbc')
-    tags = np.array(d.get('ase_tags'))
-    magmoms = np.array(d.get('ase_magnetic_moments'))
-    energy = d.get('ase_potential_energy')
+def get_atoms(cmr_data):
+    if type(cmr_data)==str:
+        raise RuntimeError('cmr db-file: the specified cmr group file does not contain any images, only references.')
+    positions = cmr_data.get('ase_positions')
+    numbers = cmr_data.get('ase_atomic_numbers')
+    symbols = cmr_data.get('ase_chemical_symbols')
+    cell = cmr_data.get('ase_cell')
+    pbc = cmr_data.get('ase_pbc')
+    tags = np.array(cmr_data.get('ase_tags'))
+    magmoms = np.array(cmr_data.get('ase_magnetic_moments'))
+    energy = cmr_data.get('ase_potential_energy')
 
-    forces = d.get('ase_forces')
+    forces = cmr_data.get('ase_forces')
 
+    
+    if numbers is None and not symbols is None:
+        numbers = [atomic_numbers[x] for x in symbols]
+
+    if numbers is None or positions is None:
+        raise RuntimeError('cmr db-file: there is no or not enough ase data available in the specified db-file.')
     atoms = Atoms(positions=positions,
                   numbers=numbers,
                   cell=cell,
@@ -39,4 +39,18 @@ def read_db(filename, index):
                                            atoms)
 
     return atoms
+
+def read_db(filename, index):
+    import cmr
+    r = cmr.read(filename)
+    if not r.has_key("ase_positions") and r.is_group():
+        hashes = r.get_member_hashes()
+        hashes = hashes[index]
+        if type(hashes)==list:
+            return [get_atoms(r.get_xmldata(hash)) for hash in hashes]
+        return get_atoms(r.get_xmldata(hashes))
+    else:
+        return get_atoms(r)
+    
+    
 
