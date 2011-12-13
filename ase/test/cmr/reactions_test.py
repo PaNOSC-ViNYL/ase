890f62ecb9432997a67664474bdd928c0e3aaee6
dulak <dulak@5af997e4-4c3d-0410-961e-ac0512e437d9>
2011-12-13 15:07:28 +0000

    fix printing order of reactions and add reactions_xsimple.py (extra simple)

--- ase/test/cmr/reactions_test.py
+++ ase/test/cmr/reactions_test.py
@@ -66,10 +66,11 @@ all = reader.find(name_value_list=[('db_calculator', 'group'),
 
 print 'reaction_id, calc, ref, calc - ref'
 # compare with the reference
-for reaction_id in reference:
+for r in reactions:
+    reaction_id = r[-1][1]
     res = all.get('reaction_id', reaction_id)
     if res is None:
-        print "Could not find reaction_id %d in reference"%reaction_id
+        print "Could not find reaction_id %s in reference"%str(reaction_id)
     else:
         calc = res['reaction_energy']
         ref = reference[reaction_id]
