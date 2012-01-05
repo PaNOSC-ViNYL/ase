4285c14bf550db18a1e3618578bb684d364a4999
sihb <sihb@5af997e4-4c3d-0410-961e-ac0512e437d9>
2012-01-05 08:58:11 +0000

    Fixing some import issues.

--- ase/cluster/__init__.py
+++ ase/cluster/__init__.py
@@ -3,7 +3,9 @@
 from ase.cluster.cluster import Cluster
 from ase.cluster.wulff import wulff_construction
 
-from ase.cluster.cubic import SimpleCubic, BodyCenteredCubic, FaceCenteredCubic
+from ase.cluster.cubic import SimpleCubic, BodyCenteredCubic,\
+                              FaceCenteredCubic
+from ase.cluster.octahedron import Octahedron
 from ase.cluster.hexagonal import Hexagonal, HexagonalClosedPacked
 from ase.cluster.icosahedron import Icosahedron
 from ase.cluster.decahedron import Decahedron
