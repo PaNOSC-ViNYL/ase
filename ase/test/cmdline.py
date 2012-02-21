41528298eebde33e474f53b34481196fbaf1177e
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2012-02-21 07:24:23 +0000

    Use better initial guess for H2 bond length to avoid error.

--- ase/test/cmdline.py
+++ ase/test/cmdline.py
@@ -1,10 +1,10 @@
 import numpy as np
 from ase.tasks.main import run
-atoms, task = run('H2 -R 0.01 -F 5,2 --atomize')
+atoms, task = run('H2 --bond-length=0.78 -R 0.01 -F 5,2 --atomize')
 atoms, task = run('H2 H -s')
 results = np.array(task.results['H2'])
 assert abs(results -
-           [1.1589, 0.0882, 0.7789, 869.0545, 5.2611, 5.3493]).max() < 0.001
+           [1.071, 0.000, 0.779, 862.780, 5.349, 5.349]).max() < 0.001
 
 atoms, task = run('bulk Cu -F 5,2')
 atoms, task = run('bulk Cu -s')
