ed2f5cd933761203b062e05cce1d3101c20173cd
s032082 <s032082@5af997e4-4c3d-0410-961e-ac0512e437d9>
2012-04-20 15:31:05 +0000

    Ported symbol explanation for graph help to new i18n translation directory - pop up was left blank after r2457. Spanish translation to the best of my abilities, but could maybe use some attention from a native speaker.

--- ase/gui/languages/da.py
+++ ase/gui/languages/da.py
@@ -23,21 +23,5 @@ translation = {
 og vinkler) ved at vælge atomer med musen (venstre knap) evt. i
 kombination med [ctrl] tasten""",
 
-'Help for plot ...':
-u"""Symboler:
-<c>e</c>:\t\ttotal energi
-<c>epot</c>:\tpotentiel energi
-<c>ekin</c>:\tkinetisk energi
-<c>fmax</c>:\tmaximum kræft
-<c>fave</c>:\tmiddel kræft
-<c>R[n,0-2]</c>:\tposition af atom nummer <c>n</c> 
-<c>d(n<sub>1</sub>,n<sub>1</sub>)</c>:\tafstand mellem to atomer
-<c>i</c>:\t\tnuværende billede nummer
-<c>E[i]</c>:\t\tenergi af billede nummer <c>i</c>
-<c>F[n,0-2]</c>:\tkræft på atom nummer <c>n</c> 
-<c>V[n,0-2]</c>:\thastighed af atom nummer <c>n</c> 
-<c>A[0-2,0-2]</c>:\tEnhedscelle vektorer
-<c>s</c>:\t\tvejlængde""",
-
 'Slice: ': 'Udsnit: '
 }
