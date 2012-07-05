4d2cd6615dbb5fbfc845daf5c5b3070e54050b29
dulak <dulak@5af997e4-4c3d-0410-961e-ac0512e437d9>
2012-07-05 11:50:12 +0000

    eos again: the default ase.utils.eos imports without scipy now. It uses conditional class definition - not elegant, alternative would be to create one EquationOfState class that includes also the long ASE2 code

--- ase/utils/eos.py
+++ ase/utils/eos.py
@@ -1,92 +1,78 @@
 # -*- coding: utf-8 -*-
-from math import sqrt
 
 import numpy as np
 
-from ase.units import kJ
+from ase.utils.eosip3 import EquationOfStateIP3
 
-class EquationOfState:
-    """Fit equation of state for bulk systems.
+try:
+    # ase.utils.eosase2 requires scipy
+    import scipy
+    from ase.utils.eosase2 import EquationOfStateASE2
 
-    The following equation is used::
+    class EquationOfState(EquationOfStateIP3, EquationOfStateASE2):
 
-                          2      3        -1/3
-      E(V) = c + c t + c t  + c t ,  t = V
-              0   1     2      3
+        """Fit equation of state for bulk systems.
 
-    Use::
+        The following equation is used::
 
-       eos = EquationOfState(volumes, energies)
-       v0, e0, B = eos.fit()
-       eos.plot()
+           ip3 (default)
+               A third order inverse polynomial fit
 
-    """
-    def __init__(self, volumes, energies):
-        self.v = np.array(volumes)
-        self.e = np.array(energies)
-        self.v0 = None
+                               2      3        -1/3
+           E(V) = c + c t + c t  + c t ,  t = V
+                   0   1     2      3
 
-    def fit(self):
-        """Calculate volume, energy, and bulk modulus.
+           taylor
+               A third order Taylor series expansion about the minimum volume
 
-        Returns the optimal volume, the minumum energy, and the bulk
-        modulus.  Notice that the ASE units for the bulk modulus is
-        eV/Angstrom^3 - to get the value in GPa, do this::
+           murnaghan
+               PRB 28, 5480 (1983)
 
-          v0, e0, B = eos.fit()
-          print B / kJ * 1.0e24, 'GPa'
-          
+           birch
+               Intermetallic compounds: Principles and Practice,
+               Vol I: Principles. pages 195-210
+
+           birchmurnaghan
+               PRB 70, 224107
+
+           pouriertarantola
+               PRB 70, 224107
+
+           vinet
+               PRB 70, 224107
+
+           antonschmidt
+               Intermetallics 11, 23-32 (2003)
+
+           p3
+               A third order polynomial fit
+
+        Use::
+
+           eos = EquationOfState(volumes, energies, eos='ip3')
+           v0, e0, B = eos.fit()
+           eos.plot()
+
         """
-        
-        fit0 = np.poly1d(np.polyfit(self.v**-(1.0 / 3), self.e, 3))
-        fit1 = np.polyder(fit0, 1)
-        fit2 = np.polyder(fit1, 1)
+        def __init__(self, volumes, energies, eos='ip3'):
+            if eos == 'ip3':
+                EquationOfStateIP3.__init__(self, volumes, energies)
+            else:
+                # old ASE2 implementation
+                EquationOfStateASE2.__init__(self, volumes, energies, eos)
 
-        self.v0 = None
-        for t in np.roots(fit1):
-            if t > 0 and fit2(t) > 0:
-                self.v0 = t**-3
-                break
+        def fit(self):
+            if self.eos_string == 'ip3':
+                return EquationOfStateIP3.fit(self)
+            else:
+                return EquationOfStateASE2.fit(self)
 
-        if self.v0 is None:
-            raise ValueError('No minimum!')
-        
-        self.e0 = fit0(t)
-        self.B = t**5 * fit2(t) / 9
-        self.fit0 = fit0
-        
-        return self.v0, self.e0, self.B
+        def plot(self, filename=None, show=None):
+            if self.eos_string == 'ip3':
+                return EquationOfStateIP3.plot(self, filename, show)
+            else:
+                return EquationOfStateASE2.plot(self, filename, show)
 
-    def plot(self, filename=None, show=None):
-        """Plot fitted energy curve.
-
-        Uses Matplotlib to plot the energy curve.  Use *show=True* to
-        show the figure and *filename='abc.png'* or
-        *filename='abc.eps'* to save the figure to a file."""
-        
-        #import matplotlib.pyplot as plt
-        import pylab as plt
-
-        if self.v0 is None:
-            self.fit()
-            
-        if filename is None and show is None:
-            show = True
-
-        x = 3.95
-        f = plt.figure(figsize=(x * 2.5**0.5, x))
-        f.subplots_adjust(left=0.12, right=0.9, top=0.9, bottom=0.15)
-        plt.plot(self.v, self.e, 'o')
-        x = np.linspace(min(self.v), max(self.v), 100)
-        plt.plot(x, self.fit0(x**-(1.0 / 3)), '-r')
-        plt.xlabel(u'volume [Å^3]')
-        plt.ylabel(u'energy [eV]')
-        plt.title(u'E: %.3f eV, V: %.3f Å^3, B: %.3f GPa' %
-                  (self.e0, self.v0, self.B / kJ * 1.0e24))
-
-        if show:
-            plt.show()
-        if filename is not None:
-            f.savefig(filename)
-
-        return f
+except ImportError:
+    # ase.utils.eosip3 requires only numpy
+    EquationOfState = EquationOfStateIP3
