94cd5f70a09d5ea2394375d85e2cc95928cff3dd
dulak <dulak@5af997e4-4c3d-0410-961e-ac0512e437d9>
2012-07-13 12:30:30 +0000

    print total optimization time in optimizers tests

--- ase/optimize/test/__init__.py
+++ ase/optimize/test/__init__.py
@@ -12,6 +12,9 @@ def get_calculator():
 
 run_test(get_atoms, get_calculator, 'Hydrogen')
 """
+
+import time
+
 import matplotlib
 matplotlib.rcParams['backend']="Agg"
 
@@ -59,6 +62,11 @@ def run_test(get_atoms, get_calculator, name,
 
     plotter = Plotter(name, fmax)
     csvwriter = CSVWriter(name)
+    # write header
+    row = ['Optimizer', 'Optimizer Steps', 'Force evaluations', 'Energy']
+    row.extend(['Time [sec]', 'Note'])
+    format = '%s,%s,%s,%s,%s,%s\n'
+    csvwriter.write(row, format)
     for optimizer in optimizers:
         note = ''
         logname = name + '-' + optimizer
@@ -72,6 +80,7 @@ def run_test(get_atoms, get_calculator, name,
 
         obs = DataObserver(atoms)
         relax.attach(obs)
+        t = time.time()
         try:
             relax.run(fmax = fmax, steps = steps)
             E = atoms.get_potential_energy()
@@ -82,6 +91,7 @@ def run_test(get_atoms, get_calculator, name,
             traceback.print_exc()
             note = 'An exception occurred'
             E = np.nan
+        t = time.time() - t
 
         nsteps = relax.get_number_of_steps()
         if hasattr(relax, 'force_calls'):
@@ -94,7 +104,10 @@ def run_test(get_atoms, get_calculator, name,
                 print '%-15s %-15s %3i %8.3f       %s' % (name, optimizer, nsteps, E, note)
 
         plotter.plot(optimizer, obs.get_E(), obs.get_fmax())
-        csvwriter.write(optimizer, nsteps, E, fc, note)
+        format = '%s,%i,%i,%.5f,%i,%s\n'
+        row = [optimizer, nsteps, fc, E]
+        row.extend([int(t), note])
+        csvwriter.write(row, format)
 
     plotter.save()
     csvwriter.finalize()
@@ -103,7 +116,7 @@ class Plotter:
     def __init__(self, name, fmax):
         self.name = name
         self.fmax = fmax
-        if rank == 0: 
+        if rank == 0:
             self.fig = pl.figure(figsize=[12.0, 9.0])
             self.axes0 = self.fig.add_subplot(2, 1, 1)
             self.axes1 = self.fig.add_subplot(2, 1, 2)
@@ -130,10 +143,8 @@ class CSVWriter:
     def __init__(self, name):
         self.f = paropen(name + '.csv', 'w')
 
-    def write(self, optimizer, nsteps, E, fc, note=''):
-        self.f.write(
-            '%s,%i,%i,%f,%s\n' % (optimizer, nsteps, fc, E, note)
-        )
+    def write(self, row, format):
+        self.f.write(format % tuple(row))
 
     def finalize(self):
         self.f.close()
--- ase/optimize/test/generate_rst.py
+++ ase/optimize/test/generate_rst.py
@@ -17,6 +17,11 @@ This page shows benchmarks of optimizations done with our different optimizers.
 Note that the iteration number (steps) is not the same as the number of force
 evaluations. This is because some of the optimizers uses internal line searches
 or similar.
+
+The most important performance characteristics of an optimizer is the
+total optimization time.
+Different optimizers may perform the same number of steps, but along a different
+path, so the time spent on calculation of energy/forces will be different.
 """
 )
 
