9ad3f794723a3f799c361a8f406f41b9c74d7c24
dulak <dulak@5af997e4-4c3d-0410-961e-ac0512e437d9>
2012-08-08 13:18:47 +0000

    jacapo eos and stress followed by eos

---
+++ ase/test/jacapo/jacapo_seos.py
@@ -0,0 +1,27 @@
+import os
+
+from ase.test.jacapo import installed
+
+assert installed()
+
+from ase import units
+from ase.tasks.main import run
+
+# warning! parameters are not converged - only an illustration!
+
+# just fit EOS
+atoms, task = run('jacapo bulk Li -x bcc -a 3.6 --k-point-density 1.5 -F 5,1.5 -t eos -p symmetry=True')
+atoms, task = run('jacapo bulk Li -x bcc -a 3.6 -t eos -s')
+data = task.data['Li']
+assert abs(data['B'] / units.kJ * 1.0e24 - 8.93) < 0.02
+# minimize stress cell and fit EOS
+f = 'Li-bulk-seos'
+# MDTMP: when run script multiple times and the nc file is already present
+# the results for seos differ!
+# https://listserv.fysik.dtu.dk/pipermail/ase-developers/2012-August/001488.html
+if 0:
+    if os.path.exists(f): os.remove(f)
+atoms, task = run('jacapo bulk Li -x bcc -a 3.6 --k-point-density 1.5 --srelax 0.05 -F 5,1.5 -t seos -p symmetry=True,calculate_stress=True')
+atoms, task = run('jacapo bulk Li -x bcc -a 3.6 -t seos -s')
+data = task.data['Li']
+assert abs(data['B'] / units.kJ * 1.0e24 - 14.73) < 0.02
