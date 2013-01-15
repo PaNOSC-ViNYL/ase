d36afda8517ef173adbc736e7f1a55210e0bffa7
jesperf <jesperf@5af997e4-4c3d-0410-961e-ac0512e437d9>
2013-01-15 21:31:41 +0000

    Applied solution suggested by Jozef Vesely to byg in in Spacegroup.equivalent_sites().

--- ase/lattice/spacegroup/spacegroup.py
+++ ase/lattice/spacegroup/spacegroup.py
@@ -356,7 +356,6 @@ class Spacegroup(object):
         """
         kinds = []
         sites = []
-        symprec2 = symprec**2
         scaled = np.array(scaled_positions, ndmin=2)
         for kind, pos in enumerate(scaled):
             for rot, trans in self.get_symop():
@@ -366,9 +365,8 @@ class Spacegroup(object):
                     kinds.append(kind)
                     continue
                 t = site - sites
-                t %= 1.0
-                t %= 1.0
-                mask = np.sum(t*t, 1) < symprec2
+                mask = np.all((abs(t) < symprec) | 
+                              (abs(abs(t) - 1.0) < symprec), axis=1)
                 if np.any(mask):
                     ind = np.argwhere(mask)[0][0]
                     if kinds[ind] == kind:
