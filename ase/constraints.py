a87f19514e34b2937ded7e2dd910de344385c8c5
anpet <anpet@5af997e4-4c3d-0410-961e-ac0512e437d9>
2013-03-30 15:17:23 +0000

    Replace BondSpring with more generic Hookean class. Add deprecation error for BondSpring.

--- ase/constraints.py
+++ ase/constraints.py
@@ -738,64 +738,121 @@ class FixInternals(FixConstraint):
         def __repr__(self):
             return 'FixDihedral(%s, %f)' % (tuple(self.indices), self.angle)
 
-class BondSpring(FixConstraint):
-    """Forces two atoms to stay close together by applying no force if they
-    are below threshhold_length, and applying a Hookian force when the
-    distance between them exceeds the thresshhold_length.
+
+class Hookean(FixConstraint):
+    """Forces two atoms to stay close together by applying no force if
+    they are below a threshold length, rt, and applying a Hookean
+    restorative force when the distance between them exceeds rt. Can also be
+    used to tether an atom to a fixed point in space or to a distance above
+    a plane.
     
-    a1, a2 : indices of atoms 1 and 2
-    a2 can alternately be a position in space to tether a1 to
-    threshhold_length (float) : the length below which there is no force
-    springconstant (integer) : Hook's law constant to apply when distance
-    between the two atoms exceeds threshhold_length, dimensions of 
-    (force / length)
+    a1 : index of atom 1
+    a2 : one of three options:
+        1) index of atom 2
+        2) a fixed point in cartesian space to which to tether a1
+        3) a plane given as (A, B, C, D) in A x + B y + C z + D = 0.
+    k (float) : Hooke's law (spring) constant to apply when distance
+        exceeds threshold_length. 
+    rt (float) : the threshold length below which there is no force. The
+        length is 1) between two atoms, 2) between atom and point.
+        This argument is not supplied in case 3.
+
+    If a plane is specified the Hooke's law force is applied if the atom
+    is on the normal side of the plane. For instance, the plane with
+    (A, B, C, D) = (0, 0, 1, -7) defines a plane in the xy plane with a z
+    intercept of +7 and a normal vector pointing in the +z direction.
+    If the atom has z > 7, then a downward force would be applied of
+    k * (atom.z - 7). The same plane with the normal vector pointing in
+    the -z direction would be given by (A, B, C, D) = (0, 0, -1, 7).
     """
-    def __init__(self, a1, a2, threshhold_length, springconstant):
+    def __init__(self, a1, a2, k, rt=None):
         if type(a2) == int:
-            self._type = 2 # two atoms tethered together
+            self._type = 'two atoms'
             self.indices = [a1, a2]
-        else:
-            self._type = 1 # one atom tethered to a point in space
-            self.index = a1
+        elif len(a2) == 3:
+            self._type = 'point'
             self.origin = np.array(a2)
-        self.threshhold = threshhold_length
-        self.spring = springconstant
+        elif len(a2) == 4:
+            self._type = 'plane'
+            self.index = a1
+            self.plane = a2
+        else:
+            raise RuntimeError('Unknown type for a2')
+        self.threshold = rt
+        self.spring = k
 
     def adjust_positions(self, oldpositions, newpositions):
         pass
 
     def adjust_forces(self, positions, forces):
-        if self._type == 2:
+        if self._type == 'plane':
+            A, B, C, D = self.plane
+            x, y, z = positions[self.index]
+            d = ((A * x + B * y + C * z + D) / 
+                 np.sqrt(A**2 + B**2 + C**2))
+            if d < 0:
+                return
+            magnitude = self.spring * d
+            direction = - np.array((A, B, C)) / np.linalg.norm((A, B, C))
+            forces[self.index] += direction * magnitude
+            return
+        if self._type == 'two atoms':
             p1, p2 = positions[self.indices]
-        else:
+        elif self._type == 'point':
             p1 = positions[self.index]
             p2 = self.origin
         displace = p2 - p1
         bondlength = np.linalg.norm(displace)
-        if bondlength > self.threshhold:
-            magnitude = self.spring * (bondlength - self.threshhold)
+        if (bondlength > self.threshold) and (self.threshold > 0):
+            magnitude = self.spring * (bondlength - self.threshold)
             direction = displace / np.linalg.norm(displace)
-            if self._type == 2:
+            if self._type == 'two atoms':
                 forces[self.indices[0]] += direction * magnitude
                 forces[self.indices[1]] -= direction * magnitude
             else:
                 forces[self.index] += direction * magnitude
 
-    def __repr__(self):
-        if self._type == 2:
-            return 'BondSpring(%d, %d)' % tuple(self.indices)
+    def adjust_potential_energy(self, positions, forces):
+        """Returns the difference to the potential energy due to an active
+        constraint."""
+        if self._type == 'plane':
+            A, B, C, D = self.plane
+            x, y, z = positions[self.index]
+            d = ((A * x + B * y + C * z + D) / 
+                 np.sqrt(A**2 + B**2 + C**2))
+            if d > 0:
+                return 0.5 * self.spring * d**2
+            else:
+                return 0.
         else:
-            return 'BondSpring(%d) to cartesian' % self.index
+            raise NotImplementedError('Adjust potential energy only '
+                                      'implemented for plane.')
+
+    def __repr__(self):
+        if self._type == 'two atoms':
+            return 'Hookean(%d, %d)' % tuple(self.indices)
+        elif self._type == 'point':
+            return 'Hookean(%d) to cartesian' % self.index
+        else:
+            return 'Hookean(%d) to plane' % self.index
 
     def copy(self):
-        if self._type == 2:
-            return BondSpring(a1=self.indices[0], a2=self.indices[1],
-                              threshhold_length=self.threshhold,
-                              springconstant=self.spring)
+        if self._type == 'two atoms':
+            return Hookean(a1=self.indices[0], a2=self.indices[1],
+                           rt=self.threshold, k=self.spring)
+        elif self._type == 'point':
+            return Hookean(a1=self.index, a2=self.origin,
+                           rt=self.threshold, k=self.spring)
         else:
-            return BondSpring(a1=self.index, a2=self.origin,
-                              threshhold_length=self.threshhold,
-                              springconstant=self.spring)
+            return Hookean(a1=self.index, a2=self.plane,
+                           k=self.spring)
+
+
+class BondSpring(FixConstraint):
+    """Deprecated in favor of Hookean constraint."""
+    def __init__(self, a1, a2, threshhold_length, springconstant):
+        raise RuntimeError('The BondSpring constraint has been deprecated.'
+                           ' Use Hookean instead.')
 
 
 class Filter:
