e24c51aa1903ce6b2c74d89392116a249ff8de7f
dulak <dulak@5af997e4-4c3d-0410-961e-ac0512e437d9>
2013-04-24 06:25:32 +0000

    new style elk interface

--- ase/test/elk/elk_cmdline.py
+++ ase/test/elk/elk_cmdline.py
@@ -1,3 +1,6 @@
 from ase.cli import run
 
-run('-x fcc -a 4.04 Al run -c elk -p kpts=3.0,xc=PBE,rgkmax=5.0,tforce=True')
+# warning! parameters are not converged - only an illustration!
+atoms = run('-x fcc -a 4.04 Al run -c elk -p ' +
+            'tasks=0,kpts=1.5,rgkmax=5.0,tforce=True,' +
+            'smearing=(fermi-dirac,0.05)')
