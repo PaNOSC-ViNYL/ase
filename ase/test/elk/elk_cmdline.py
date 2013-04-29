ee8a93c035e66d0790811387c41010c330c9e148
dulak <dulak@5af997e4-4c3d-0410-961e-ac0512e437d9>
2013-04-29 15:36:03 +0000

    not ready for cli elk tests in trunk yet

--- ase/test/elk/elk_cmdline.py
+++
@@ -1,6 +0,0 @@
-from ase.cli import run
-
-# warning! parameters are not converged - only an illustration!
-atoms = run('-x fcc -a 4.04 Al run -c elk -p ' +
-            'tasks=0,kpts=1.5,rgkmax=5.0,tforce=True,' +
-            'smearing=(fermi-dirac,0.05)')
