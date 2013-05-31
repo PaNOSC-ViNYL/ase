d1b8c7cd7507f75ced8bc90173e58b15bba82905
dulak <dulak@5af997e4-4c3d-0410-961e-ac0512e437d9>
2013-05-31 11:42:05 +0000

    this option not ready yet

--- ase/test/aims/aims_cmdline.py
+++ ase/test/aims/aims_cmdline.py
@@ -1,6 +1,6 @@
 from ase.cli import run
 
 # warning! parameters are not converged - only an illustration!
-atoms = run('-x bcc -a 3.6 Li optimize -c aims -f 0.3 --unit-cell-filter -p ' +
+atoms = run('-x bcc -a 3.6 Li optimize -c aims -s 0.3 -p ' +
             'kpts=1.5,xc=LDA,sc_accuracy_eev=5.e-2,relativistic=none,' +
             'compute_analytical_stress=True,sc_accuracy_forces=5.e-2')
