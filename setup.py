2d451b36f497471ef805241b89121df5486b8520
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2013-06-21 14:03:39 +0000

    More tools

--- setup.py
+++ setup.py
@@ -141,7 +141,8 @@ if svnversion and os.name not in ['ce', 'nt']: # MSI accepts only version X.X.X
 else:
     version = version_base
 
-scripts = ['tools/ag', 'tools/ase-cli', 'tools/ase-db', 'tools/ase-info']
+scripts = ['tools/ase-gui', 'tools/ase-cli', 'tools/ase-db', 'tools/ase-info',
+           'tools/ag']
 # provide bat executables in the tarball and always for Win
 if 'sdist' in sys.argv or os.name in ['ce', 'nt']:
     for s in scripts[:]:
