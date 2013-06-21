ff9c02367dc068203899b9691151bcd2702c22b6
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2013-06-21 13:40:45 +0000

    Convert foldtrajectory to Python script

--- ase/io/foldtrajectory.py
+++ ase/io/foldtrajectory.py
@@ -1,27 +1,27 @@
-#!/usr/bin/env python
 """foldtrajectory - folds atoms into the periodic computational box.
 
 Usage:
-    foldtrajectory infile.traj outfile.traj
+    python -m ase.io.foldtrajectory infile.traj outfile.traj
     
 In molecular dynamics simulations with periodic boundary conditions,
 atoms sometimes move out of one side of the computational box and in
-through the other.  Such atoms have coordinates outside the box.  
+through the other.  Such atoms have coordinates outside the box.
 This facilitates analysis of e.g. diffusion, but can be problematic
-when plotting.  This script reads through a trajectory file, and 
+when plotting.  This script reads through a trajectory file, and
 write a new one where all atoms are mapped into the computational box.
-If there are axes with free boundary conditions, the corresponding 
+If there are axes with free boundary conditions, the corresponding
 coordinate is left unchanged.
 
 SIDE EFFECT: All energies, forces and stresses are removed (yes, this
 can be considered as a bug!)
 """
 
-from ase.io.trajectory import PickleTrajectory
+from __future__ import print_function
 import sys
+from ase.io.trajectory import PickleTrajectory
 
 if len(sys.argv) != 3:
-    print __doc__
+    print(__doc__)
     sys.exit(1)
     
 infile = PickleTrajectory(sys.argv[1])
@@ -29,9 +29,9 @@ outfile = None
 
 for atoms in infile:
     atoms.set_scaled_positions(atoms.get_scaled_positions())
-    atoms.set_calculator(None) #Or the singlepointcalculator fails!
+    atoms.set_calculator(None)  # or the singlepointcalculator fails!
     if outfile is None:
-        outfile = PickleTrajectory(sys.argv[2], "w")
+        outfile = PickleTrajectory(sys.argv[2], 'w')
     outfile.write(atoms)
         
 outfile.close()
