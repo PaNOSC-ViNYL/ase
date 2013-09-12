5e3f7bf5c888d124ea321052d8d74a09696b41e9
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2013-09-12 12:20:45 +0000

    Patch from Lasse Vilhelmsen.  Faster Lennard-Jones potential

--- ase/calculators/lj.py
+++ ase/calculators/lj.py
@@ -14,18 +14,24 @@ class LennardJones(Calculator):
     def calculate(self, atoms, properties, changes):
         epsilon = self.parameters.epsilon
         sigma = self.parameters.sigma
+        sigma2 = sigma**2
+        
         positions = atoms.get_positions()
         energy = 0.0
         forces = np.zeros((len(atoms), 3))
-        for i1, p1 in enumerate(positions):
-            for i2, p2 in enumerate(positions[:i1]):
-                diff = p2 - p1
-                d2 = np.dot(diff, diff)
-                c6 = (sigma**2 / d2)**3
-                c12 = c6**2
-                energy += 4 * epsilon * (c12 - c6)
-                F = 24 * epsilon * (2 * c12 - c6) / d2 * diff
-                forces[i1] -= F
-                forces[i2] += F
+
+        for i in range(len(positions) - 1):
+            dist = positions[i, :] - positions[i + 1:, :]
+            d2 = (dist**2).sum(axis=1)
+            c6 = (sigma2 / d2) ** 3
+            c12 = c6 ** 2
+
+            energy += sum(4 * epsilon * (c12 - c6))
+            ljf = (24 * epsilon * (2 * c12 - c6) / d2)
+            ljf = ljf.reshape((len(d2), 1)) * dist
+            
+            forces[i + 1:, :] -= ljf
+            forces[i, :] += ljf.sum(axis=0)
+
         self.results['energy'] = energy
         self.results['forces'] = forces
