fdc1f9969a52301df275f1b54913321fd087fa5f
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2013-09-27 11:35:16 +0000

    typo

--- ase/test/elk/elk_cmdline.py
+++ ase/test/elk/elk_cmdline.py
@@ -4,4 +4,4 @@ from ase.test import cli, require
 require('elk')
 cli("""ase-build -x fcc -a 4.04 Al | \
 ase-run elk -p \
-tasks=0,kpts=1.5,rgkmax=5.0,tforce=True,smearing=(fermi-dirac,0.05)""")
+"tasks=0,kpts=1.5,rgkmax=5.0,tforce=True,smearing=(fermi-dirac,0.05)" """)
