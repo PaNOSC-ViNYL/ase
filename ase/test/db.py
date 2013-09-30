767218e1d42c7ee61074805e2da2069092f973b8
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2013-09-30 10:26:57 +0000

    An earlier test was also using x.json

--- ase/test/db.py
+++ ase/test/db.py
@@ -2,17 +2,17 @@ from ase.test import cli
 from ase.db import connect
 
 cmd = """
-ase-build H | ase-run emt -d x.json &&
-ase-build H2O | ase-run emt -d x.json &&
-ase-build O2 | ase-run emt -d x.json &&
-ase-build H2 | ase-run emt -f 0.02 -d x.json &&
-ase-build O2 | ase-run emt -f 0.02 -d x.json &&
-ase-build -x fcc Cu | ase-run emt -E 5 -d x.json &&
-ase-db x.json id=H --delete --yes &&
-ase-db x.json "H>0" -k hydro"""
+ase-build H | ase-run emt -d y.json &&
+ase-build H2O | ase-run emt -d y.json &&
+ase-build O2 | ase-run emt -d y.json &&
+ase-build H2 | ase-run emt -f 0.02 -d y.json &&
+ase-build O2 | ase-run emt -f 0.02 -d y.json &&
+ase-build -x fcc Cu | ase-run emt -E 5 -d y.json &&
+ase-db y.json id=H --delete --yes &&
+ase-db y.json "H>0" -k hydro"""
 
-for name in ['x.json', 'x.db']:#, 'postgres://localhost']:
-    cli(cmd.replace('x.json', name))
+for name in ['y.json', 'y.db']:#, 'postgres://localhost']:
+    cli(cmd.replace('y.json', name))
     con = connect(name)
     assert len(list(con.select())) == 4
     assert len(list(con.select('hydro'))) == 2
