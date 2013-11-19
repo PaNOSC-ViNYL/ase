38acbca494c7ea02699a488dc36f49f4f4ab2e59
schiotz <schiotz@5af997e4-4c3d-0410-961e-ac0512e437d9>
2013-11-19 15:01:10 +0000

    Fix indentation and error message.

--- ase/cli/info.py
+++ ase/cli/info.py
@@ -8,18 +8,18 @@ from ase.io.bundletrajectory import print_bundletrajectory_info
 description = 'Print summary of information from trajectory files.'
 
 def main():
-   p = OptionParser(usage='%prog file.traj [file2.traj ...]',
-                    description=description)
+    p = OptionParser(usage='%prog file.traj [file2.traj ...]',
+                     description=description)
 
-   opts, args = p.parse_args()
+    opts, args = p.parse_args()
 
-   if len(args) == 0:
-      p.error()
+    if len(args) == 0:
+        p.error('Usage: ase.info file.traj [file2.traj ...]')
 
-   for f in args:
-      if os.path.isfile(f):
-         print_trajectory_info(f)
-      elif os.path.isdir(f):
-         print_bundletrajectory_info(f)
-      else:
-         p.error('%s is neither a file nor a directory!' % f)
+    for f in args:
+        if os.path.isfile(f):
+            print_trajectory_info(f)
+        elif os.path.isdir(f):
+            print_bundletrajectory_info(f)
+        else:
+            p.error('%s is neither a file nor a directory!' % f)
