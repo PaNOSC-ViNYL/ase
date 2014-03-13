2dfc3fa9536d0ed81ff602bfedb95749a9ee4982
jesswe <jesswe@5af997e4-4c3d-0410-961e-ac0512e437d9>
2014-03-13 15:37:00 +0000

    Allow for ghost atoms in FHI-AIMS. Useful for BSSE corrections.

--- ase/calculators/aims.py
+++ ase/calculators/aims.py
@@ -158,7 +158,7 @@ class Aims(FileIOCalculator):
             self.reset()
         return changed_parameters
 
-    def write_input(self, atoms, properties=None, system_changes=None):
+    def write_input(self, atoms, properties=None, system_changes=None, ghosts=None):
         FileIOCalculator.write_input(self, atoms, properties, system_changes)
 
         have_lattice_vectors = atoms.pbc.any()
@@ -168,7 +168,7 @@ class Aims(FileIOCalculator):
             raise RuntimeError('Found lattice vectors but no k-grid!')
         if not have_lattice_vectors and have_k_grid:
             raise RuntimeError('Found k-grid but no lattice vectors!')
-        write_aims(os.path.join(self.directory, 'geometry.in'), atoms)
+        write_aims(os.path.join(self.directory, 'geometry.in'), atoms, ghosts)
         self.write_control(atoms, os.path.join(self.directory, 'control.in'))
         self.write_species(atoms, os.path.join(self.directory, 'control.in'))
         self.parameters.write(os.path.join(self.directory, 'parameters.ase'))
