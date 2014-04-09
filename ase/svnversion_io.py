a6a9bf984f99131d98480e682ef3de0763caf060
askhl <askhl@5af997e4-4c3d-0410-961e-ac0512e437d9>
2014-04-09 19:37:18 +0000

    fix bug due to hardcoded strings exported/Unversioned which do not cover all cases.  Checking whether first char is a digit is hopefully reliable on all systems.

--- ase/svnversion_io.py
+++ ase/svnversion_io.py
@@ -34,8 +34,7 @@ def get_svnversion_from_svn(dir):
     cmd = popen3('svnversion -n '+dir)[1] # assert we are in the project dir
     output = cmd.read().strip()
     cmd.close()
-    if output.startswith('exported') or output.startswith('Unversioned'):
-        # we build from exported source (e.g. rpmbuild)
+    if not output[0].isdigit():
         output = None
     return output
 
