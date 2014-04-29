cc988fdb391a4fcbe573397a21591271e03d61b7
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2014-04-29 11:32:35 +0000

    More work on cli and web-interface

--- ase/db/web.py
+++ ase/db/web.py
@@ -6,6 +6,7 @@ from urllib import unquote
 from wsgiref.simple_server import make_server
 
 from ase.io.png import write_png
+from ase.db.summary import Summary
 
 from jinja2 import Environment, PackageLoader
 
@@ -13,13 +14,9 @@ from jinja2 import Environment, PackageLoader
 def D(*a, **k):
     print(*a, file=sys.stderr, **k)
 
-
-D('GO')
-
-
-def run(rows=None, summary=None):
-    app = App(rows)
-
+    
+def run(table):
+    app = App(table)
     httpd = make_server('', 8000, app)
     print('Serving HTTP on port 8000...')
     webbrowser.open('http://localhost:8000', new=1)
@@ -27,45 +24,67 @@ def run(rows=None, summary=None):
 
     
 class App:
-    def __init__(self, rows):
-        self.rows = rows
+    def __init__(self, table):
+        self.table = table
         env = Environment(loader=PackageLoader('ase.db', '.'))
-        self.template = env.get_template('table.html')
+        self.template1 = env.get_template('table.html')
+        self.template2 = env.get_template('summary.html')
         
-    def __call__(self, env, s):
+    def __call__(self, env, start_response):
         q = env['QUERY_STRING']
         path = env['PATH_INFO']
-        D(q,path)
         
-        if path == '/style.css':
-            s('200 OK', [('Content-type', 'text/css')])
-            D(__file__)
-            with open(__file__.rsplit('/', 1)[0] + '/style.css') as fd:
-                return [fd.read()]
-            
-        if path.endswith('.png'):
-            s('200 OK', [('Content-type', 'image/png')])
-            name = path[1:]
-            if not os.path.isfile(name):
-                id = int(name[:-4])
-                a = self.rows.connection.get_atoms(id)
-                write_png(name, a, show_unit_cell=1)
-            return [open(name, 'rb').read()]
-            
-        s('200 OK', [('Content-type', 'text/html')])
         args = {}
         if q:
             for k, v in (x.split('=') for x in q.split('&')):
                 if v.isdigit():
                     v = int(v)
+                else:
+                    v = unquote(v)
                 args[k] = v
+                
+        if path == '/style.css':
+            start_response('200 OK', [('Content-type', 'text/css')])
+            return self.css()
+    
+        if path.endswith('.png'):
+            start_response('200 OK', [('Content-type', 'image/png')])
+            return self.png(path[1:])
+            
+        if path != '/':
+            start_response('200 OK', [('Content-type', 'text/html')])
+            return self.summary(path[1:])
+            
+        start_response('200 OK', [('Content-type', 'text/html')])
+        
         if 'query' in args:
-            query = args['query']
-            if isinstance(query, str):
-                query = unquote(query)
-            self.rows.search(query)
-        elif args:
-            getattr(self.rows, args.pop('m'))(**args)
-        self.rows.format()
-        txt = self.template.render(q=self.rows)
+            self.table.search(args['query'])
+        elif 'm' in args:
+            method = getattr(self.table, args.pop('m'))
+            method(**args)
+            
+        self.table.format('html')
+        txt = self.template1.render(t=self.table)
         return [txt.encode()]
+        
+    def css(self):
+        with open(__file__.rsplit('/', 1)[0] + '/style.css') as fd:
+            return [fd.read()]
+            
+    def png(self, name):
+        if not os.path.isfile(name):
+            id = int(name[:-4])
+            a = self.table.connection.get_atoms(id)
+            write_png(name, a, show_unit_cell=1)
+        return [open(name, 'rb').read()]
+            
+    def summary(self, name):
+        s = Summary(self.table.connection.get(int(name)), 'html')
+        txt = self.template2.render(s=s)
+        return [txt.encode()]
+
+        #s('200 OK', [('Content-Disposition',
+        #              'attachment; filename="myfile.json"')])
+        #return ['{}'.encode()]
+            
+        
