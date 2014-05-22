99f4989ef564e70dbc85ad84aa102ff61b1f95d4
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2014-05-22 09:42:10 +0000

    Test magmom ...

--- ase/test/db.py
+++ ase/test/db.py
@@ -9,14 +9,15 @@ ase-build O2 | ase-run emt -d y.json &&
 ase-build H2 | ase-run emt -f 0.02 -d y.json &&
 ase-build O2 | ase-run emt -f 0.02 -d y.json &&
 ase-build -x fcc Cu | ase-run emt -E 5 -d y.json &&
-ase-db y.json natoms=1,H=1 --delete --yes &&
+ase-db y.json natoms=1,Cu=1 --delete --yes &&
 ase-db y.json "H>0" -k hydro"""
 
-for name in ['y.json', 'y.db']:#, 'postgres://localhost']:
+for name in ['y.json', 'y.db']:  #, 'postgres://localhost']:
     cli(cmd.replace('y.json', name))
     con = connect(name)
     assert len(list(con.select())) == 5
-    assert len(list(con.select('hydro'))) == 2
+    assert len(list(con.select('hydro'))) == 3
+    assert con.get_atoms(H=1)[0].magmom == 1
 
 id = con.reserve(abc=7)
 assert con[id].abc == 7
