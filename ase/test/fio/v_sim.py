62e2f1eab90155e14368ccdebc4fcd7ab89f1fe0
dulak <dulak@5af997e4-4c3d-0410-961e-ac0512e437d9>
2014-07-06 15:43:40 +0000

    trying to catch [Errno 104] Connection reset by peer

--- ase/test/fio/v_sim.py
+++ ase/test/fio/v_sim.py
@@ -1,5 +1,6 @@
 import urllib2
 import urllib
+from socket import error as SocketError
 
 from ase.test import NotAvailable
 
@@ -9,7 +10,7 @@ src = 'http://inac.cea.fr/L_Sim/V_Sim/files/' + dest
 try:
     e = urllib2.urlopen(src)
     urllib.urlretrieve(src, filename=dest)
-except urllib2.URLError:
+except (urllib2.URLError, SocketError):
     raise NotAvailable('Retrieval of ' + src + ' failed')
 
 from ase.io import read
