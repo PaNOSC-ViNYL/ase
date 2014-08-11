e7202436cc281fec21c29ccae5c85a98fb37df7f
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2014-08-11 12:06:05 +0000

    Calling ase.visualize.mlab.main() will exit the interpreter - do this instead

--- doc/ase/visualize/mlab_options.py
+++ doc/ase/visualize/mlab_options.py
@@ -1,11 +1,4 @@
 # creates: mlab_options.txt
-import sys
-from ase.visualize.mlab import main
-fd = open('mlab_options.txt', 'w')
-try:
-    sys.stdout, old = fd, sys.stdout
-    main(['-h'])
-    fd.close()
-finally:
-    sys.stdout = old
-
+import subprocess
+subprocess.check_call('python -m ase.visualize.mlab -h > mlab_options.txt',
+                      shell=True)
