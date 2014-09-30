629057693d15afe6fb4ad304d0460de8965bd787
mohpa <mohpa@5af997e4-4c3d-0410-961e-ac0512e437d9>
2014-09-30 10:49:30 +0000

    more deprecated keywords/functions removed

--- ase/calculators/jacapo/jacapo.py
+++ ase/calculators/jacapo/jacapo.py
@@ -297,11 +297,6 @@ class Jacapo:
 
         if len(kwargs) > 0:
 
-            if 'stress' in kwargs:
-                raise DacapoInput, '''\
-                stress keyword is deprecated.
-                you must use calculate_stress instead'''
-
             #make sure to set calculator on atoms if it was in kwargs
             #and do this first, since some parameters need info from atoms
             if 'atoms' in kwargs:
