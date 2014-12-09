fe5e14997f8f2086f246fc77d5d9ae673889193f
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2014-12-09 15:50:24 +0000

    Fix PostgreSQL: convert long to int

--- ase/db/postgresql.py
+++ ase/db/postgresql.py
@@ -48,7 +48,7 @@ class PostgreSQLDatabase(SQLite3Database):
     def get_last_id(self, cur):
         cur.execute('select last_value from systems_id_seq')
         id = cur.fetchone()[0]
-        return id
+        return int(id)
 
 
 def reset():
