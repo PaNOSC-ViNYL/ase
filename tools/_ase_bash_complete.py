3121884d99ba0a59d512c225ad212f6f31d70162
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2015-01-15 09:11:00 +0000

    Improved bash completion

--- tools/_ase_bash_complete.py
+++ tools/_ase_bash_complete.py
@@ -3,7 +3,8 @@
 
 Put this in your .bashrc::
     
-    complete -C _ase_bash_complete.py ase-db ase-run ase-build ase-info ase-gui
+    complete -o default -C _ase_bash_complete.py ase-db ase-run \
+    ase-build ase-info ase-gui
 """
 
 import os
@@ -18,20 +19,24 @@ point = int(os.environ['COMP_POINT'])
 def options(short, long):
     return ['-' + s for s in short] + ['--' + l for l in long.split()]
 
+
+def match(word, *suffixes):
+    return [w for w in glob(word + '*')
+            if any(w.endswith(suffix) for suffix in suffixes)]
     
+    
+words = []
+
 if command == 'ase-db':
     if word[:1] == '-':
         words = options(
             'hvqnliakycspwL',
             'help verbose quiet count long insert-into add-from-file '
             'add-key-value-pairs limit offset delete '
-            'delete-keys yes columns sort cut  python csv '
+            'delete-keys yes columns sort cut python csv '
             'open-web-browser')
     elif previous == 'ase-db':
-        words = [w for w in glob(word + '*')
-                 if w.endswith('.db') or w.endswith('.json')]
-    else:
-        words = glob(word + '*')
+        words = match(word, '.db', '.json')
 elif command == 'ase-run':
     if word[:1] == '-':
         words = options(
@@ -39,9 +44,7 @@ elif command == 'ase-run':
             'help tag parameter database skip properties maximum-force '
             'constrain-tags maximum-stress equation-of-state modify after')
     elif previous == 'ase-run':
-        words = 'emt nwchem'.split()
-    else:
-        words = glob(word + '*')
+        from ase.calculators.calculator import names as words
 elif command == 'ase-build':
     if previous in ['-x', '--crystal-structure']:
         words = ['sc', 'fcc', 'bcc', 'hcp', 'diamond', 'zincblende',
@@ -51,20 +54,16 @@ elif command == 'ase-build':
             'hMvxarg',
             'help magnetic-moment modify vacuum unit-cell bond-length '
             'crystal-structure lattice-constant orthorhombic cubic repeat gui')
-    else:
-        words = glob(word + '*')
 elif command == 'ase-info':
     if word[:1] == '-':
         words = options('h', 'help')
     else:
-        words = [w for w in glob(word + '*') if w.endswith('.traj')]
+        words = match(word, '.traj')
 else:  # ase-gui
     if word[:1] == '-':
         words = options(
             'hnurRogtbs', 'help image-number show-unit-cell repeat '
             'rotations output graph terminal aneb interpolate bonds scale')
-    else:
-        words = glob(word + '*')
         
 for w in words:
     if w.startswith(word):
