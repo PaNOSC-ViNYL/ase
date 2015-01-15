b5f6c29f5c9e1547a3d1afb1a722e4a3e5d9eda1
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2015-01-15 11:53:36 +0000

    Document Timer class and remove old unused functions.

--- ase/utils/timing.py
+++ ase/utils/timing.py
@@ -3,64 +3,10 @@ from __future__ import print_function
 # Copyright (C) 2003  CAMP
 # Please see the accompanying LICENSE file for further information.
 
-"""A replacement for the ``time.clock()`` function.
-
-From the clock man page::
-
-       Note that the time can wrap around. On a 32bit system
-       where CLOCKS_PER_SEC equals 1000000 this function will
-       return the same value approximately every 72 minutes.
-
-The ``clock()`` function defined below tries to fix this problem.
-However, if the ``clock()`` function is not called often enough (more
-than 72 minutes between two calls), then there is no way of knowing
-how many times the ``time.clock()`` function has wrapped arround! - in
-this case a huge number is returned (1.0e100).  This problem can be
-avoided by calling the ``update()`` function at intervals smaller than
-72 minutes."""
 
 import sys
 import time
-import math
 import functools
-
-import numpy as np
-
-wrap = 1e-6 * 2**32
-
-# Global variables:
-c0 = time.clock()
-t0 = time.time()
-cputime = 0.0
-trouble = False
-
-
-def clock():
-    """clock() -> floating point number
-
-    Return the CPU time in seconds since the start of the process."""
-
-    update()
-    if trouble:
-        return 1.0e100
-    return cputime
-
-    
-def update():
-    global trouble, t0, c0, cputime
-    if trouble:
-        return
-    t = time.time()
-    c = time.clock()
-    if t - t0 >= wrap:
-        trouble = True
-        return
-    dc = c - c0
-    if dc < 0.0:
-        dc += wrap
-    cputime += dc
-    t0 = t
-    c0 = c
 
     
 def function_timer(func, *args, **kwargs):
@@ -73,6 +19,26 @@ def function_timer(func, *args, **kwargs):
 
     
 class Timer:
+    """Timer object.
+    
+    Use like this::
+    
+        timer = Timer()
+        timer.start('description')
+        # do something
+        timer.stop()
+        
+    or::
+        
+        with timer('description'):
+            # do something
+            
+    To get a summary call::
+        
+        timer.write()
+
+    """
+    
     def __init__(self, print_levels=1000):
         self.timers = {}
         self.t0 = time.time()
@@ -215,4 +181,3 @@ class timer:
                 pass
             return x
         return new_method
-
