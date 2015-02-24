145848883e65f10546b2d58cb127188ea7b35ca9
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2015-02-24 08:32:44 +0000

    Disable failing test

--- ase/test/fio/v_sim.py
+++ ase/test/fio/v_sim.py
@@ -9,6 +9,8 @@ from socket import error as SocketError
 from ase.test import NotAvailable
 from ase.io import read
 
+raise NotAvailable
+
 dest = 'demo.ascii'
 src = 'http://inac.cea.fr/L_Sim/V_Sim/files/' + dest
 
