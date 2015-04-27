5f4834f1f92e0c554c616b439ac636074c28edb3
jensj <jensj@5af997e4-4c3d-0410-961e-ac0512e437d9>
2015-04-27 13:46:20 +0000

    Only use pbc=1 for those that need it ...

--- ase/test/calculator/traj.py
+++ ase/test/calculator/traj.py
@@ -7,9 +7,9 @@ from ase.test import test_calculator_names
 
 
 def h2(name, par):
-    calc = get_calculator(name)(**par)
-    h2 = molecule('H2', calculator=calc, pbc=1)
+    h2 = molecule('H2', pbc=par.pop('pbc', False))
     h2.center(vacuum=2.0)
+    h2.calc = get_calculator(name)(**par)
     e = h2.get_potential_energy()
     f = h2.get_forces()
     assert not h2.calc.calculation_required(h2, ['energy', 'forces'])
@@ -23,13 +23,12 @@ parameters = {
     'abinit': dict(ecut=200, toldfe=0.0001),
     'aims': dict(sc_accuracy_rho=5.e-3),
     'gpaw': dict(mode='lcao', basis='sz(dzp)', realspace=False),
-    'elk': dict(tasks=0, rgkmax=5.0)}
+    'elk': dict(tasks=0, rgkmax=5.0, pbc=True),
+    'jacapo': dict(pbc=True)}
 
 for name in test_calculator_names:
     par = parameters.get(name, {})
     os.mkdir(name + '-test')
     os.chdir(name + '-test')
-    try:
-        h2(name, par)
-    finally:
-        os.chdir('..')
+    h2(name, par)
+    os.chdir('..')
