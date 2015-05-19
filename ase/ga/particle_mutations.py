bb35d6f95cff5007d786b50a5aa14b542d9f7810
stly <stly@5af997e4-4c3d-0410-961e-ac0512e437d9>
2015-05-19 13:19:44 +0000

    in ase.ga fixed error in RandomPermutation in particle_mutations.py

--- ase/ga/particle_mutations.py
+++ ase/ga/particle_mutations.py
@@ -126,9 +126,10 @@ class RandomPermutation(Mutation):
     Parameters:
 
     num_muts: the number of times to perform this operation."""
-    def __init__(self, num_muts=1):
+    def __init__(self, elements=None, num_muts=1):
         Mutation.__init__(self, num_muts=num_muts)
         self.descriptor = 'RandomPermutation'
+        self.elements = elements
 
     def get_new_individual(self, parents):
         f = parents[0].copy()
@@ -140,7 +141,7 @@ class RandomPermutation(Mutation):
         indi.info['data']['parents'] = [f.info['confid']]
 
         for _ in range(self.num_muts):
-            RandomPermutation.mutate(f)
+            RandomPermutation.mutate(f, self.elements)
 
         for atom in f:
             indi.append(atom)
@@ -149,12 +150,13 @@ class RandomPermutation(Mutation):
                 self.descriptor + ': {0}'.format(f.info['confid']))
 
     @classmethod
-    def mutate(cls, atoms):
+    def mutate(cls, atoms, elements=None):
         """Do the actual permutation."""
-        i1 = random.randrange(len(atoms))
-        i2 = random.randrange(len(atoms))
+        indices = [a.index for a in atoms if a.symbol in elements]
+        i1 = random.choice(indices)
+        i2 = random.choice(indices)
         while atoms[i1].symbol == atoms[i2].symbol:
-            i2 = random.randrange(len(atoms))
+            i2 = random.choice(indices)
         Mutation.interchange2(atoms, i1, i2)
 
 
@@ -352,6 +354,7 @@ class Poor2richPermutation(_NeighborhoodPermutation):
     @classmethod
     def mutate(cls, atoms, elements):
         _NP = _NeighborhoodPermutation
+        # indices = [a.index for a in atoms if a.symbol in elements]
         ac = atoms.copy()
         del ac[[atom.index for atom in ac
                 if atom.symbol not in elements]]
