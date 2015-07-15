f142c45f725e5779fb89e8f0bbb9f82423521699
oschuett <oschuett@5af997e4-4c3d-0410-961e-ac0512e437d9>
2015-07-15 13:21:36 +0000

    CP2K: Improve test of libxc

--- ase/test/cp2k/cp2k_H2_libxc.py
+++ ase/test/cp2k/cp2k_H2_libxc.py
@@ -20,12 +20,13 @@ def main():
     if "ASE_CP2K_COMMAND" not in os.environ:
         raise NotAvailable('$ASE_CP2K_COMMAND not defined')
 
-    calc = CP2K(xc='XC_MGGA_X_BR89', pseudo_potential="GTH-PBE",
+    calc = CP2K(xc='XC_GGA_X_PBE XC_GGA_C_PBE',
+                pseudo_potential="GTH-PBE",
                 label='test_H2_libxc')
     h2 = molecule('H2', calculator=calc)
     h2.center(vacuum=2.0)
     energy = h2.get_potential_energy()
-    energy_ref = -30.763724545059
+    energy_ref = -31.591716529642
     diff = abs((energy - energy_ref) / energy_ref)
     assert diff < 1e-10
     print('passed test "H2_libxc"')
