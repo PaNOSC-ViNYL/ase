cac18dfe513518efd9f0380b38034cbfb684e741
Lars Pastewka <lars.pastewka@kit.edu>
2015-10-19 08:38:12 +0200

    Updated CFG read to be able to handle both standard and extended CFG formats.

--- ase/io/cfg.py
+++ ase/io/cfg.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 import ase
-
+from ase.data import chemical_symbols
 from ase.parallel import paropen
 
 
@@ -66,7 +66,7 @@ def write_cfg(f, a):
     for i in a:
         el  = i.symbol
 
-        f.write('%f\n' % ase.data.atomic_masses[ase.data.chemical_symbols.index(el)])
+        f.write('%f\n' % ase.data.atomic_masses[chemical_symbols.index(el)])
         f.write('%s\n' % el)
 
         x, y, z = spos[i.index, :]
@@ -132,111 +132,108 @@ def write_clr(f, atoms):
 
 ###
 
-def read_key_val(f):
-    while True:
-        if isinstance(f, str):
-            l = f
-        else:
-            l = f.readline()
-        if not l.startswith('#'):
-            break
-    s = l.split('=')
-    if len(s) != 2:
-        raise RuntimeError("Line '%s' is not of the form 'key = value'." % l[:-1])
-    return ( s[0].strip(), s[1].strip() )
-
-
-def read_str_key(f, key, key2=None):
-    in_key, val  = read_key_val(f)
-    if key2 is None:
-        if key.upper() != in_key.upper():
-            raise RuntimeError("Key '%s' expected, '%s' found." % ( key, in_key ))
-    else:
-        if key.upper() != in_key.upper() and key2.upper() != in_key.upper():
-            raise RuntimeError("Key '%s' or '%s' expected, '%s' found." % ( key, key2, in_key ))
-    return val
-
-
-def read_int_key(f, key):
-    vals  = read_str_key(f, key).split()
-    # Ignore units
-    return int(vals[0])
-
-
-def read_float_key(f, key):
-    vals  = read_str_key(f, key).split()
-    # Ignore units
-    return float(vals[0])
-
-
-###
-
 def read_cfg(f):
     """Read atomic configuration from a CFG-file (native AtomEye format).
        See: http://mt.seas.upenn.edu/Archive/Graphics/A/
     """
     if isinstance(f, str):
-        f  = open(f)
+        f = open(f)
 
-    nat   = read_int_key(f, 'Number of particles')
-    unit  = read_float_key(f, 'A')
+    nat = None
+    naux = 0
+    aux = None
+    auxstrs = None
 
-    cell  = np.zeros( [ 3, 3 ] )
-    for i in range(3):
-        for j in range(3):
-            cell[i, j]  = read_float_key(f, 'H0(%1.1i,%1.1i)' % (i + 1, j + 1))
+    cell = np.zeros([3, 3])
+    transform = np.eye(3)
+    eta = np.zeros([3, 3])
 
-    l     = f.readline()
-    vels  = None
-    if l.strip() == '.NO_VELOCITY.':
-        l     = f.readline()
-    else:
-        vels  = np.zeros( [ nat, 3 ] )
+    current_atom = 0
+    current_symbol = None
+    current_mass = None
 
-    naux      = read_int_key(l, 'entry_count') - 3
-    if vels is not None:
-        naux -= 3
-    aux       = np.zeros( [ nat, naux ] )
-
-    auxstrs   = [ ]
-    for i in range(naux):
-        s  = read_str_key(f, 'auxiliary[%1.1i]' % i, 'auxiliary[%2.2i]' % i)
-        if '[' in s:
-            s = s[:s.find('[')]
-        auxstrs.append(s.strip())
-
-    spos     = np.zeros( [ nat, 3 ] )
-    masses   = np.zeros( nat )
-    syms     = [ '' for i in range(nat) ]
-
-    i  = 0
-    s  = f.readline().split()
+    l = f.readline()
     while l:
-        mass   = float(s[0])
-        sym    = f.readline().strip()
+        l = l.strip()
+        if len(l) != 0 and not l.startswith('#'):
+            if l == '.NO_VELOCITY.':
+                vels = None
+            else:
+                s = l.split('=')
+                if len(s) == 2:
+                    key, value = s
+                    key = key.strip()
+                    value = [x.strip() for x in value.split()]
+                    if key == 'Number of particles':
+                        nat = int(value[0])
+                        spos = np.zeros([nat, 3])
+                        masses = np.zeros(nat)
+                        syms = ['']*nat
+                        vels  = np.zeros([nat, 3])
+                        if naux > 0:
+                            aux = np.zeros([nat, naux])
+                    elif key == 'A':
+                        unit = float(value[0])
+                    elif key == 'entry_count':
+                        naux = int(value[0]) - 3
+                        auxstrs = ['']*naux
+                        if nat is not None:
+                            aux = np.zeros([nat, naux])
+                    elif key.startswith('H0('):
+                        i, j = [int(x) for x in key[3:-1].split(',')]
+                        cell[i-1, j-1] = float(value[0])
+                    elif key.startswith('Transform('):
+                        i, j = [int(x) for x in key[10:-1].split(',')]
+                        transform[i-1, j-1] = float(value[0])
+                    elif key.startswith('eta('):
+                        i, j = [int(x) for x in key[4:-1].split(',')]
+                        eta[i-1, j-1] = float(value[0])
+                    elif key.startswith('auxiliary['):
+                        i = int(key[10:-1])
+                        auxstrs[i] = value[0]
+                else:
+                    # Everything else must be particle data.
+                    # First check if current line contains an element mass or
+                    # name. Then we have an extended XYZ format.
+                    s = [x.strip() for x in l.split()]
+                    if len(s) == 1:
+                        if l in chemical_symbols:
+                            current_symbol = l
+                        else:
+                            current_mass = float(l)
+                    elif current_symbol is None and current_mass is None:
+                        # Standard CFG format
+                        masses[current_atom] = float(s[0])
+                        syms[current_atom] = s[1]
+                        spos[current_atom, :] = [float(x) for x in s[2:5]]
+                        vels[current_atom, :] = [float(x) for x in s[5:8]]
+                        current_atom += 1
+                    elif current_symbol is not None and \
+                        current_mass is not None:
+                        # Extended CFG format
+                        masses[current_atom] = current_mass
+                        syms[current_atom] = current_symbol
+                        props = [float(x) for x in s]
+                        spos[current_atom, :] = props[0:3]
+                        off  = 3
+                        if vels is not None:
+                            off = 6
+                            vels[current_atom, :] = props[3:6]
+                        aux[current_atom, :] = props[off:]
+                        current_atom += 1
+        l = f.readline()
 
-        l      = f.readline()
-        s      = l.split()
+    # Sanity check
+    if current_atom != nat:
+        raise RuntimeError('Number of atoms reported for CFG file (={0}) and '
+                           'number of atoms actually read (={1}) differ.'
+                           .format(nat, current_atom))
 
-        while l and len(s) > 1:
-            masses[i]  = mass
-            syms[i]    = sym
-            props      = [ float(x) for x in s ]
+    if np.any(eta != 0):
+        raise NotImplementedError('eta != 0 not yet implemented for CFG '
+                                  'reader.')
+    cell = cell.dot(transform)
 
-            spos[i, :] = props[0:3]
-            off        = 3
-            if vels is not None:
-                off         = 6
-                vels[i, :]  = props[3:6]
-
-            aux[i, :]  = props[off:]
-
-            i  += 1
-
-            l  = f.readline()
-            if l:
-                s  = l.split()
-                
     if vels is None:
         a = ase.Atoms(
             symbols           = syms,
@@ -255,17 +252,14 @@ def read_cfg(f):
             pbc               = True
         )
 
-    i  = 0
+    i = 0
     while i < naux:
         auxstr  = auxstrs[i]
-
         if auxstr[-2:] == '_x':
             a.set_array(auxstr[:-2], aux[:, i:i+3])
-
-            i  += 3
+            i += 3
         else:
             a.set_array(auxstr, aux[:, i])
-
-            i  += 1
+            i += 1
 
     return a
