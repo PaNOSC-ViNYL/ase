860eb37f09658b212665e0971d47faaa38c4d810
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2015-11-06 10:01:45 +0100

    Simplify test output

--- ase/test/__init__.py
+++ ase/test/__init__.py
@@ -67,13 +67,7 @@ class ScriptTestCase(unittest.TestCase):
         return self.filename
 
     def __str__(self):
-        f = self.filename
-        dir = os.path.basename(os.path.dirname(f))
-        file = os.path.basename(f)
-        if f.find('test') + len('test') + 1 == f.find(file):
-            return '%s (ScriptTestCase)' % file
-        else:
-            return '%s (ScriptTestCase)' % os.path.join(dir, file)
+        return self.filename.split('test/')[-1]
 
     def __repr__(self):
         return "ScriptTestCase(filename='%s')" % self.filename
