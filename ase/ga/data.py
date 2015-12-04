17dfa2f42e6a1d610a2ef019d9fb6478898fe9da
stly <stly@dtu.dk>
2015-12-04 11:55:08 +0100

    few changes in ase.ga.data

--- ase/ga/data.py
+++ ase/ga/data.py
@@ -3,8 +3,8 @@
 """
 import os
 from ase import Atoms
-from ase.ga import get_raw_score, set_neighbor_list
-from ase.ga import set_parametrization, get_parametrization
+from ase.ga import get_raw_score
+from ase.ga import set_parametrization, set_neighbor_list
 import ase.db
 
 
