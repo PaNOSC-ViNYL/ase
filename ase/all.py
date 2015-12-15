77aa6a54a040556077c02767945659eb41ccd147
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2015-12-15 10:59:22 +0100

    Deprecate use of ase.all module

--- ase/all.py
+++ ase/all.py
@@ -1,8 +1,13 @@
-"""This module imports many important modules at once."""
+import warnings
+from math import pi, sqrt
+
+import numpy as np
 
 from ase.atom import Atom
 from ase.atoms import Atoms
-from ase.units import *
+from ase.units import (
+    Ang, Angstrom, Bohr, C, Debye, GPa, Ha, Hartree, J, Pascal, Ry, Rydberg,
+    alpha, eV, fs, invcm, kB, kJ, kcal, kg, m, mol, nm, s, second)
 from ase.io import read, write
 from ase.io.trajectory import Trajectory
 from ase.dft import STM, monkhorst_pack, DOS
@@ -14,7 +19,10 @@ from ase.optimize.bfgs import BFGS
 from ase.optimize import QuasiNewton
 from ase.md.verlet import VelocityVerlet
 from ase.md.langevin import Langevin
-from ase.constraints import *
+from ase.constraints import (
+    Filter, FixAtoms, FixBondLength, FixBondLengths, FixCartesian,
+    FixConstraint, FixConstraintSingle, FixInternals, FixScaled, FixedLine,
+    FixedMode, FixedPlane, Hookean, StrainFilter, UnitCellFilter)
 from ase.calculators.lj import LennardJones
 from ase.calculators.emt import EMT
 from ase.calculators.siesta import Siesta
@@ -23,13 +31,31 @@ from ase.calculators.aims import Aims, AimsCube
 from ase.calculators.turbomole import Turbomole
 from ase.calculators.dftb import Dftb
 from ase.neb import NEB, SingleCalculatorNEB
-from ase.dimer import DimerControl, DimerAtoms, DimerTranslate, \
-     MinModeAtoms, MinModeTranslate
+from ase.dimer import (DimerControl, DimerAtoms, DimerTranslate,
+                       MinModeAtoms, MinModeTranslate)
 from ase.visualize import view
-from ase.data import chemical_symbols, atomic_numbers, atomic_names, \
-     atomic_masses, covalent_radii, reference_states
-from ase.structure import *
-#from ase.lattice import bulk
+from ase.data import (chemical_symbols, atomic_numbers, atomic_names,
+                      atomic_masses, covalent_radii, reference_states)
+from ase.structure import graphene_nanoribbon, molecule, nanotube
 
-from math import pi, sqrt
-import numpy as np
+
+warnings.warn('Do not import stuff from here!  The ase.all module will be '
+              'removed some day in the future.')
+
+__all__ = ['Atom', 'Atoms', 'read', 'write', 'Trajectory', 'STM',
+           'monkhorst_pack', 'DOS', 'MDMin', 'HessLBFGS', 'FIRE', 'LBFGS',
+           'LBFGSLineSearch', 'BFGS', 'QuasiNewton', 'VelocityVerlet',
+           'Langevin', 'LennardJones', 'EMT', 'Siesta', 'Vasp', 'Aims',
+           'AimsCube', 'Turbomole', 'Dftb', 'NEB', 'SingleCalculatorNEB',
+           'DimerControl', 'DimerAtoms', 'DimerTranslate', 'MinModeAtoms',
+           'MinModeTranslate', 'view', 'chemical_symbols', 'atomic_numbers',
+           'atomic_names', 'atomic_masses', 'covalent_radii',
+           'reference_states', 'graphene_nanoribbon', 'molecule',
+           'nanotube', 'pi', 'sqrt', 'np', 'Ang', 'Angstrom', 'Bohr', 'C',
+           'Debye', 'GPa', 'Ha', 'Hartree', 'J', 'Pascal', 'Ry', 'Rydberg',
+           'alpha', 'eV', 'fs', 'invcm', 'kB', 'kJ', 'kcal', 'kg', 'm', 'mol',
+           'nm', 's', 'second', 'Filter', 'FixAtoms', 'FixBondLength',
+           'FixBondLengths', 'FixCartesian', 'FixConstraint',
+           'FixConstraintSingle', 'FixInternals', 'FixScaled', 'FixedLine',
+           'FixedMode', 'FixedPlane', 'Hookean', 'StrainFilter',
+           'UnitCellFilter']
