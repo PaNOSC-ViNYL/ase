5068efef4a53c3e7760460c5ae9fa4e107e2cc61
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2016-02-16 11:31:06 +0100

    Read the docs

--- doc/conf.py
+++ doc/conf.py
@@ -1,4 +1,6 @@
 import sys
+import sphinx_rtd_theme
+
 sys.path.append('.')
 assert sys.version >= '2.7'
 
@@ -9,18 +11,20 @@ extensions = ['ext',
               'sphinx.ext.viewcode',
               'sphinx.ext.intersphinx']
 source_suffix = '.rst'
-master_doc = 'contents'
+#master_doc = 'contents'
 project = 'ASE'
-copyright = '2015, CAMd'
-templates_path = ['templates']
+copyright = '2016, ASE-developers'
+#templates_path = ['templates']
 exclude_patterns = ['build']
 default_role = 'math'
 pygments_style = 'sphinx'
 autoclass_content = 'both'
-html_style = 'ase.css'
-html_logo = 'static/ase.ico'
-html_favicon = 'static/ase.ico'
-html_static_path = ['static']
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#html_style = 'ase.css'
+#html_logo = 'static/ase.ico'
+#html_favicon = 'static/ase.ico'
+#html_static_path = ['static']
 html_last_updated_fmt = '%a, %d %b %Y %H:%M:%S'
 intersphinx_mapping = {'gpaw': ('http://wiki.fysik.dtu.dk/gpaw', None),
                        'python': ('http://docs.python.org/2.7', None)}
