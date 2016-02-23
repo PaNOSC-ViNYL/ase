cc0e089c100784951ac5c72c46d9b47c44aae945
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2016-02-23 11:19:06 +0100

    Stop reading old traj files

--- ase/test/fio/oldtraj.py
+++ ase/test/fio/oldtraj.py
@@ -1,75 +1,18 @@
-"""Check that we can read old version 1 Trajectories."""
-import functools
-import pickle
-import sys
-from io import BytesIO
-
+"""Make sure we can't read old traj file, but we can convert them."""
+import ase.io as aio
 from ase import Atoms
-from ase.constraints import FixAtoms
 from ase.io.pickletrajectory import PickleTrajectory
-from ase.test import NotAvailable
-
-if sys.version_info[0] == 3:
-    raise NotAvailable
+from ase.io.trajectory import convert
+from ase.test import must_raise
 
 
-Trajectory = functools.partial(PickleTrajectory, _warn=False)
+t = PickleTrajectory('hmm.traj', 'w', _warn=False)
+a = Atoms('H')
+t.write(a)
+t.close()
 
-a = Atoms('FOO')
-
-
-def v1(a):
-    """Create old version-1 trajectory."""
-    fd = BytesIO()
-    fd.write(b'PickleTrajectory')
-    d = {'pbc': a.pbc,
-         'numbers': a.numbers,
-         'tags': None,
-         'masses': None,
-         'constraints': a.constraints}
-    pickle.dump(d, fd, protocol=-1)
-    d = {'positions': a.positions,
-         'cell': a.cell,
-         'momenta': None}
-    pickle.dump(d, fd, protocol=-1)
-    return BytesIO(fd.getvalue())
-
-
-def v2(a):
-    """Create new version-2 trajectory."""
-    fd = BytesIO()
-    t = Trajectory(fd, 'w')
-    t.write(a)
-    return BytesIO(fd.getvalue())
-
-
-class MyFixAtoms(FixAtoms):
-    pass
-
-
-a.constraints = FixAtoms(indices=[2])
-t1 = v1(a)
-t2 = v2(a)
-
-# Read old trajectory:
-c1 = Trajectory(t1)[0].constraints
-assert c1[0].index[0] == 2
-
-# Read new trajectory:
-c2 = Trajectory(t2)[0].constraints
-assert c2[0].index[0] == 2
-
-a.constraints = MyFixAtoms(indices=[1])
-t3 = v2(a)
-
-# Read new trajectory with missing constraint class.  This should
-# ignore the constraint and issue a warning:
-del MyFixAtoms, a
-import warnings
-warnings.filterwarnings('error')
-try:
-    c3 = Trajectory(t3)[0].constraints
-except UserWarning:
-    pass
-else:
-    assert False
+with must_raise(DeprecationWarning):
+    aio.read('hmm.traj')
+    
+convert('hmm.traj')
+aio.read('hmm.traj')
