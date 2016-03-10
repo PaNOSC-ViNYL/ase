32fb39675cbb248d02ebd2a968af6b9943666e3c
tgmaxson <tgmaxson@gmail.com>
2016-03-10 15:49:43 -0500

    Improve test to cover a bit more of the root functions

--- ase/test/root_test.py
+++ ase/test/root_test.py
@@ -1,23 +1,40 @@
-from ase.lattice.surface import fcc111, fcc111_root
-from ase.lattice.root import root_surface, root_surface_analysis
+from ase.lattice.surface import fcc111
+from ase.lattice.surface import bcc111
+from ase.lattice.surface import hcp0001
+from ase.lattice.surface import fcc111_root 
+from ase.lattice.root import root_surface
+from ase.lattice.root import root_surface_analysis
 
-# Make sample primitive cell
-primitive = fcc111("Pt", (1, 1, 3))
+# Make samples of primitive cell
+prim_fcc111 = fcc111("H", (1, 1, 3), a=1)
+prim_bcc111 = bcc111("H", (1, 1, 3), a=1)
+prim_hcp0001 = hcp0001("H", (1, 1, 3), a=1)
 
-# Check valid roots up to root search 5 (much higher than root 5)
-valid = root_surface_analysis(primitive, 5)
+# Check valid roots up to root 21 (the 10th root cell)
+valid_fcc111 = root_surface_analysis(prim_fcc111, 21)
+valid_bcc111 = root_surface_analysis(prim_bcc111, 21)
+valid_hcp0001 = root_surface_analysis(prim_hcp0001, 21)
+
+# These should have different positions, but the same
+# cell geometry.
+assert valid_fcc111 == valid_bcc111 == valid_hcp0001
 
 # Make an easy sample to check code errors
-atoms1 = root_surface(primitive, 7)
+atoms1 = root_surface(prim_fcc111, 7)
 
-# Ensure the valid roots are the first 10 valid
-# roots for this system
-assert valid[:10] == [1.0, 3.0, 4.0, 7.0, 9.0,
-                      12.0, 13.0, 16.0, 19.0, 21.0]
+# Ensure the valid roots are the roots are valid against
+# a set of manually checked roots for this system
+assert valid_fcc111 == [1.0, 3.0, 4.0, 7.0, 9.0,
+                        12.0, 13.0, 16.0, 19.0, 21.0]
 
 # Remake easy sample using surface function
-atoms2 = fcc111_root("Pt", 7, (1, 1, 3))
+atoms2 = fcc111_root("H", 7, (1, 1, 3), a=1)
 
+# Right number of atoms
 assert len(atoms1) == len(atoms2)
+
+# Same positions
 assert (atoms1.positions == atoms2.positions).all()
+
+# Same cell
 assert (atoms1._cell == atoms2._cell).all()
