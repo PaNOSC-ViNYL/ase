40f76e8f8f7aae3aada8f6dcb191e301ef496b23
Lars Pastewka <lars.pastewka@kit.edu>
2016-04-15 16:37:37 +0200

    BUG: Reading CFG files with velocity information was broken.

--- ase/test/fio/cfg.py
+++ ase/test/fio/cfg.py
@@ -11,3 +11,9 @@ write('test.cfg', a)
 
 b = read('test.cfg')
 assert np.all(b.get_array('test') == f)
+
+a.set_momenta(2*f)
+write('test.cfg', a)
+
+b = read('test.cfg')
+assert np.all(np.abs(a.get_momenta()-b.get_momenta())<1e-3)
