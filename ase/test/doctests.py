c672159fb20f5dc9134b66db3d8306f1ffa80fb8
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2016-04-20 10:35:55 +0200

    Fix doctests and run them

--- ase/test/doctests.py
+++ ase/test/doctests.py
@@ -5,13 +5,16 @@ try:
 except ImportError:
     scipy = None
     
+from ase import atoms
 from ase.collection import collection
 from ase.spacegroup import spacegroup, findsym, xtal
 from ase.geometry import geometry, cell
+from ase.build import tools
+from ase.io import aff
 
-modules = [collection, xtal, spacegroup, cell, findsym]
+modules = [collection, xtal, spacegroup, cell, findsym, tools, aff, atoms]
 if scipy:
     modules.append(geometry)
     
 for mod in modules:
-    doctest.testmod(mod, raise_on_error=True)
+    print(mod, doctest.testmod(mod, raise_on_error=True))
