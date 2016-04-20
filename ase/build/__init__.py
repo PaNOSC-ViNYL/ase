3dfac5436ef4b8c3aa1fa73667ea6428468f9001
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2016-04-20 07:18:23 +0200

    Fix some imports

--- ase/build/__init__.py
+++ ase/build/__init__.py
@@ -5,15 +5,18 @@ from ase.build.surface import (
     diamond100, diamond111,
     fcc100, fcc110, fcc111, fcc211,
     hcp0001, hcp10m10, mx2)
+from ase.build.bulk import bulk
 from ase.build.general_surface import surface
 from ase.build.root import (hcp0001_root, fcc111_root, bcc111_root,
                             root_surface, root_surface_analysis)
 
 __all__ = ['minimize_rotation_and_translation',
-           'surface', 'add_adsorbate', 'add_vacuum',
+           'add_adsorbate', 'add_vacuum',
            'bcc100', 'bcc110', 'bcc111',
            'diamond100', 'diamond111',
            'fcc100', 'fcc110', 'fcc111', 'fcc211',
            'hcp0001', 'hcp10m10', 'mx2',
+           'surface',
            'hcp0001_root', 'fcc111_root', 'bcc111_root',
-           'root_surface', 'root_surface_analysis']
+           'root_surface', 'root_surface_analysis',
+           'bulk']
