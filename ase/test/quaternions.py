b651c86e25341354ad4fce4b3bb38e9e6aef20df
Simone Sturniolo <simone.sturniolo@stfc.ac.uk>
2016-05-09 13:15:33 +0100

    TST: Added test for Quaternion multiplication

--- ase/test/quaternions.py
+++ ase/test/quaternions.py
@@ -1,9 +1,7 @@
 import numpy as np
 from ase.quaternions import Quaternion
 
-# First: test that rotations DO work
-for i in range(10):
-	# 10 random tests
+def rand_rotm():
 
 	# Axis & angle rotations
 	u = np.random.random(3)
@@ -17,7 +15,15 @@ for i in range(10):
 	rotm = np.cos(theta)*np.identity(3) + np.sin(theta)*ucpm + \
 	       (1 - np.cos(theta))*np.kron(u[:,None], u[None,:])
 
-	q = Quaternion().from_matrix(rotm)
+	return rotm
+
+# First: test that rotations DO work
+for i in range(10):
+	# 10 random tests
+
+	rotm = rand_rotm()
+
+	q = Quaternion.from_matrix(rotm)
 
 	# Now test this with a vector
 	v = np.random.random(3)
@@ -32,6 +38,23 @@ for i in range(10):
 rotm = np.identity(3)
 rotm[:2,:2] *= -1 		# Rotate PI around z axis
 
-q = Quaternion().from_matrix(rotm)
+q = Quaternion.from_matrix(rotm)
 
 assert not np.isnan(q.q).any()
+
+# Third: test compound rotations and operator overload
+for i in range(10):
+
+	rotm1 = rand_rotm()
+	rotm2 = rand_rotm()
+
+	q1 = Quaternion.from_matrix(rotm1)
+	q2 = Quaternion.from_matrix(rotm2)
+
+	# Now test this with a vector
+	v = np.random.random(3)
+
+	vrotM = np.dot(rotm2, np.dot(rotm1, v))
+	vrotQ = (q2*q1).rotate(v)
+
+	assert np.allclose(vrotM, vrotQ)
