8db836ba1f6693f6e6bc9df9fc0c5ca4022b79f2
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2016-05-12 13:16:55 +0200

    Add SymPy test for B and Bp

--- ase/eos.py
+++ ase/eos.py
@@ -71,6 +71,18 @@ def birchmurnaghan(V, E0, B0, BP, V0):
     return E
 
     
+def check_birchmurnaghan():
+    from sympy import symbols, Rational, diff, simplify
+    v, b, bp, v0 = symbols('v b bp v0')
+    x = (v0 / v)**Rational(2, 3)
+    e = 9 * b * v0 * (x - 1)**2 * (6 + bp * (x - 1) - 4 * x) / 16
+    print(e)
+    B = diff(e, v, 2) * v
+    BP = -v * diff(B, v) / b
+    print(simplify(B.subs(v, v0)))
+    print(simplify(BP.subs(v, v0)))
+    
+    
 def pouriertarantola(V, E0, B0, BP, V0):
     'Pourier-Tarantola equation from PRB 70, 224107'
 
