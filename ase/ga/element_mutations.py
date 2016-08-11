d8cd552b9893013827271548662056c7fd8ff0f1
Paul C. Jennings <jennings.p.c@gmail.com>
2016-08-11 13:15:52 -0700

    Added FullElementMutation, a more aggressive element mutation that swaps all of one element type for another.

--- ase/ga/element_mutations.py
+++ ase/ga/element_mutations.py
@@ -538,3 +538,51 @@ class MoveLeftMutation(ElementMutation):
 
         return (self.finalize_individual(indi),
                 used_descriptor + ':Parent {0}'.format(f.info['confid']))
+
+
+class FullElementMutation(OffspringCreator):
+    """Mutation that exchanges an all elements of a certain type with another
+    randomly chosen element from the supplied pool of elements. Any constraints
+    on the mutation are inhereted from the original candidate.
+
+    Parameters:
+
+    element_pool: List of elements in the phase space. The elements can be
+        grouped if the individual consist of different types of elements.
+        The list should then be a list of lists e.g. [[list1], [list2]]
+    """
+    def __init__(self, element_pool, verbose=False, num_muts=1):
+        OffspringCreator.__init__(self, verbose, num_muts=num_muts)
+        self.descriptor = 'FullElementMutation'
+        if not isinstance(element_pool[0], (list, np.ndarray)):
+            self.element_pools = [element_pool]
+        else:
+            self.element_pools = element_pool
+
+    def get_new_individual(self, parents):
+        f = parents[0]
+
+        indi = self.initialize_individual(f)
+        indi.info['data']['parents'] = [f.info['confid']]
+
+        # Randomly choose and element to mutate in the current individual.
+        old_element = random.choice([f[i].symbol for i in range(len(f))])
+        # Find the list containing the chosen element. By choosing a new
+        # element from the same list, the percentages are not altered.
+        for i in range(len(self.element_pools)):
+            if old_element in self.element_pools[i]:
+                lm = i
+
+        not_val = True
+        while not_val:
+            new_element = random.choice(self.element_pools[lm])
+            if new_element != old_element:
+                not_val = False
+
+        for a in f:
+            if a.symbol == old_element:
+                a.symbol = new_element
+            indi.append(a)
+
+        return (self.finalize_individual(indi),
+                self.descriptor + ': Parent {0}'.format(f.info['confid']))
