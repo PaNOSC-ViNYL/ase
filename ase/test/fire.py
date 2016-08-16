7930868c1d2e773a0c3f840503909df611825ce4
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2016-08-16 11:17:33 +0200

    Import bulk from ase.build

--- ase/test/fire.py
+++ ase/test/fire.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 from ase.calculators.emt import EMT
-from ase.lattice import bulk
+from ase.build import bulk
 from ase.optimize import FIRE
 
 a = bulk('Au')
-a *= (2,2,2)
+a *= (2, 2, 2)
 
 a[0].x += 0.5
 
@@ -17,20 +17,24 @@ e1 = a.get_potential_energy()
 n1 = opt.nsteps
 
 a = bulk('Au')
-a *= (2,2,2)
+a *= (2, 2, 2)
 
 a[0].x += 0.5
 
 a.set_calculator(EMT())
 
 reset_history = []
+
+
+def callback(a, r, e, e_last):
+    reset_history.append([e - e_last])
+
 opt = FIRE(a, dtmax=10.0, dt=10.0, maxmove=100.0, downhill_check=True,
-           position_reset_callback= \
-               lambda a, r, e, e_last: reset_history.append([e-e_last]))
+           position_reset_callback=callback)
 opt.run(fmax=0.001)
 e2 = a.get_potential_energy()
 n2 = opt.nsteps
 
-assert abs(e1-e2) < 1e-6
+assert abs(e1 - e2) < 1e-6
 assert n2 < n1
-assert (np.array(reset_history)>0).all
+assert (np.array(reset_history) > 0).all
