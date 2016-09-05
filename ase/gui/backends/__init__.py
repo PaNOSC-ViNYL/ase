121d5ce3d88e27571e7c03ff815d20a1e72cfb03
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2016-09-05 15:12:16 +0200

    Rename

--- ase/gui/backends/__init__.py
+++
@@ -1,7 +0,0 @@
-from ase.utils import import_module
-
-
-def select_backend(name):
-    import ase.gui.ui as ui
-    module = import_module('ase.gui.backends.' + name)
-    ui.Button = module.Button
