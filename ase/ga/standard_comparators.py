633121b5168debe5f36b21a2bd1794305a03c6c3
stly <stly@dtu.dk>
2016-09-05 16:05:29 +0200

    Renamed HashComparator to StringComparator, i.e. the name now makes sense

--- ase/ga/standard_comparators.py
+++ ase/ga/standard_comparators.py
@@ -125,17 +125,19 @@ class SequentialComparator(object):
         return False
         
         
-class HashComparator(object):
+class StringComparator(object):
     """Compares the calculated hash strings. These strings should be stored
-       in atoms.info['data'][key1] and atoms.info['data'][key2] ...
-       where the keys should be supplied as a list [key1,key2]
+       in atoms.info['key_value_pairs'][key1] and
+       atoms.info['key_value_pairs'][key2] ...
+       where the keys should be supplied as parameters i.e.
+       StringComparator(key1, key2, ...)
     """
     def __init__(self, *keys):
         self.keys = keys
         
     def looks_like(self, a1, a2):
         for k in self.keys:
-            if a1.info['data'][k] == a2.info['data'][k]:
+            if a1.info['key_value_pairs'][k] == a2.info['key_value_pairs'][k]:
                 return True
         return False
         
