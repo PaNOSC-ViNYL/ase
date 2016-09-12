7417c7ebed7d3c62cd8234716dd384c5301202b6
Michael Walter <Michael.Walter@fmf.uni-freiburg.de>
2016-09-12 14:19:29 +0200

    Test really added.

---
+++ ase/test/gpaw/no_spin_and_spin.py
@@ -0,0 +1,20 @@
+from ase.build import molecule
+from ase import io
+from gpaw import GPAW
+
+txt = 'out.txt'
+if 1:
+    calculator = GPAW(h=0.3, txt=txt)
+    atoms = molecule('H2', calculator=calculator)
+    atoms.center(vacuum=3)
+    #atoms.set_initial_magnetic_moments([0.5, -0.5])
+    atoms.get_potential_energy()
+
+    atoms.set_initial_magnetic_moments([0.5, 0.5])
+    calculator.set(charge=1)
+    atoms.get_potential_energy()
+
+# read again
+t = io.read(txt, index=':')
+assert(type(t) == type([]))
+
