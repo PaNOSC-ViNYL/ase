53b49eabbd4bb1570ea6adbd09b449f6e4309b98
Edward Tait <ewt23@cam.ac.uk>
2016-09-13 16:03:28 +0100

    TST: do a better job of finding pseudopotentials

--- ase/test/onetep/test-onetep.py
+++ ase/test/onetep/test-onetep.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 
 from ase.build import molecule
 from ase.calculators import onetep
-from os.path import isfile
+from os.path import isfile, dirname, abspath, join
 
 
 def main():
@@ -11,13 +11,16 @@ def main():
     calc = onetep.Onetep(label="water")
     # Tests conducted with the JTH PAW data set.
     # http://www.abinit.org/downloads/PAW2
-    if not (isfile("H.abinit") and isfile("O.abinit")):
+    prefix = dirname(abspath(__file__))
+    h_path = join(prefix,"H.abinit")
+    o_path = join(prefix,"O.abinit")
+    if not (isfile(h_path) and isfile(o_path)):
         raise Exception("""You must supply PAW data sets for
             hydrogen and oxygen to run this test.
             Please see http://www.abinit.org/downloads/PAW2
             for suitable data. ONETEP takes PAW data sets in the
             abinit format. I need H.abinit and O.abinit""")
-    calc.set_pseudos([('H', "H.abinit"), ('O', "O.abinit")])
+    calc.set_pseudos([('H', h_path), ('O', o_path)])
     calc.set(PAW=True, xc="PBE")
     mol.set_calculator(calc)
     energy = mol.get_total_energy()
