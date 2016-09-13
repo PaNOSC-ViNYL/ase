0988cf338e3d2e9288c80de6f37f768da686e4f4
Edward Tait <ewt23@cam.ac.uk>
2016-09-13 14:23:23 +0100

    TST: change to make onetep test raise exception on failure

--- ase/test/onetep/test-onetep.py
+++ ase/test/onetep/test-onetep.py
@@ -10,12 +10,12 @@ def main():
     calc = onetep.Onetep(label="water")
     #Tests conducted with the JTH PAW data set.
     #http://www.abinit.org/downloads/PAW2
-    if not (isfile("H.abinit") and isfile("O.abinit")):
-        raise Exception( """You must supply PAW data sets for
-            hydrogen and oxygen to run this test.
-            Please see http://www.abinit.org/downloads/PAW2
-            for suitable data. ONETEP takes PAW data sets in the
-            abinit format. I need H.abinit and O.abinit""")
+    #if not (isfile("H.abinit") and isfile("O.abinit")):
+    #    raise Exception( """You must supply PAW data sets for
+    #        hydrogen and oxygen to run this test.
+    #        Please see http://www.abinit.org/downloads/PAW2
+    #        for suitable data. ONETEP takes PAW data sets in the
+    #        abinit format. I need H.abinit and O.abinit""")
     calc.set_pseudos([('H',"H.abinit"),('O',"O.abinit")])
     calc.set(PAW=True,xc="PBE")
     mol.set_calculator(calc)
@@ -24,6 +24,7 @@ def main():
     if abs(energy-ref_energy) < 1e-5:
         print("Test passed.")
     else:
-        print("Test failed. Expected %f, got %f" % (energy,ref_energy))
+        raise Exception("Test failed. Expected energy %f, got %f" \
+            % (energy,ref_energy))
 
 main()
