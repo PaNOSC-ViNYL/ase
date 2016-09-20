c35ca2925517a14d4b013fdbe75410b7ec6f75cf
marc barbry <marc.barbarosa@gmail.com>
2016-09-20 18:31:35 +0200

    small corrections

--- ase/vibrations/__init__.py
+++ ase/vibrations/__init__.py
@@ -1,4 +1,4 @@
 from ase.vibrations.vibrations import Vibrations
 from ase.vibrations.infrared import Infrared
-from ase.vibrations.siesta_raman import Siesta_Raman
+from ase.calculators.siesta.siesta_raman import SiestaRaman
 __all__ = ['Vibrations', 'Infrared', 'Siesta_Raman']
