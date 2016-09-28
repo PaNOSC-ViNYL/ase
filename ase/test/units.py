fdec32da832fb3e1d10264abb27738e49f0fbb18
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2016-09-28 12:01:01 +0200

    Actually run the test + PEP8

--- ase/test/units.py
+++ ase/test/units.py
@@ -1,27 +1,30 @@
-# This test cross-checks our implementation of CODATA against the
-# implementation that SCIPY brings with it
+"""This test cross-checks our implementation of CODATA against the
+implementation that SCIPY brings with it.
+"""
+
 
 def test_units():
     import numpy as np
     from ase.units import CODATA
     import scipy.constants.codata
 
-    name_map = {'_c' : 'speed of light in vacuum',
-                '_mu0' : 'mag. const.',
-                '_Grav' : 'Newtonian constant of gravitation',
-                '_hplanck' : 'Planck constant',
-                '_e' : 'elementary charge',
-                '_me' : 'electron mass',
-                '_mp' : 'proton mass',
-                '_Nav' : 'Avogadro constant',
-                '_k' : 'Boltzmann constant',
-                '_amu' : 'atomic mass unit-kilogram relationship'}
+    name_map = {'_c': 'speed of light in vacuum',
+                '_mu0': 'mag. const.',
+                '_Grav': 'Newtonian constant of gravitation',
+                '_hplanck': 'Planck constant',
+                '_e': 'elementary charge',
+                '_me': 'electron mass',
+                '_mp': 'proton mass',
+                '_Nav': 'Avogadro constant',
+                '_k': 'Boltzmann constant',
+                '_amu': 'atomic mass unit-kilogram relationship'}
 
     for version in sorted(CODATA.keys()):
         print('Checking CODATA version "{}"'.format(version))
 
         try:
-            scipy_CODATA = getattr(scipy.constants.codata, '_physical_constants_{}'.format(version))
+            scipy_CODATA = getattr(scipy.constants.codata,
+                                   '_physical_constants_{}'.format(version))
         except AttributeError:
             print('\tNot available through scipy, skipping')
             continue
@@ -37,10 +40,13 @@ def test_units():
                 else:
                     msg += '[FALSE]'
                     ok = False
-                print('\t'+msg)
+                print('\t' + msg)
                 if not ok:
                     raise AssertionError
 
             except KeyError:
                 # 2002 in scipy contains too little data
                 continue
+
+
+test_units()
