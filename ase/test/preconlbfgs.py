afb03d89d91d7dfe6b00664565bde6a5463c9392
James Kermode <james.kermode@gmail.com>
2016-10-18 16:33:06 +0100

    TST: speed up and simplify preconditioned optimizer test by using EMT

--- ase/test/preconlbfgs.py
+++ ase/test/preconlbfgs.py
@@ -5,6 +5,7 @@ import numpy as np
 from ase.build import bulk
 from ase.constraints import UnitCellFilter
 from ase.calculators.lj import LennardJones
+from ase.calculators.emt import EMT
 from ase.optimize.precon import Exp, PreconLBFGS, PreconFIRE
 
 N = 1
@@ -18,24 +19,15 @@ a0.set_scaled_positions(s)
 
 nsteps = []
 energies = []
-for variable_cell in [False, True]:
-    for OPT in [PreconLBFGS, PreconFIRE]:
-        for precon in [None, Exp(A=3)]:
-            atoms = a0.copy()
-            atoms.set_calculator(LennardJones())
-            if variable_cell:
-                atoms = UnitCellFilter(atoms)
-            opt = OPT(atoms, precon=precon, use_armijo=True)
-            opt.run(1e-4)
-            energies += [atoms.get_potential_energy()]
-            nsteps += [opt.get_number_of_steps()]
+for OPT in [PreconLBFGS, PreconFIRE]:
+    for precon in [None, Exp(A=3)]:
+        atoms = a0.copy()
+        atoms.set_calculator(EMT())
+        opt = OPT(atoms, precon=precon, use_armijo=True)
+        opt.run(1e-4)
+        energies += [atoms.get_potential_energy()]
+        nsteps += [opt.get_number_of_steps()]
 
 # check we get the expected energy for all methods
+assert np.abs(np.array(energies) - -0.022726045433998365).max() < 1e-4
 
-# fixed cell cases
-assert np.abs(
-    np.array(energies[:len(energies) // 2]) - -0.92612723).max() < 1e-4
-
-# variable cell cases
-assert np.abs(
-    np.array(energies[len(energies) // 2:]) - -31.7516156).max() < 1e-4
