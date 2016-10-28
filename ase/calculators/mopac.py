091fa2a5ecf3417cfb5de87636b6ad71a973ed3f
Mikkel Strange <mikst@fysik.dtu.dk>
2016-10-28 11:46:08 +0200

    eigenvalues, homo-lumo, somo etc

--- ase/calculators/mopac.py
+++ ase/calculators/mopac.py
@@ -8,6 +8,7 @@ Set $ASE_MOPAC_COMMAND to something like::
 
 """
 import os
+import re
 
 import numpy as np
 
@@ -75,39 +76,76 @@ class MOPAC(FileIOCalculator):
 
         with open(self.label + '.out') as f:
             lines = f.readlines()
-            
+
+        p = re.compile(r"Empirical Formula:\s*.*=\s*(?P<na>[0-9]+)\s*atoms")
+        for line in lines:
+            m = re.search(p, line)
+            if m:
+                natoms = int(m.group('na'))
+                break
+
         for i, line in enumerate(lines):
             if line.find('TOTAL ENERGY') != -1:
                 self.results['energy'] = float(line.split()[3])
+            elif line.find('FINAL HEAT OF FORMATION') != -1:
+                self.final_hof = float(line.split()[5]) * kcal /  mol
+            elif line.find('NO. OF FILLED LEVELS') != -1:
+                self.no_occ_levels = int(line.split()[-1])
+            elif line.find('NO. OF ALPHA ELECTRON') != -1:
+                self.no_alpha_electrons = int(line.split()[-1])
+                self.no_beta_electrons = int(lines[i+1].split()[-1])
+            elif line.find('FINAL  POINT  AND  DERIVATIVES') != -1:
+                forces = [-float(line.split()[6])
+                          for line in lines[i + 3:i + 3 + 3 * natoms]]
+                forces = np.array(forces).reshape((-1, 3)) * kcal / mol
+                self.results['forces'] = forces
             elif line.find('EIGENVALUES') != -1:
                 if line.find('ALPHA') != -1:
+                    self.nspins = 2
                     j = i + 1
                     eigs_alpha = []
                     while lines[j].strip():
                         eigs_alpha += [float(e) for e in lines[j].split()]
                         j += 1
                 elif line.find('BETA') != -1:
-                    assert 0
                     j = i + 1
                     eigs_beta = []
                     while lines[j].strip():
                         eigs_beta += [float(e) for e in lines[j].split()]
                         j += 1
                     eigs = np.array([eigs_alpha, eigs_beta]).reshape(2, 1, -1)
+                    self.eigenvalues = eigs
                 else:
+                    self.nspins = 1
                     eigs = []
                     j = i + 1
                     while lines[j].strip():
                         eigs += [float(e) for e in lines[j].split()]
                         j += 1
-                    eigs = np.array(eigs).reshape(1, 1, -1)
-                self.eigenvalues = eigs
-            elif line.find('FINAL  POINT  AND  DERIVATIVES') != -1:
-                forces = [-float(line.split()[6])
-                          for line in lines[i + 3:i + 3 + 3 * len(self.atoms)]]
-                forces = np.array(forces).reshape((-1, 3)) * kcal / mol
-                self.results['forces'] = forces
+                    self.eigenvalues = np.array(eigs).reshape(1, 1, -1)
 
-    def get_eigenvalues(self, spin=0):
-        kpt = 0
+    def get_eigenvalues(self, kpt=0, spin=0):
+        print (self.eigenvalues.shape)
         return self.eigenvalues[spin, kpt]
+
+    def get_homo_lumo_levels(self):
+        if self.nspins == 1:
+            n = self.no_occ_levels
+            eigs = self.eigenvalues
+            return np.array([eigs[0, 0, n - 1], eigs[0, 0, n]])
+        else:
+            na = self.no_alpha_electrons
+            nb = self.no_beta_electrons
+            eigs = self.eigenvalues
+            eah, eal = eigs[0, 0, na - 1: na + 1]
+            ebh, ebl = eigs[1, 0, nb - 1: nb + 1]
+            return np.array([max(eah, ebh), min(eal, ebl)])
+
+    def get_somo_levels(self):
+        assert self.nspins == 2
+        eigs = self.eigenvalues
+        na, nb = self.no_alpha_electrons, self.no_beta_electrons
+        return np.array([eigs[0, 0, na - 1], eigs[1, 0, nb - 1]])
+
+    def get_final_heat_of_formation(self):
+        return self.final_hof
