25899b2257d63eef0c46d47bdfec907c7b986db1
Erik Fransson <karl.erik.fransson@gmail.com>
2016-11-22 18:05:45 +0100

    MAINT: Added some ValueErrors for IO functions that can only handle certain pbc conditions.

--- ase/io/dmol.py
+++ ase/io/dmol.py
@@ -1,9 +1,34 @@
+"""
+IO functions for DMol3 file formats.
 
 
-# IO functions
-#-------------------
+The formats follow some very strict formatting
 
-#TODO : Document properly what is supported and what is not
+Car
+----
+col: 1-5     atom name    
+col: 7-20    x Cartesian coordinate of atom  in A
+col: 22-35   y Cartesian coordinate of atom  in A
+col: 37-50   z Cartesian coordinate of atom  in A
+col: 52-55   type of residue containing atom      
+col: 57-63   residue sequence name   relative to beginning of current molecule, 
+                left justified
+col: 64-70   potential type of atom  left justified
+col: 72-73   element symbol   
+col: 75-80   partial charge on atom
+
+Incoor
+------
+$cell vectors
+             37.83609647462165    0.00000000000000    0.00000000000000
+              0.00000000000000   37.60366016124745    0.00000000000000
+              0.00000000000000    0.00000000000000   25.29020473078921
+$coordinates
+Si           15.94182672614820    1.85274838936809   16.01426481346124
+Si            4.45559370448989    2.68957177851318   -0.05326937257442
+$end
+
+"""
 
 from datetime import datetime
 import numpy as np
@@ -16,19 +41,10 @@ from ase.units import Hartree, Bohr
 def write_dmol_car(filename, atoms):
     """ Write a dmol car-file from an Atoms object
     
+    Notes
+    -----
     Can not handle multiple images.
-    Only allows for pbc 111 or 000
-
-    atoms:
-  	1-5 	atom name 	 
-  	7-20 	x Cartesian coordinate of atom 	in A
-  	22-35 	y Cartesian coordinate of atom 	in A
-  	37-50 	z Cartesian coordinate of atom 	in A
-  	52-55 	type of residue containing atom 	 
-  	57-63 	residue sequence name 	relative to beginning of current molecule, left justified
-  	64-70 	potential type of atom 	left justified
-  	72-73 	element symbol 	 
-  	75-80 	partial charge on atom
+    Only allows for pbc 111 or 000.
     """
 
     f = open(filename,'w')
@@ -44,7 +60,7 @@ def write_dmol_car(filename, atoms):
         f.write('PBC=OFF\n\n')
         f.write('!DATE     %s\n'% dt.strftime('%b %d %H:%m:%S %Y') )
     else:
-        raise RuntimeError('PBC must be all true or all false')
+        raise ValueError('PBC must be all true or all false for .car format')
     for i,a in enumerate(atoms):
         f.write('%-6s  %12.8f   %12.8f   %12.8f XXXX 1      xx      %-2s  0.000\n'%(
                a.symbol+str(i+1),a.x, a.y, a.z, a.symbol))
@@ -53,7 +69,12 @@ def write_dmol_car(filename, atoms):
 
 
 def read_dmol_car(filename):
-    """ Read a dmol car-file and return an Atoms object. """
+    """ Read a dmol car-file and return an Atoms object. 
+
+    Notes
+    -----
+    Cell is constructed from cellpar so orientation of cell might be off.
+    """
 
     try:
         lines = open(filename, 'r').readlines()
@@ -78,27 +99,20 @@ def read_dmol_car(filename):
         print 'Could not read car file %s'%filename
 
 
-
 def write_dmol_incoor(filename, atoms, bohr=True):
     """ Write a dmol incoor-file from an Atoms object
     
+    Notes
+    -----
     Only used for pbc 111.
     Can not handle multiple images.
-    
-    NB: incoor file is automatically deleted by DMol3
-
-    $cell vectors
-                 37.83609647462165    0.00000000000000    0.00000000000000
-                  0.00000000000000   37.60366016124745    0.00000000000000
-                  0.00000000000000    0.00000000000000   25.29020473078921
-    $coordinates
-    Si           15.94182672614820    1.85274838936809   16.01426481346124
-    Si            4.45559370448989    2.68957177851318   -0.05326937257442
-    $end
-
+    DMol3 expect data in .incoor files to be in bohr, if bohr is false however
+    the data is written in Angstroms.
     """
     
-    
+    if not np.all(atoms.pbc):
+        raise ValueError('PBC must be all true for .incoor format')
+
     if bohr:
         cell = atoms.cell / Bohr
         positions = atoms.positions / Bohr
@@ -107,7 +121,6 @@ def write_dmol_incoor(filename, atoms, bohr=True):
         positions = atoms.positions
 
     f = open(filename,'w')
-    
     f.write('$cell vectors\n')
     f.write('            %18.14f  %18.14f  %18.14f\n'%(
            cell[0,0], cell[0,1],cell[0,2]))
@@ -124,9 +137,14 @@ def write_dmol_incoor(filename, atoms, bohr=True):
     f.close()
 
 
-
 def read_dmol_incoor(filename, bohr=True):
-    """ Reads an incoor file and returns an atoms object. """
+    """ Reads an incoor file and returns an atoms object.
+    
+    Notes
+    -----
+    If bohr is True then incoor is assumed to be in bohr and the data
+    is rescaled to Angstrom.
+    """
     
     try:
         lines = open(filename, 'r').readlines()
@@ -152,6 +170,37 @@ def read_dmol_incoor(filename, bohr=True):
         atoms.positions = atoms.positions * Bohr
     return atoms
 
+
+def write_dmol_arc(filename, images):
+    """ Writes all images to file filename in arc format. 
+
+    Similar to the .car format only pbc 111 or 000 is supported.
+    """
+
+    f = open(filename,'w')
+    f.write('!BIOSYM archive 3\n')
+    if np.all(images[0].pbc):
+        f.write('PBC=ON\n\n')
+    elif not np.any(images[0].pbc):
+        f.write('PBC=OFF\n\n')
+    else:
+        raise ValueError('PBC must be all true or all false for .arc format')
+    for atoms in images:
+        dt = datetime.now()
+        if np.all(atoms.pbc):    
+            a,b,c,alpha,beta,gamma = cell_to_cellpar(atoms.get_cell())
+            f.write('!DATE     %s\n'% dt.strftime('%b %d %H:%m:%S %Y') )
+            f.write('PBC %9.5f %9.5f %9.5f %9.5f %9.5f %9.5f\n'%(a,b,c,alpha,beta,gamma))
+        elif not np.any(atoms.pbc): # [False,False,False]
+            f.write('!DATE     %s\n'% dt.strftime('%b %d %H:%m:%S %Y') )
+        else:
+            raise ValueError('PBC must be all true or all false for .arc format')
+        for i,a in enumerate(atoms):
+            f.write('%-6s  %12.8f   %12.8f   %12.8f XXXX 1      xx      %-2s  0.000\n'%(
+                   a.symbol+str(i+1),a.x, a.y, a.z, a.symbol))
+        f.write('end\nend\n')
+        f.write('\n')
+    f.close()
 
 
 def read_dmol_arc(filename, index=-1):
@@ -219,31 +268,3 @@ def read_dmol_arc(filename, index=-1):
                 if stop < 0:
                     stop += len(images)
         return [images[i] for i in range(start, stop, step)]
-
-def write_dmol_arc(filename, images):
-    """ Writes all images to file filename in arc format. """
-
-    f = open(filename,'w')
-    f.write('!BIOSYM archive 3\n')
-    if np.all(images[0].pbc):
-        f.write('PBC=ON\n\n')
-    elif not np.any(images[0].pbc):
-        f.write('PBC=OFF\n\n')
-    else:
-        raise RuntimeError('PBC must be all true or all false')
-    for atoms in images:
-        dt = datetime.now()
-        if np.all(atoms.pbc):    
-            a,b,c,alpha,beta,gamma = cell_to_cellpar(atoms.get_cell())
-            f.write('!DATE     %s\n'% dt.strftime('%b %d %H:%m:%S %Y') )
-            f.write('PBC %9.5f %9.5f %9.5f %9.5f %9.5f %9.5f\n'%(a,b,c,alpha,beta,gamma))
-        elif not np.any(atoms.pbc): # [False,False,False]
-            f.write('!DATE     %s\n'% dt.strftime('%b %d %H:%m:%S %Y') )
-        else:
-            raise RuntimeError('PBC must be all true or all false')
-        for i,a in enumerate(atoms):
-            f.write('%-6s  %12.8f   %12.8f   %12.8f XXXX 1      xx      %-2s  0.000\n'%(
-                   a.symbol+str(i+1),a.x, a.y, a.z, a.symbol))
-        f.write('end\nend\n')
-        f.write('\n')
-    f.close()
