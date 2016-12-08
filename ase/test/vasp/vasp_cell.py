6b8eadf39002cb805ed1d64e5024c067378519f1
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2016-12-08 11:25:53 +0100

    Use must_raise() context manager

--- ase/test/vasp/vasp_cell.py
+++ ase/test/vasp/vasp_cell.py
@@ -6,16 +6,13 @@ Check the unit cell is handled correctly
 
 from ase.calculators.vasp import Vasp
 from ase.build import molecule
+from ase.test import must_raise
 
 # Molecules come with no unit cell
 
 atoms = molecule('CH4')
 calc = Vasp()
 
-try:
-    atoms.set_calculator(calc)    
+with must_raise(ValueError):
+    atoms.set_calculator(calc)
     atoms.get_total_energy()
-except ValueError:
-    pass
-else:
-    raise AssertionError()
