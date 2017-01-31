390178e1d1d2dc2cce7650f2691ffcd200752a3b
tdd20@cam.ac.uk <tdd20@cam.ac.uk>
2017-01-31 15:57:28 +0000

    ENG: add units members to __all__ & attribute access
    The expected memebrs of the units module are added to
    __all__ so they can be picked up by introspection and
    do not appear as missing attributes.
    
    The units dict from create_units is now a subclass that
    implements accessing values with attributes so that it
    can be a drop in replacement for the module with
    new_units.eV and new_units['eV'] being equivalent.

--- ase/units.py
+++ ase/units.py
@@ -1,7 +1,29 @@
 from math import pi, sqrt
 
+# Define all the expected symbols so that introspection will know that
+# they exist when the module is imported.
+__all__ = ['__codata_version__', 'create_units', 'CODATA',
+           '_Grav', '_Nav', '_amu', '_auf', '_aup', '_aut', '_auv', '_c',
+           '_e', '_eps0', '_hbar', '_hplanck', '_k', '_me', '_mp', '_mu0',
+           'A', 'AUT', 'Ang', 'Angstrom', 'Bohr', 'C', 'Debye', 'GPa',
+           'Ha', 'Hartree', 'J', 'Pascal', 'Ry', 'Rydberg',
+           'alpha', 'eV', 'fs', 'invcm', 'kB', 'kJ', 'kcal', 'kg', 'm',
+           'mol', 'nm', 's', 'second']
+
+
 # the version we actually use
 __codata_version__ = '2014'
+
+
+# Instead of a plain dict, if the units are in the __dict__ of a
+# dict subclass, they can be accessed as attributes in a similar way
+# to a module.
+class Units(dict):
+    """Dictionary for units that supports .attribute access."""
+    def __init__(self, *args, **kwargs):
+        super(Units, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
 
 # this is the hard-coded CODATA values
 # all other units are dynamically derived from these values upon import of the
@@ -21,7 +43,7 @@ CODATA = {
              '_Nav': 6.0221367e23,          # Avogadro number
              '_k': 1.380658e-23,            # Boltzmann constant, J/K
              '_amu': 1.6605402e-27},         # atomic mass unit, kg
-    
+
     # CODATA 1998 taken from
     # http://dx.doi.org/10.1103/RevModPhys.72.351
     '1998': {'_c': 299792458.,
@@ -34,7 +56,7 @@ CODATA = {
              '_Nav': 6.02214199e23,
              '_k': 1.3806503e-23,
              '_amu': 1.66053873e-27},
-    
+
     # CODATA 2002 taken from
     # http://dx.doi.org/10.1103/RevModPhys.77.1
     '2002': {'_c': 299792458.,
@@ -47,7 +69,7 @@ CODATA = {
              '_Nav': 6.0221415e23,
              '_k': 1.3806505e-23,
              '_amu': 1.66053886e-27},
-    
+
     # CODATA 2006 taken from
     # http://dx.doi.org/10.1103/RevModPhys.80.633
     '2006': {'_c': 299792458.,
@@ -60,7 +82,7 @@ CODATA = {
              '_Nav': 6.02214179e23,
              '_k': 1.3806504e-23,
              '_amu': 1.660538782e-27},
-    
+
     # CODATA 2010 taken from
     # http://dx.doi.org/10.1103/RevModPhys.84.1527
     '2010': {'_c': 299792458.,
@@ -73,7 +95,7 @@ CODATA = {
              '_Nav': 6.02214129e23,
              '_k': 1.3806488e-23,
              '_amu': 1.660538921e-27},
-    
+
     # CODATA 2014 taken from
     # http://arxiv.org/pdf/1507.07956.pdf
     '2014': {'_c': 299792458.,
@@ -91,14 +113,15 @@ CODATA = {
 def create_units(codata_version):
     """
     Function that creates a dictionary containing all units previously hard
-    coded in ase.units depending on a certain CODATA version. Note that you can
-    use the dictionary returned to update your local or global namespace.
+    coded in ase.units depending on a certain CODATA version. Note that
+    returned dict has attribute access it can be used in place of the module
+    or to update your local or global namespace.
 
     Parameters:
 
     codata_version: str
         The CODATA version to be used. Implemented are
-        
+
         * '1986'
         * '1998'
         * '2002'
@@ -110,7 +133,7 @@ def create_units(codata_version):
 
     units: dict
         Dictionary that contains all formerly hard coded variables from
-        ase.units as key-value pairs.
+        ase.units as key-value pairs. The dict supports attribute access.
 
     Raises:
 
@@ -119,7 +142,7 @@ def create_units(codata_version):
     """
 
     try:
-        u = dict(CODATA[codata_version])
+        u = Units(CODATA[codata_version])
     except KeyError:
         raise NotImplementedError('CODATA version "{0}" not implemented'
                                   .format(__codata_version__))
