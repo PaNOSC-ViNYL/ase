55a2808158f9248f4d2a0235c9b6497408269082
Ask Hjorth Larsen <asklarsen@gmail.com>
2017-02-13 11:08:46 +0100

    calculator now raises NotImplementedError when asked for free_energy and no free energy is provided

--- ase/calculators/calculator.py
+++ ase/calculators/calculator.py
@@ -421,6 +421,11 @@ class Calculator:
     def get_potential_energy(self, atoms=None, force_consistent=False):
         energy = self.get_property('energy', atoms)
         if force_consistent:
+            if 'free_energy' not in self.results:
+                name = self.__class__.__name__
+                raise NotImplementedError('Force consistent/free energy '
+                                          'not provided by {} '
+                                          'calculator'.format(name))
             return self.results['free_energy']
         else:
             return energy
