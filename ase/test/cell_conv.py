ca5cddebf9c683fe13b3c1bd25e6f4a64d6a761a
Gaël Donval <gdonval+gitlab@gmail.com>
2017-02-24 17:01:55 +0000

    Add test for cell parameter <-> tensor conversions

---
+++ ase/test/cell_conv.py
@@ -0,0 +1,70 @@
+import numpy as np
+from ase.build import bulk
+from ase.geometry import cell_to_cellpar as c2p, cellpar_to_cell as p2c
+
+eps = 2 * np.spacing(90., dtype=np.float64)
+def nearly_equal(a, b):
+    return np.all(np.abs(b-a) < eps)
+
+# Constants
+a = 5.43
+d = a / 2.0
+h = a / np.sqrt(2.0)
+
+
+# Systems
+## Primitive cell, non-orthorhombic, non-cubic
+### Parameters
+si_prim_p  = np.array([h] * 3 +  [60.] * 3)
+### Tensor format
+si_prim_m  = np.array([[0.,  d,  d],
+                       [ d, 0.,  d],
+                       [ d,  d, 0.]])
+### Tensor format in the default basis
+si_prim_m2 = np.array([[2.0, 0., 0.],
+                       [1.0, np.sqrt(3.0), 0.],
+                       [1.0, np.sqrt(3.0)/3.0, 2*np.sqrt(2/3)]])
+si_prim_m2 *= h/2.0
+
+
+## Orthorhombic cell, non-cubic
+### Parameters
+si_ortho_p = np.array([h] * 2 +  [a] +  [90.] * 3)
+### Tensor format in the default basis
+si_ortho_m = np.array([[h, 0.0, 0.0],
+                       [0.0, h, 0.0],
+                       [0.0, 0.0, a]])
+
+## Cubic cell
+### Parameters
+si_cubic_p = np.array([a] * 3 +  [90.] * 3)
+### Tensor format in the default basis
+si_cubic_m =  np.array([[a, 0.0, 0.0],
+                       [0.0, a, 0.0],
+                       [0.0, 0.0, a]])
+
+# Cell matrix -> cell parameters
+assert nearly_equal(c2p(si_prim_m), si_prim_p)
+assert nearly_equal(c2p(si_prim_m2), si_prim_p)
+assert nearly_equal(c2p(si_ortho_m), si_ortho_p)
+assert nearly_equal(c2p(si_cubic_m), si_cubic_p)
+assert not nearly_equal(c2p(si_prim_m), si_ortho_p)
+
+# Cell parameters -> cell matrix
+assert nearly_equal(p2c(si_prim_p), si_prim_m2)
+assert nearly_equal(p2c(si_ortho_p), si_ortho_m)
+assert nearly_equal(p2c(si_cubic_p), si_cubic_m)
+assert not nearly_equal(p2c(si_prim_p), si_ortho_m)
+
+# Idempotency (provided everything is provided in the default basis)
+ref1 = si_prim_m2[:]
+ref2 = si_ortho_m[:]
+ref3 = si_cubic_m[:]
+for i in range(20):
+    ref1[:] = p2c(c2p(ref1))
+    ref2[:] = p2c(c2p(ref2))
+    ref3[:] = p2c(c2p(ref3))
+
+assert nearly_equal(ref1, si_prim_m2)
+assert nearly_equal(ref2, si_ortho_m)
+assert nearly_equal(ref3, si_cubic_m)
