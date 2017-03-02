Fixed some bugs in Quaternions from Euler angles methods, added test case

ase/quaternions.py
@@ -138,7 +138,7 @@ class Quaternion:
         else:
             raise ValueError('Invalid Euler angles mode {0}'.format(mode))
 
-        return a, b, c
+        return np.array([a, b, c])
 
     def arc_distance(self, other):
         """Gives a metric of the distance between two quaternions,
@@ -235,4 +235,4 @@ class Quaternion:
         else:
             raise ValueError('Invalid Euler angles mode {0}'.format(mode))
 
-        return q_a*q_b*q_c
+        return q_c*q_b*q_a

ase/test/quaternions.py
@@ -2,25 +2,45 @@ import numpy as np
 from ase.quaternions import Quaternion
 
 
-def rand_rotm():
-    """Axis & angle rotations."""
-    u = np.random.random(3)
+def axang_rotm(u, theta):
+
+    u = np.array(u).astype(np.float)
     u /= np.linalg.norm(u)
-    theta = np.random.random() * np.pi * 2
-    
+
     # Cross product matrix for u
     ucpm = np.array([[0, -u[2], u[1]], [u[2], 0, -u[0]], [-u[1], u[0], 0]])
-    
+
     # Rotation matrix
     rotm = (np.cos(theta) * np.identity(3) + np.sin(theta) * ucpm +
             (1 - np.cos(theta)) * np.kron(u[:, None], u[None, :]))
-    
+
     return rotm
-    
+
+
+def rand_rotm():
+    """Axis & angle rotations."""
+    u = np.random.random(3)
+    theta = np.random.random() * np.pi * 2
+
+    return axang_rotm(u, theta)
+
+
+def eulang_rotm(a, b, c, mode='zyz'):
+
+    rota = axang_rotm([0, 0, 1], a)
+    rotc = axang_rotm([0, 0, 1], c)
+
+    if mode == 'zyz':
+        rotb = axang_rotm([0, 1, 0], b)
+    elif mode == 'zxz':
+        rotb = axang_rotm([1, 0, 0], b)
+
+    return np.dot(rotc, np.dot(rotb, rota))
+
 # First: test that rotations DO work
 for i in range(10):
     # 10 random tests
-        
+
     rotm = rand_rotm()
 
     q = Quaternion.from_matrix(rotm)
@@ -47,14 +67,29 @@ for i in range(10):
 
     rotm1 = rand_rotm()
     rotm2 = rand_rotm()
-    
+
     q1 = Quaternion.from_matrix(rotm1)
     q2 = Quaternion.from_matrix(rotm2)
-    
+
     # Now test this with a vector
     v = np.random.random(3)
-    
+
     vrotM = np.dot(rotm2, np.dot(rotm1, v))
     vrotQ = (q2 * q1).rotate(v)
-    
+
     assert np.allclose(vrotM, vrotQ)
+
+# Fourth: test Euler angles
+for mode in ['zyz', 'zxz']:
+    for i in range(10):
+
+        abc = np.random.random(3)*2*np.pi
+        v2 = np.random.random((2, 3))  # Two random vectors to rotate rigidly
+
+        q_eul = Quaternion.from_euler_angles(*abc, mode=mode)
+        rot_eul = eulang_rotm(*abc, mode=mode)
+
+        v2_q = np.array([q_eul.rotate(v) for v in v2])
+        v2_m = np.array([np.dot(rot_eul, v) for v in v2])
+
+        assert np.allclose(v2_q, v2_m)