d036e982e6c544a994bd4857c6dbb97876a8bfc7
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2017-03-03 12:44:28 +0100

    Add angleHOH constant in degrees

--- ase/calculators/tip3p.py
+++ ase/calculators/tip3p.py
@@ -10,7 +10,7 @@ qH = 0.417
 sigma0 = 3.15061
 epsilon0 = 0.1521 * units.kcal / units.mol
 rOH = 0.9572
-thetaH2O = 104.52
+angleHOH = 104.52
 thetaHOH = 104.52 / 180 * np.pi  # we keep this for backwards compatibility
 
 
