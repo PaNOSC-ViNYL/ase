cae73c1f95971750b9b6de4e4f6c386679b4f442
Miguel Caro <mcaroba@gmail.com>
2017-04-11 05:13:30 +0000

    Update vdwcorrection.py

--- ase/calculators/vdwcorrection.py
+++ ase/calculators/vdwcorrection.py
@@ -171,7 +171,7 @@ class vdWTkatchenko09prl(Calculator):
             try:
                 xc_name = self.calculator.hamiltonian.xc.kernel.name
                 self.sR = sR_opt[xc_name]
-            except:
+            except KeyError:
                 raise ValueError('Tkatchenko-Scheffler dispersion correction not implemented for %s functional' % xc_name)
         else:
             self.sR = sR
