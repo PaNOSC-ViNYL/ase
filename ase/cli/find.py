8d7d08fb9a7a439b3e6a1e6a1a34cbdb4273de87
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2017-04-12 20:10:06 +0200

    Add find command

---
+++ ase/cli/find.py
@@ -0,0 +1,38 @@
+import os
+import os.path as op
+
+from ase.io import read
+from ase.io.formats import filetype
+from ase.db.core import parse_query, check_xxx
+
+
+class CLICommand:
+    short_description = 'Find files with atoms in.'
+
+    @staticmethod
+    def add_arguments(parser):
+        parser.add_argument('folder')
+        parser.add_argument('query')
+        parser.add_argument('-v', '--verbose', action='store_true')
+
+    @staticmethod
+    def run(args):
+        main(args)
+
+
+def main(args):
+    keys, cmps = parse_query(...)
+
+    def check(atoms):
+        return check_xxx(atoms, keys, cmps)
+
+    for dirpath, dirnames, filenames in os.walk(args.folder):
+        for name in filenames:
+            path = op.join(dirpath, name)
+            try:
+                filetype(path, guess=False)
+            except:
+                continue
+            atoms = read(path)
+            if check(atoms):
+                print(path)
