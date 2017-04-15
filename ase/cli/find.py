4ac81646582f254b3e86653b8fcd5eda6d8bb45d
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2017-04-15 20:54:46 +0200

    ...

--- ase/cli/find.py
+++ ase/cli/find.py
@@ -1,9 +1,10 @@
+import multiprocessing as mp
 import os
 import os.path as op
 
 from ase.io import read
 from ase.io.formats import filetype
-from ase.db.core import parse_query, check_xxx
+# from ase.db.core import parse_query, check_xxx
 
 
 class CLICommand:
@@ -21,30 +22,30 @@ class CLICommand:
 
 
 def main(args):
-    keys, cmps = parse_query(...)
+    # keys, cmps = parse_query(...)
 
-    pool=...
-    paths = Queue()
-    result = Queue()
-
-    def check(paths):
-        path= paths.get()
-        try:
-            filetype(path, guess=False)
-        except:
-            continue
-        atoms = read(path)
-        if check(atoms):
-            print(path)
-        return check_xxx(atoms, keys, cmps)
+    paths = mp.Queue()
+    results = mp.Queue()
 
     for dirpath, dirnames, filenames in os.walk(args.folder):
         for name in filenames:
             path = op.join(dirpath, name)
             paths.put(path)
-
         # Skip .git and friends:
         dirnames[:] = (name for name in dirnames if name[0] != '.')
 
+    with mp.Pool() as pool:
+        pool.apply(check, [paths, results])
+
     print(results)
-    
+
+
+def check(paths, results):
+    for path in iter(paths.get, None):
+        try:
+            filetype(path)
+        except ValueError:
+            continue
+        # atoms = read(path)
+        if 1:  # check(atoms):
+            results.put(path)
