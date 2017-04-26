e561095afa76368d2fb8cc36221079c6f1ecce41
Asmus O. Dohn <asod@hi.is>
2017-04-26 14:14:15 +0000

    fixed tip4p test

--- ase/test/tip4p.py
+++ ase/test/tip4p.py
@@ -2,10 +2,10 @@
 from math import cos, sin
 
 from ase import Atoms
-from ase.calculators.tip4p import TIP4P, rOH, thetaHOH
+from ase.calculators.tip4p import TIP4P, rOH, angleHOH
 
 r = rOH
-a = thetaHOH
+a = angleHOH
 
 dimer = Atoms('H2OH2O',
               [(r * cos(a), 0, r * sin(a)),
@@ -14,8 +14,12 @@ dimer = Atoms('H2OH2O',
                (r * cos(a / 2), r * sin(a / 2), 0),
                (r * cos(a / 2), -r * sin(a / 2), 0),
                (0, 0, 0)])
-dimer.calc = TIP4P(rc=4.0, width=2.0)  # put O-O distance in the cutoff range
+
+# tip4p sequence OHH, OHH, ..
+dimer = dimer[[2]]+dimer[:2]+dimer[[-1]]+dimer[3:5]
 dimer.positions[3:, 0] += 2.8
+
+dimer.calc = TIP4P(rc=4.0, width=2.0)  # put O-O distance in the cutoff range
 F = dimer.get_forces()
 print(F)
 dF = dimer.calc.calculate_numerical_forces(dimer) - F
