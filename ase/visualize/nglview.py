343f275fd2810e3d87efa60260c47f6f91baa312
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2017-05-09 16:40:19 +0200

    Add skeletal ngl viewer to visualize module

---
+++ ase/visualize/nglview.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+
+import numpy as np
+from ase import Atoms, units
+import ase.io
+from ase.io.trajectory import Trajectory
+from ipywidgets import HBox, VBox, Checkbox, Dropdown, IntSlider, FloatSlider
+from io import BytesIO
+import nglview 
+import glob
+
+def view_ngl( atoms ):
+    return 
