87d6ed0afe270d357eb144755796ee518f1217ed
Magnus Nord <Magnus.Nord@glasgow.ac.uk>
2017-05-25 15:59:23 +0100

    IO matplotlib: add docstring

--- ase/io/matplotlib.py
+++ ase/io/matplotlib.py
@@ -27,6 +27,26 @@ class MATPLOTLIB:
 
 
 def write_matplotlib(atoms, ax, **parameters):
+    """Plot an atoms object in a matplotlib subplot.
+
+    Parameters
+    ----------
+    atoms : Atoms object
+    ax : Matplotlib subplot object
+    rotation : str, optional
+        In degrees. In the form '10x,20y,30z'
+    show_unit_cell : bool, optional, default False
+        Draw the bounds of the atoms object as dashed lines.
+    radii : float, optional
+        The radii of the atoms
+    colors : list of strings, optional
+        Color of the atoms, must be the same length as
+        the number of atoms in the atoms object.
+    scale : float, optional
+        Scaling of the plotted atoms and lines.
+    offset : tuple (float, float), optional
+        Offset of the plotted atoms and lines.
+    """
     if isinstance(atoms, list):
         assert len(atoms) == 1
         atoms = atoms[0]
