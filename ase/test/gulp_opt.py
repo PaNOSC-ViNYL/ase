963ce28bbe9ad9cefe4dc8e00021be3938e0d046
Ask Hjorth Larsen <asklarsen@gmail.com>
2017-05-31 19:15:51 +0200

    draft of gulp/ase optimizer test

---
+++ ase/test/gulp_opt.py
@@ -0,0 +1,16 @@
+from ase.calculators.gulp import GULP
+from ase.optimize import BFGS
+from ase.build import bulk
+
+atoms = bulk('Si').repeat((4, 4, 4))
+
+atoms1 = atoms.copy()
+atoms1.calc = GULP()
+opt1 = BFGS(atoms1)
+opt1.run(fmax=0.05)
+
+atoms2 = atoms.copy()
+opt2 = GULP.get_optimizer(atoms2)
+opt2.run(fmax=0.05)
+
+assert np.abs(opt1.positions - opt2.positions).max() < 1e-5
