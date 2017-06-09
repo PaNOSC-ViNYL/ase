f830657819fdbf02b708a57c0309a961a39a4e2a
Ask Hjorth Larsen <asklarsen@gmail.com>
2017-06-09 17:11:28 +0200

    write required pdb file.  Fixes: #102

--- ase/test/amber/amber.py
+++ ase/test/amber/amber.py
@@ -12,6 +12,18 @@ from ase.test import require
 
 require('amber')
 
+with open('2h2o.pdb', 'w') as outfile:
+    outfile.write("""\
+ATOM      1  O   HOH     1       0.000   0.000   0.000  1.00  0.00           O
+ATOM      3  H1  HOH     1      -0.241  -0.971   0.000  1.00  0.00           H
+ATOM      6  H2  HOH     1       1.050   0.100   0.100  1.00  0.00           H
+TER
+ATOM      2  O   HOH     2       2.200   0.000   0.000  1.00  0.00           O
+ATOM      4  H2  HOH     2       2.459   0.966   0.000  1.00  0.00           H
+ATOM      5  H1  HOH     2       2.459  -0.483   0.837  1.00  0.00           H
+END
+""")
+
 with open('mm.in', 'w') as outfile:
     outfile.write("""\
 zero step md to get energy and force
