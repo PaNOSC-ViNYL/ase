b6f747c8edb6cd4fd226ba60a598dfb4a2d9d92a
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2017-06-29 13:12:51 +0200

    Fix for label=None and also show labels

--- ase/dft/band_structure.py
+++ ase/dft/band_structure.py
@@ -60,11 +60,12 @@ class BandStructure:
         return BandStructure(**dct)
 
     def plot(self, ax=None, spin=None, emin=-10, emax=5, filename=None,
-             show=None, ylabel=None, colors=None, label=None, **plotkwargs):
+             show=None, ylabel=None, colors=None, label=None,
+             spin_labels=['spin up', 'spin down'], **plotkwargs):
         """Plot band-structure.
 
         spin: int or None
-            Spin channel.  Default behaviour is to plot both spi up and down
+            Spin channel.  Default behaviour is to plot both spin up and down
             for spin-polarized calculations.
         emin,emax: float
             Maximum energy above reference.
@@ -91,21 +92,24 @@ class BandStructure:
                 colors = 'yb'
 
         nspins = len(e_skn)
-        if nspins == 2:
-            spin_labels = [' spin up', ' spin down']
 
         for spin, e_kn in enumerate(e_skn):
             color = colors[spin]
             kwargs = dict(color=color)
             kwargs.update(plotkwargs)
             if nspins == 2:
-                ax.plot(self.xcoords, e_kn[:, 0], label=label+spin_labels[spin], **kwargs)
+                if label:
+                    lbl = label + ' ' + spin_labels[spin]
+                else:
+                    lbl = spin_labels[spin]
             else:
-                ax.plot(self.xcoords, e_kn[:, 0], label=label, **kwargs)
+                lbl = label
+            ax.plot(self.xcoords, e_kn[:, 0], label=lbl, **kwargs)
             for e_k in e_kn.T[1:]:
                 ax.plot(self.xcoords, e_k, **kwargs)
 
-        self.finish_plot(filename, show)
+        legend = label is not None or nspins == 2
+        self.finish_plot(filename, show, legend)
 
         return ax
 
@@ -168,8 +172,11 @@ class BandStructure:
         self.ax = ax
         return ax
 
-    def finish_plot(self, filename, show):
+    def finish_plot(self, filename, show, legend=False):
         import matplotlib.pyplot as plt
+
+        if legend:
+            plt.legend()
 
         if filename:
             plt.savefig(filename)
