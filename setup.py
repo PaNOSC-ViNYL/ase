30d52f44a4fc3d13258c6ecd2e968485fd03e953
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2017-08-09 10:26:27 +0200

    Clean up setup.py a bit

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2007  CAMP
+# Copyright (C) 2007-2017  CAMd
 # Please see the accompanying LICENSE file for further information.
 
 import os
@@ -58,13 +58,7 @@ class build_py(_build_py):
         return _build_py.get_outputs(self, *args, **kwargs) + self.mofiles
 
 
-name = 'ase'  # PyPI name
-
-# Linux-distributions may want to change the name:
-if 0:
-    name = 'python-ase'
-
-setup(name=name,
+setup(name='ase',
       version=version,
       description='Atomic Simulation Environment',
       url='https://wiki.fysik.dtu.dk/ase',
@@ -74,7 +68,6 @@ setup(name=name,
       platforms=['unix'],
       packages=find_packages(),
       install_requires=['numpy', 'scipy', 'matplotlib', 'flask'],
-      # package_dir=package_dir,
       package_data=package_data,
       entry_points={'console_scripts': ['ase=ase.cli.main:main',
                                         'ase-db=ase.cli.main:old',
