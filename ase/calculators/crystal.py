74afbbf3cb6aea9a4b0d6e30062b0490ae036dc5
gianlu6022 <gianlu6022@gmail.com>
2017-08-25 15:36:26 +0200

    FIX: Deleted lines of dftb.py from crystal.py for clarity.

--- ase/calculators/crystal.py
+++ ase/calculators/crystal.py
@@ -11,10 +11,11 @@ and it will be updated during the crystal calculations.
 
 The keywords are given, for instance, as follows::
 
-    BLOCK3_SPIN ='YES',
-    BLOCK3_TOLDEE = 8,
-    BLOCK3_ANDERSON = 'YES',
-    BLOCK3_FMIXING = 95,
+    DFT_SPIN ='YES',
+    DFT_CORRELAT = 'B3',
+    TOLDEE = 8,
+    ANDERSON = 'YES',
+    FMIXING = 95,
     ... # put other examples
 
 """
@@ -36,8 +37,7 @@ class crystal(FileIOCalculator):
 
     implemented_properties = ['energy', 'forces']
 
-    def __init__(self, restart=None, ignore_bad_restart_file=False,
-                 label='cry', atoms=None, kpts=None,
+    def __init__(self, label='cry', atoms=None, kpts=None,
                  **kwargs):
         """Construct a crystal calculator.
 
@@ -47,7 +47,7 @@ class crystal(FileIOCalculator):
         if 'CRY_BASIS' in os.environ:
             basis_dir = os.environ['CRY_BASIS']
         else:
-            slako_dir = './'
+            basis_dir = './'
 
         # call crystal only to run a single point calculation
         # [PUT HERE DEFAULT PARAMETERS]
@@ -94,10 +94,6 @@ class crystal(FileIOCalculator):
         for key, value in sorted(self.parameters.items()):
             current_depth = key.rstrip('_').count('_')
             previous_depth = previous_key.rstrip('_').count('_')
-            for my_backsclash in reversed(
-                    range(previous_depth - current_depth)):
-                outfile.write(3 * (1 + my_backsclash) * myspace + '} \n')
-            outfile.write(3 * current_depth * myspace)
             if key.endswith('_'):
                 outfile.write(key.rstrip('_').rsplit('_')[-1] +
                               ' = ' + str(value) + '{ \n')
@@ -105,118 +101,8 @@ class crystal(FileIOCalculator):
                 outfile.write(str(value) + ' \n')
             else:
                 outfile.write(key.rsplit('_')[-1] + ' = ' + str(value) + ' \n')
-            if self.pcpot is not None and ('DFTB' in str(value)):
-                outfile.write('   ElectricField = { \n')
-                outfile.write('      PointCharges = { \n')
-                outfile.write(
-                    '         CoordsAndCharges [Angstrom] = DirectRead { \n')
-                outfile.write('            Records = ' +
-                              str(len(self.pcpot.mmcharges)) + ' \n')
-                outfile.write(
-                    '            File = "dftb_external_charges.dat" \n')
-                outfile.write('         } \n')
-                outfile.write('      } \n')
-                outfile.write('   } \n')
             previous_key = key
         current_depth = key.rstrip('_').count('_')
         for my_backsclash in reversed(range(current_depth)):
             outfile.write(3 * my_backsclash * myspace + '} \n')
-        # output to 'results.tag' file (which has proper formatting)
-        outfile.write('Options { \n')
-        outfile.write('   WriteResultsTag = Yes  \n')
-        outfile.write('} \n')
-        outfile.write('ParserOptions { \n')
-        outfile.write('   IgnoreUnprocessedNodes = Yes  \n')
-        outfile.write('} \n')
-
-        outfile.close()
-
-    def write_input(self, atoms, properties=None, system_changes=None):
-        from ase.io import write
-        FileIOCalculator.write_input(
-            self, atoms, properties, system_changes)
-        self.write_crystal_in(os.path.join(self.directory, 'INPUT'))
-        write(os.path.join(self.directory, 'fort.34'), atoms)
-        # self.atoms is none until results are read out,
-        # then it is set to the ones at writing input
-        self.atoms_input = atoms
-        self.atoms = None
-
-    def read_results(self):
-        """ all results are read from results.tag file
-            It will be destroyed after it is read to avoid
-            reading it once again after some runtime error """
-        from ase.units import Hartree, Bohr
-
-        myfile = open(os.path.join(self.directory, 'results.tag'), 'r')
-        self.lines = myfile.readlines()
-        myfile.close()
-
-        self.atoms = self.atoms_input
-        charges = self.read_charges()
-        self.results['charges'] = charges
-        energy = 0.0
-        forces = None
-        energy = self.read_energy()
-        forces = self.read_forces()
-        self.results['energy'] = energy
-        self.results['forces'] = forces
-        self.mmpositions = None
-        # stress stuff begins
-        sstring = 'stress'
-        have_stress = False
-        stress = list()
-        for iline, line in enumerate(self.lines):
-            if sstring in line:
-                have_stress = True
-                start = iline + 1
-                end = start + 3
-                for i in range(start, end):
-                    cell = [float(x) for x in self.lines[i].split()]
-                    stress.append(cell)
-        if have_stress:
-            stress = -np.array(stress) * Hartree / Bohr**3
-        elif not have_stress:
-            stress = np.zeros((3, 3))
-        self.results['stress'] = stress
-        # stress stuff ends
-
-        # calculation was carried out with atoms written in write_input
-        os.remove(os.path.join(self.directory, 'results.tag'))
-
-    def read_energy(self):
-        """Read Energy from dftb output file (results.tag)."""
-        from ase.units import Hartree
-        # Energy line index
-        for iline, line in enumerate(self.lines):
-            estring = 'total_energy'
-            if line.find(estring) >= 0:
-                index_energy = iline + 1
-                break
-        try:
-            return float(self.lines[index_energy].split()[0]) * Hartree
-        except:
-            raise RuntimeError('Problem in reading energy')
-
-    def read_forces(self):
-        """Read Forces from dftb output file (results.tag)."""
-        from ase.units import Hartree, Bohr
-
-        # Force line indexes
-        for iline, line in enumerate(self.lines):
-            fstring = 'forces   '
-            if line.find(fstring) >= 0:
-                index_force_begin = iline + 1
-                line1 = line.replace(':', ',')
-                index_force_end = iline + 1 + \
-                    int(line1.split(',')[-1])
-                break
-        try:
-            gradients = []
-            for j in range(index_force_begin, index_force_end):
-                word = self.lines[j].split()
-                gradients.append([float(word[k]) for k in range(0, 3)])
-            return np.array(gradients) * Hartree / Bohr
-        except:
-            raise RuntimeError('Problem in reading forces')
 
