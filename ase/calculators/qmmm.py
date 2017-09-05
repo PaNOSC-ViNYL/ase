9c1fe1af2e94af6263749c6720974ed02f7610ce
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2017-09-05 14:25:40 +0200

    Remove AMBER stuff not related to this MR

--- ase/calculators/qmmm.py
+++ ase/calculators/qmmm.py
@@ -70,17 +70,6 @@ class SimpleQMMM(Calculator):
 
         energy = self.qmcalc.get_potential_energy(self.qmatoms)
         qmforces = self.qmcalc.get_forces(self.qmatoms)
-
-        # update charges on QM atoms in MM2 and MM1 calculators (amber)
-        if (self.mmcalc1.name == 'amber'):
-            qmcharges = self.qmcalc.get_charges(self.qmatoms)
-            self.mmcalc1.set_charges(list(range(len(qmcharges))),
-                                     qmcharges, 'parmed_mm1.input')
-        if (self.mmcalc2.name == 'amber'):
-            qmcharges = self.qmcalc.get_charges(self.qmatoms)            
-            self.mmcalc2.set_charges(self.selection,
-                                     qmcharges, 'parmed_mm2.input')
-
         energy += self.mmcalc2.get_potential_energy(atoms)
         forces = self.mmcalc2.get_forces(atoms)
 
