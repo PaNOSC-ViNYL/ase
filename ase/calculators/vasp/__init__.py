475db9881a446fef463d1c00bc2788e8b865c744
Alexander Sougaard Tygesen <atygesen@hotmail.com>
2017-10-23 10:46:51 +0200

    Keep auxilary functions in vasp.py for now

--- ase/calculators/vasp/__init__.py
+++ ase/calculators/vasp/__init__.py
@@ -1,6 +1,5 @@
-from .vasp import Vasp
+from .vasp import Vasp, VaspChargeDensity, VaspDos, xdat2traj
 from .vasp_fileio import VaspFileIo
-from .vasp_auxilary import VaspChargeDensity, VaspDos, xdat2traj
 from .interactive import VaspInteractive
 __all__ = ['Vasp', 'VaspChargeDensity', 'VaspDos', 'xdat2traj',
            'VaspInteractive', 'VaspFileIo']
