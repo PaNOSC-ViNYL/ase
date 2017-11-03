2197c441535c3e1daa8cc3bd9c144c199aca1e16
Logan Ward <ward.logan.t@gmail.com>
2017-11-03 17:23:40 -0500

    BUG: Skip test if jupyter is not installed

--- ase/test/x3d.py
+++ ase/test/x3d.py
@@ -1,17 +1,21 @@
-from ase.visualize import x3d
 from ase import Atoms
-from IPython.display import HTML
 import os
 
-a = 3.6
-b = a / 2
-atoms = Atoms('Cu4',
-                positions=[(0, 0, 0),
-                           (0, b, b),
-                           (b, 0, b),
-                           (b, b, 0)],
-                cell=(a, a, a),
-                pbc=True)
-my_obj, my_tempfile = x3d.view_x3d(atoms, return_path=True)
-assert isinstance(my_obj, HTML)
-assert not os.path.isfile(my_tempfile)
+try:
+    from IPython.display import HTML
+    from ase.visualize import x3d
+except:
+    pass
+else:
+    a = 3.6
+    b = a / 2
+    atoms = Atoms('Cu4',
+                    positions=[(0, 0, 0),
+                               (0, b, b),
+                               (b, 0, b),
+                               (b, b, 0)],
+                    cell=(a, a, a),
+                    pbc=True)
+    my_obj, my_tempfile = x3d.view_x3d(atoms, return_path=True)
+    assert isinstance(my_obj, HTML)
+    assert not os.path.isfile(my_tempfile)
