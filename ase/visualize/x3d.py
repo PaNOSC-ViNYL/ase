bc1b77aa867a1f6b397cf5a732e300514a7eed22
Logan Ward <ward.logan.t@gmail.com>
2017-11-03 17:07:05 -0500

    BUG: Fixing X3D compatibility with Windows
    Problem with accessing a NamedTemporaryFile and Windows NT (I don't get
    it, I just know how to fix it). Also added tests that make sure the
    tempfile gets deleted properly

--- ase/visualize/x3d.py
+++ ase/visualize/x3d.py
@@ -1,14 +1,27 @@
 """Inline viewer for jupyter notebook using X3D."""
 
-from tempfile import NamedTemporaryFile
+from tempfile import NamedTemporaryFile, mkstemp
 from IPython.display import HTML
+import os
 
-
-def view_x3d(atoms):
+def view_x3d(atoms, return_path=False):
     """View atoms inline in a jupyter notbook. This command
-    should only be used within a jupyter/ipython notebook."""
-    with NamedTemporaryFile('r+', suffix='.html') as ntf:
-        atoms.write(ntf.name, format='html')
-        ntf.seek(0)
-        html_atoms = ntf.read()
+    should only be used within a jupyter/ipython notebook.
+    
+    Args:
+        return_path - bool, whether to return the path to the tempfile 
+            created during this operation [debug purposes]"""
+    
+    ntf_fp, ntf = mkstemp(suffix=".html")
+    try:
+        atoms.write(ntf, format='html')
+        with open(ntf, 'r') as fp:
+            html_atoms = fp.read()
+    finally:
+        os.close(ntf_fp)
+        os.remove(ntf)
+        
+    if return_path:
+        # Debug purposes
+        return HTML(html_atoms), ntf
     return HTML(html_atoms)
