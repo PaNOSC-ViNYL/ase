1ffc00b077349db00c1539cd00c34dbd1ebef6d2
Alexander Sougaard Tygesen <atygesen@hotmail.com>
2017-12-05 15:13:27 +0100

    ENH: Add calculator properties to quickinfo

--- ase/gui/quickinfo.py
+++ ase/gui/quickinfo.py
@@ -1,7 +1,10 @@
 "Module for displaying information about the system."
 
 from __future__ import unicode_literals
+
+import numpy as np
 from ase.gui.i18n import _
+from ase.calculators.calculator import PropertyNotImplementedError
 
 singleimage = _('Single image loaded.')
 multiimage = _('Image %d loaded (0 - %d).')
@@ -11,6 +14,7 @@ ucvaries = _('Unit cell varies.')
 format = _("""\
 %s
 
+%s
 Number of atoms: %d.
 
 Unit cell:
@@ -22,6 +26,10 @@ Unit cell:
 %s
 """)
 
+calc_format = """
+
+%s
+"""
 
 def info(gui):
     images = gui.images
@@ -54,9 +62,29 @@ def info(gui):
                     for periodic in atoms.pbc]
 
         # TRANSLATORS: This has the form Periodic: no, no, yes
+        formula = atoms.get_chemical_formula()
         pbcstring = _('Periodic: %s, %s, %s') % tuple(periodic)
-        txt = format % ((imgtxt, natoms) + tuple(uc.flat) +
+        txt = format % ((imgtxt, formula, natoms) + tuple(uc.flat) +
                         (pbcstring,) + (uctxt,))
         if atoms.number_of_lattice_vectors == 3:
             txt += _('Volume: ') + '{:8.3f}'.format(atoms.get_volume())
+
+        # Print electronic structure information if we have a calculator
+        if atoms.calc:
+            energy = _('Energy: ')
+            energy += '{:.3f} eV'.format(atoms.get_potential_energy())
+
+            maxf = np.linalg.norm(atoms.get_forces(apply_constraint=True),
+                                  axis=1).max()
+            forces = _('Max force: ')
+            forces += '{:.3f}'.format(maxf)
+
+            try:
+                magmom = atoms.get_magnetic_moments().sum()
+                mag = _('Magmom: ')
+                mag += '{:.2f}'.format(magmom)
+            except PropertyNotImplementedError:
+                mag = ''
+            txt += calc_format % '\n'.join([energy, forces, mag])
+
     return txt
