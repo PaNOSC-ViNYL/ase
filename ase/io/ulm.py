682c6374f43ffa88fb36f93f76962227ebe324d8
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2017-12-05 16:05:55 +0100

    Check for empty file

--- ase/io/ulm.py
+++ ase/io/ulm.py
@@ -134,13 +134,15 @@ class Writer:
                 data = {}
             else:
                 data = {'_little_endian': False}
-            file_as_string = isinstance(fd, basestring)
-            if mode == 'w' or (file_as_string and not os.path.isfile(fd)):
+            fd_is_string = isinstance(fd, basestring)
+            if mode == 'w' or (fd_is_string and
+                               not (os.path.isfile(fd) and
+                                    os.path.getsize(fd) > 0)):
                 self.nitems = 0
                 self.pos0 = 48
                 self.offsets = np.array([-1], np.int64)
 
-                if file_as_string:
+                if fd_is_string:
                     fd = builtins.open(fd, 'wb')
 
                 # File format identifier and other stuff:
@@ -151,7 +153,7 @@ class Writer:
                                a.tostring() +
                                self.offsets.tostring())
             else:
-                if file_as_string:
+                if fd_is_string:
                     fd = builtins.open(fd, 'r+b')
 
                 version, self.nitems, self.pos0, offsets = read_header(fd)[1:]
