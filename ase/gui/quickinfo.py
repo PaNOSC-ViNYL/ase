071bcab333f2a7ccdc15172db5ee34e8622aeec7
Ask Hjorth Larsen <asklarsen@gmail.com>
2017-12-15 17:17:59 +0100

    Always write units in quick info.  Redo the string formatting, so the strings become better for translators.  Disable the now-conflicting Ctrl+I shortcut for 'select immobile atoms' - it seems slightly too specialized to warrant a hotkey.

--- ase/gui/quickinfo.py
+++ ase/gui/quickinfo.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8
 "Module for displaying information about the system."
 
 from __future__ import unicode_literals
@@ -6,68 +7,57 @@ import numpy as np
 from ase.gui.i18n import _
 import warnings
 
-singleimage = _('Single image loaded.')
-multiimage = _('Image %d loaded (0 - %d).')
-ucconst = _('Unit cell is fixed.')
-ucvaries = _('Unit cell varies.')
 
-format = _("""\
-%s
-
-%s
-Number of atoms: %d.
-
-Unit cell:
-  %8.3f  %8.3f  %8.3f
-  %8.3f  %8.3f  %8.3f
-  %8.3f  %8.3f  %8.3f
-
-%s
-%s
-""")
-
-calc_format = """
-
-%s
+ucellformat = """\
+  {:8.3f}  {:8.3f}  {:8.3f}
+  {:8.3f}  {:8.3f}  {:8.3f}
+  {:8.3f}  {:8.3f}  {:8.3f}
 """
+
 
 def info(gui):
     images = gui.images
     nimg = len(images)
     atoms = gui.atoms
-    natoms = len(atoms)
+
+    tokens = []
+    def add(token=''):
+        tokens.append(token)
 
     if len(atoms) < 1:
-        txt = _('This frame has no atoms.')
+        add(_('This frame has no atoms.'))
     else:
         img = gui.frame
 
-        uc = atoms.cell
-        if nimg > 1:
-            equal = True
-            for i in range(nimg):
-                equal = equal and (uc == images[i].cell).all()
-            if equal:
-                uctxt = ucconst
-            else:
-                uctxt = ucvaries
-        else:
-            uctxt = ''
         if nimg == 1:
-            imgtxt = singleimage
+            add(_('Single image loaded.'))
         else:
-            imgtxt = multiimage % (img, nimg - 1)
+            add(_('Image {} loaded (0–{}).').format(img, nimg - 1))
+        add()
+        add(_('Number of atoms: {}').format(len(atoms)))
 
-        periodic = [[_('no'), _('yes')][periodic]
-                    for periodic in atoms.pbc]
-
-        # TRANSLATORS: This has the form Periodic: no, no, yes
+        # We need to write Å³ further down, so we have no choice but to
+        # use proper subscripts in the chemical formula:
         formula = atoms.get_chemical_formula()
-        pbcstring = _('Periodic: %s, %s, %s') % tuple(periodic)
-        txt = format % ((imgtxt, formula, natoms) + tuple(uc.flat) +
-                        (pbcstring,) + (uctxt,))
+        subscripts = dict(zip('0123456789', '₀₁₂₃₄₅₆₇₈₉'))
+        pretty_formula = ''.join(subscripts.get(c, c) for c in formula)
+        add(pretty_formula)
+
+        add()
+        add(_('Unit cell [Å]:'))
+        add(ucellformat.format(*atoms.cell.ravel()))
+        periodic = [[_('no'), _('yes')][periodic] for periodic in atoms.pbc]
+        # TRANSLATORS: This has the form Periodic: no, no, yes
+        add(_('Periodic: {}, {}, {}').format(*periodic))
+
+        if nimg > 1:
+            if all((atoms.cell == img.cell).all() for img in images):
+                add(_('Unit cell is fixed.'))
+            else:
+                add(_('Unit cell varies.'))
+
         if atoms.number_of_lattice_vectors == 3:
-            txt += _('Volume: ') + '{:8.3f}'.format(atoms.get_volume())
+            add(_('Volume: {:.3f} Å³').format(atoms.get_volume()))
 
         # Print electronic structure information if we have a calculator
         if atoms.calc:
@@ -89,40 +79,32 @@ def info(gui):
                     warnings.warn(errmsg)
                 return quantity
 
-            calc_strs = []
             # SinglePointCalculators are named after the code which
             # produced the result, so this will typically list the
             # name of a code even if they are just cached results.
+            add()
             from ase.calculators.singlepoint import SinglePointCalculator
             if isinstance(calc, SinglePointCalculator):
-                calc_strs.append(_('Calculator: {} (cached)')
-                                 .format(calc.name))
+                add(_('Calculator: {} (cached)').format(calc.name))
             else:
-                calc_strs.append(_('Calculator: {} (attached)'))
+                add(_('Calculator: {} (attached)'.format(calc.name)))
 
             energy = getresult('energy', atoms.get_potential_energy)
             forces = getresult('forces', atoms.get_forces)
             magmoms = getresult('magmoms', atoms.get_magnetic_moments)
 
             if energy is not None:
-                energy_str = _('Energy: ')
-                energy_str += '{:.3f} eV'.format(energy)
-                calc_strs.append(energy_str)
+                energy_str = _('Energy: {:.3f} eV'.format(energy))
+                add(energy_str)
 
             if forces is not None:
                 maxf = np.linalg.norm(forces, axis=1).max()
-                forces_str = _('Max force: ')
-                forces_str += '{:.3f}'.format(maxf)
-                calc_strs.append(forces_str)
+                forces_str = _('Max force: {:.3f} eV/Å').format(maxf)
+                add(forces_str)
 
             if magmoms is not None:
                 magmom = magmoms.sum()
-                mag_str = _('Magmom: ')
-                mag_str += '{:.3f}'.format(magmom)
-                calc_strs.append(mag_str)
+                mag_str = _('Magmom: {:.3f} µ'.format(magmom))
+                add(mag_str)
 
-            # Format into string
-            if calc_strs:
-                txt += calc_format % '\n'.join(calc_strs)
-
-    return txt
+    return '\n'.join(tokens)
