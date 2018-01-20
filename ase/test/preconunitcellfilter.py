c7425c6e68034e176a4432a6769a9d5cba3af740
James Kermode <james.kermode@gmail.com>
2018-01-20 11:14:58 +0000

    MAINT: suppress UserWarning from precon tests by specifying mu and mu_c params

--- ase/test/preconunitcellfilter.py
+++ ase/test/preconunitcellfilter.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from ase.build import bulk
 from ase.calculators.lj import LennardJones
-from ase.optimize.precon import PreconLBFGS
+from ase.optimize.precon import PreconLBFGS, Exp
 from ase.constraints import UnitCellFilter
 
 cu0 = bulk("Cu") * (2, 2, 2)
@@ -13,7 +13,7 @@ cu.set_cell(1.2*cu.get_cell())
 cu.set_calculator(lj)
 
 ucf = UnitCellFilter(cu, constant_volume=True)
-opt = PreconLBFGS(ucf)
+opt = PreconLBFGS(ucf, precon=Exp(mu=1.0, mu_c=1.0))
 opt.run(fmax=1e-3)
 
 assert abs(np.linalg.det(cu.cell)/np.linalg.det(cu0.cell) - 1.2**3) < 1e-3
