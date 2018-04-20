f581125db6fa08050edfc91e31604111f4238ddc
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-04-20 15:34:57 +0200

    Move things to systems.py

--- ase/optimize/test/Cu_bulk.py
+++
@@ -1,4 +0,0 @@
-from ase.lattice.cubic import FaceCenteredCubic
-atoms = FaceCenteredCubic(directions=[[1,-1,0], [1,1,0], [0,0,1]],
-                          size=(3,3,3), symbol='Cu', pbc=(1,1,1))
-atoms.rattle(stdev=0.1,seed=42)
--- ase/optimize/test/H2.py
+++
@@ -1,4 +0,0 @@
-from ase import Atoms
-cell = (5, 5, 5)
-atoms = Atoms('H2', [(0, 0, 0), (0, 0, 1.4)], cell=cell)
-atoms.center()
