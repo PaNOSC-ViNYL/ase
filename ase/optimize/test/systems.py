ba4e6597867223a9467fb8ef645e4c9a40813b7e
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-04-23 15:16:54 +0200

    More vacuum

--- ase/optimize/test/systems.py
+++ ase/optimize/test/systems.py
@@ -104,9 +104,9 @@ zpos = cos(134.3 / 2.0 * pi / 180.0) * 1.197
 xpos = sin(134.3 / 2.0 * pi / 180.0) * 1.19
 co = Atoms('CO', positions=[(-xpos + 1.2, 0, -zpos),
                             (-xpos + 1.2, -1.1, -zpos)])
-slab = fcc111('Au', size=(2, 2, 4), vacuum=5, orthogonal=True)
-slab.center()
+slab = fcc111('Au', size=(2, 2, 4), orthogonal=True)
 add_adsorbate(slab, co, 1.5, 'bridge')
+slab.center(vacuum=6, axis=2)
 slab.set_pbc((True, True, False))
 constraint = FixAtoms(mask=[(a.tag == 4) or (a.tag == 3) or (a.tag == 2)
                             for a in slab])
