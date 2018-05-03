71289a45c9189f2bab8e1b04e8c6d61a84d9062c
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-05-03 20:21:49 +0200

    Simplify things

--- ase/optimize/test/plot.py
+++ ase/optimize/test/plot.py
@@ -1,30 +1,51 @@
 from collections import defaultdict
-
-import matplotlib.pyplot as plt
+from math import inf
 
 import ase.db
 
 
-energies = defaultdict(list)
-formulas = []
-for row in ase.db.connect('results.db').select(sort='sid'):
-    if row.formula not in formulas:
-        formulas.append(row.formula)
-    energies[row.formula].append(row.energy)
-emin = {formula: min(energies[formula]) for formula in energies}
+def analyze(filename, tag='results'):
+    energies = defaultdict(list)
+    formulas = []
+    db = ase.db.connect(filename)
+    for row in db.select(sort='name'):
+        if row.formula not in formulas:
+            formulas.append(row.formula)
+        energies[row.formula].append(row.get('energy', inf))
+    emin = {formula: min(energies[formula]) for formula in energies}
 
-n = defaultdict(list)
-for row in ase.db.connect('results.db').select(sort='sid'):
-    if row.energy - emin[row.formula] < 0.01:
-        nsteps = row.n
-    else:
-        nsteps = float('inf')
-    n[row.optimizer].append(nsteps)
+    data = defaultdict(list)
+    for row in db.select(sort='name'):
+        if row.get('energy', inf) - emin[row.formula] < 0.01:
+            nsteps = row.n if row.n < 100 else 9999
+            t = row.t
+        else:
+            nsteps = 9999
+            t = 9999
+        data[row.optimizer].append((nsteps, t))
 
-print(formulas)
-N = sorted(n.items(), key=lambda x: sum(x[1]))
-for o, n in N:
-    print('{:18}{}'.format(o, ' '.join('{:3}'.format(x) for x in n)))
+    print(formulas)
 
-plt.imshow([n for o, n in N])
-plt.show()
+    D = sorted(data.items(), key=lambda x: sum(y[0] for y in x[1]))
+    with open(tag + '-iterations.csv', 'w') as f:
+        print('optimizer,' + ','.join(formulas), file=f)
+        for o, d in D:
+            print('{:18},{}'
+                  .format(o, ','.join('{:3}'.format(x[0])
+                                      if x[0] < 100 else '   '
+                                      for x in d)),
+                  file=f)
+
+    D = sorted(data.items(), key=lambda x: sum(y[1] for y in x[1]))
+    with open(tag + '-time.csv', 'w') as f:
+        print('optimizer,' + ','.join(formulas), file=f)
+        for o, d in D:
+            print('{:18},{}'
+                  .format(o, ','.join('{:8.3f}'.format(x[1])
+                                      if x[0] < 100 else '        '
+                                      for x in d)),
+                  file=f)
+
+
+if __name__ == '__main__':
+    analyze('results.db')
