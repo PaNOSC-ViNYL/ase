eb65a55b09900194b9b97630c8d21f5944dbb36a
Kirsten Winther <winther@stanford.edu>
2018-05-07 11:58:54 -0700

    numbers and tags should be int

--- ase/db/postgresql.py
+++ ase/db/postgresql.py
@@ -110,21 +110,25 @@ class PostgreSQLDatabase(SQLite3Database):
 def schema_update(sql):
     for a, b in [('REAL', 'DOUBLE PRECISION'),
                  ('INTEGER PRIMARY KEY AUTOINCREMENT',
-                  'SERIAL PRIMARY KEY'),
-                 ('BLOB', 'DOUBLE PRECISION')]:
+                  'SERIAL PRIMARY KEY')]:
         sql = sql.replace(a, b)
 
     arrays_1D = ['numbers', 'initial_magmoms', 'initial_charges', 'masses',
                  'tags', 'momenta', 'stress', 'dipole', 'magmoms', 'charges']
+
     arrays_2D = ['positions', 'cell', 'forces']
 
     txt2jsonb = ['calculator_parameters', 'key_value_pairs', 'data']
 
     for column in arrays_1D:
-        sql = sql.replace('{} DOUBLE PRECISION,'.format(column),
-                          '{} DOUBLE PRECISION[],'.format(column))
+        if column in ['numbers', 'tags']:
+            dtype = 'INTEGER'
+        else:
+            dtype = 'DOUBLE PRECISION'
+        sql = sql.replace('{} BLOB,'.format(column),
+                          '{} {}[],'.format(column, dtype))
     for column in arrays_2D:
-        sql = sql.replace('{} DOUBLE PRECISION,'.format(column),
+        sql = sql.replace('{} BLOB,'.format(column),
                           '{} DOUBLE PRECISION[][],'.format(column))
     for column in txt2jsonb:
         sql = sql.replace('{} TEXT,'.format(column),
