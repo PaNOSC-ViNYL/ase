5b449948ec88cf2817838f631796538d0bd68c56
David Kleiven <davidkleiven446@gmail.com>
2018-05-30 21:41:19 +0200

    Fixed error in tests

--- ase/test/structure_comparator.py
+++ ase/test/structure_comparator.py
@@ -1,8 +1,9 @@
 from ase.structcomp import StructureComparator
-import glob
 import os
 from ase.test import NotAvailable
 from ase.build import bulk
+from ase import Atoms
+from ase.spacegroup import spacegroup, get_spacegroup
 from random import randint
 import numpy as np
 
@@ -16,9 +17,9 @@ When comparing atoms-objects from different subfolders they should return False
 
 
 def get_atoms_with_mixed_elements(crystalstructure="fcc"):
-    atoms = bulk("Al", crystalstructure=crystalstructure, a=4.05)
-    atoms = atoms * (2, 2, 3)
-    symbs = ["Al", "Mg", "Si", "Cu", "Zn"]
+    atoms = bulk("Al", crystalstructure=crystalstructure, a=3.2)
+    atoms = atoms*(2,2,2)
+    symbs = ["Al", "Cu", "Zn"]
     symbols = [symbs[randint(0, len(symbs) - 1)] for _ in range(len(atoms))]
     for i in range(len(atoms)):
         atoms[i].symbol = symbols[i]
@@ -100,7 +101,7 @@ def test_rotations_to_standard(comparator):
     for i in range(20):
         cell = np.random.rand(3, 3) * 4.0 - 4.0
         s1.set_cell(cell)
-        new_cell = comparator.standarize_cell(s1).get_cell().T
+        new_cell = comparator._standarize_cell(s1).get_cell().T
         assert abs(new_cell[1, 0]) < tol
         assert abs(new_cell[2, 0]) < tol
         assert abs(new_cell[2, 1]) < tol
@@ -114,7 +115,7 @@ def test_point_inversion(comparator):
 
 
 def test_mirror_plane(comparator):
-    s1=get_atoms_with_mixed_elements()
+    s1=get_atoms_with_mixed_elements(crystalstructure="hcp")
     s2=s1.copy()
     mat=np.array([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, -1.0]])
     s2.set_positions(mat.dot(s2.get_positions().T).T)
@@ -130,13 +131,13 @@ def test_mirror_plane(comparator):
 
 
 def test_hcp_symmetry_ops(comparator):
-    s1=get_atoms_with_mixed_elements(crystalstructure = "hcp")
+    s1=get_atoms_with_mixed_elements(crystalstructure="hcp")
     s2=s1.copy()
     sg=spacegroup.Spacegroup(194)
     cell=s2.get_cell().T
     inv_cell=np.linalg.inv(cell)
     for op in sg.get_rotations():
-        s1=get_atoms_with_mixed_elements()
+        s1=get_atoms_with_mixed_elements(crystalstructure="hcp")
         s2=s1.copy()
         transformed_op=cell.dot(op).dot(inv_cell)
         s2.set_positions(transformed_op.dot(s1.get_positions().T).T)
@@ -193,10 +194,9 @@ def run_all_tests(comparator):
     test_single_impurity(comparator)
     test_translations(comparator)
     test_rot_60_deg(comparator)
-    est_rot_120_deg(comparator)
+    test_rot_120_deg(comparator)
     test_rotations_to_standard(comparator)
     test_point_inversion(comparator)
-    test_mirror_plane(comparator)
     test_mirror_plane(comparator)
     test_hcp_symmetry_ops(comparator)
     test_fcc_symmetry_ops(comparator)
