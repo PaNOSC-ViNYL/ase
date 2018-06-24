48c5fdfa6c35fcfa3a869047d608221919a65db1
Alexander Sougaard Tygesen <atygesen@hotmail.com>
2018-06-24 03:44:35 +0200

    Add initial sample function

--- ase/dft/pdos.py
+++ ase/dft/pdos.py
@@ -3,15 +3,15 @@ from collections import OrderedDict
 
 
 class PDOStype:
-    def __init__(self, energy, weights,
-                 info=None):
+    def __init__(self, weights,
+                 energy=None, info=None):
         """Basic PDOS derived type"""
         self.energy = energy
         self.weights = weights
 
-
         # Store any other information about this
         # PDOS that the user might want to use later.
+        # From atoms.info
         if info is None:
             self.info = {}
         else:
@@ -19,10 +19,7 @@ class PDOStype:
 
 
 class PDOS:
-    def __init__(self, dos, width=0.1, npts=401):
-
-        self.width = width
-        self.npts = npts
+    def __init__(self, dos=None):
 
         # We maintain the order as well as we can
         self.pdos = OrderedDict()
@@ -34,21 +31,28 @@ class PDOS:
         # Possbility of telling that the energies are on the
         # same scale, so we only need to store it once?
 
-        if isinstance(dos, dict):
-            # assume it's a dict of dicts
-            for key, value in dos.items():
-                # The dict needs to have
-                # "energy" and "weights"
-                en = value.pop('energy')
-                weights = value.pop('weights')
-                self.pdos[key] = PDOStype(en, weights, info=value)
-        else:
-            # Let's assume it's a zip(names, things)
-            # and things is a dict
-            for names, things in dos:
-                en = things.pop('energy')
-                weights = things.pop('weights')
-                self.pdos[names] = PDOStype(en, weights, info=things)
+        # Let's allow the user to set up an empty PDOS object,
+        # and then add the energies and DOS afterwards
+        if dos:
+            if isinstance(dos, dict):
+                # assume it's a dict of dicts
+                for key, value in dos.items():
+                    # The dict needs to have
+                    # "energy" and "weights"
+                    en = value.pop('energy')
+                    weights = value.pop('weights')
+                    self.pdos[key] = PDOStype(weights, energy=en, info=value)
+            else:
+                # Let's assume it's a zip(names, things)
+                # and things is a dict
+                for names, things in dos:
+                    en = things.pop('energy')
+                    weights = things.pop('weights')
+                    self.pdos[names] = PDOStype(weights,
+                                                energy=en, info=things)
+
+    def add(self, name, weights, energy=None, **info):
+        self.pdos[name] = PDOStype(weights, energy=energy, info=info)
 
     def __iter__(self):
         self._it = iter(self.pdos.items())
@@ -57,46 +61,85 @@ class PDOS:
     def __next__(self):
         return next(self._it)
 
-    def delta(self, x, x0):
+    def delta(self, x, x0, width):
         """Return a delta-function centered at 'x0'."""
-        x1 = -((x - x0) / self.width)**2
-        return np.exp(x1) / (np.sqrt(np.pi) * self.width)
+        x1 = -((x - x0) / width)**2
+        return np.exp(x1) / (np.sqrt(np.pi) * width)
 
-    def smear(self, energy, weights):
+    def smear(self, energy, weights, npts=401, width=0.1):
         """Add Gaussian smearing, and map energies and weights on grid
         of length npts. Disabled for 0 width
         """
 
-        if self.width == 0.0:
+        if width == 0.0:
             return energy, weights
         else:
-            dos = np.zeros(self.npts)
-            energies = np.linspace(min(energy), max(energy), self.npts)
+            dos = np.zeros(npts)
+            energies = np.linspace(min(energy), max(energy), npts)
 
             for en, w in zip(energy, weights):
-                dos += w * self.delta(energies, en)
+                dos += w * self.delta(energies, en, width)
             return energies, dos
+
+    def sample(self, npts=401, width=0.1, type='Gauss',
+               window=None, grid=None, sampling={'type': 'raw'}):
+
+        pdos_new = PDOS()
+
+        if window is None:
+            emin, emax = None, None
+        else:
+            emin, emax = window
+        if emin is None:
+            emin = -np.infty
+        if emax is None:
+            emax = np.infty
+
+        for name, pd in self:
+            energy = pd.energy
+
+            idx = (emin <= energy) & (energy <= emax)
+            energy = energy[idx]
+            weights = pd.weights[idx]
+
+            energy_lin, weights = self.smear(energy, weights,
+                                             npts=npts, width=width)
+
+            pdos_new.add(name, weights, energy=energy_lin, info=pd.info)
+
+        return pdos_new
+
+    def plot(self, *plotargs,
+             # We need to grab the init keywords
+             ax=None,
+             emin=None, emax=None,
+             ymin=None, ymax=None, ylabel=None,
+             **plotkwargs):
+
+        pdp = PDOSPlot(self, ax=None,
+                       emin=None, emax=None,
+                       ymin=None, ymax=None, ylabel=None)
+        return pdp.plot(*plotargs, **plotkwargs)
 
 
 class PDOSPlot:
-    def __init__(self, pdos):
+    def __init__(self, pdos, ax=None,
+                 emin=None, emax=None,
+                 ymin=None, ymax=None, ylabel=None):
         self.pdos = pdos
-        self.ax = None
+        self.ax = ax
+        if self.ax is None:
+            self.ax = self.prepare_plot(ax, emin, emax,
+                                        ymin=ymin, ymax=ymax,
+                                        ylabel=ylabel)
 
-    def plot(self, ax=None, emin=None, emax=None, ymin=None, ymax=None,
-             filename=None, show=None, ylabel=None, colors=None,
+    def plot(self, filename=None, show=None, colors=None,
              show_legend=True, loc=None, **plotkwargs):
 
-        if self.ax is None:
-            ax = self.prepare_plot(ax, emin, emax,
-                                   ymin=ymin, ymax=ymax,
-                                   ylabel=ylabel)
+        ax = self.ax
 
-        for name, _pdos in self.pdos.pdos.items():
-            energies = _pdos.energy
-            weights = _pdos.weights
-            en, w = self.pdos.smear(energies, weights)
-            ax.plot(en, w, label=name)
+        for name, _pdos in self.pdos:
+            ax.plot(_pdos.energy, _pdos.weights, label=name)
 
         self.finish_plot(filename, show, show_legend, loc)
 
@@ -115,7 +158,6 @@ class PDOSPlot:
         return ax
 
     def finish_plot(self, filename, show, show_legend, loc):
-        # Put in finalize_plot
         import matplotlib.pyplot as plt
 
         if show_legend:
