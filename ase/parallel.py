90bcea9796d615fb1f348aecdd72c4342e45ec53
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-08-15 09:42:00 +0200

    Quick fix to get GPAW working again

--- ase/parallel.py
+++ ase/parallel.py
@@ -97,6 +97,8 @@ class MPI4PY:
         a[:] = self.comm.bcast(a, root=rank)
 
 
+world = None
+
 # Check for special MPI-enabled Python interpreters:
 if '_gpaw' in sys.builtin_module_names:
     # http://wiki.fysik.dtu.dk/gpaw
@@ -105,7 +107,8 @@ if '_gpaw' in sys.builtin_module_names:
 elif '_gpaw' in sys.modules:
     # Same thing as above but for the module version
     import _gpaw
-    world = _gpaw.Communicator()
+    if hasattr(_gpaw, 'Communicator'):
+        world = _gpaw.Communicator()
 elif '_asap' in sys.builtin_module_names:
     # Modern version of Asap
     # http://wiki.fysik.dtu.dk/asap
@@ -120,7 +123,8 @@ elif 'Scientific_mpi' in sys.modules:
     from Scientific.MPI import world
 elif 'mpi4py' in sys.modules:
     world = MPI4PY()
-else:
+
+if world is None:
     # This is a standard Python interpreter:
     world = DummyMPI()
 
