a51ecfe23c27aa9881689b637c9d559861eb4651
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-09-12 07:32:59 +0200

    Add failing test

--- ase/test/db/db2.py
+++ ase/test/db/db2.py
@@ -96,3 +96,7 @@ for name in ['testase.json', 'testase.db', 'postgresql']:
 
     e = [row.get('energy') for row in c.select(sort='energy')]
     assert len(e) == 5 and abs(e[0] - 1.991) < 0.0005
+
+    # Test NaN and Inf handling:
+    id = c.write(Atoms(), f1=np.nan, f2=np.inf, f3=-np.inf, f4='NaN2')
+    print(c[id].f4)
