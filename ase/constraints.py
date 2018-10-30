cd64862b61826481dc887b8ad7e7a4a7d973bb53
James Kermode <james.kermode@gmail.com>
2018-10-30 14:59:01 +0000

    MAINT: minor improvements to ExpCellFilter
    - Use Cauchy stress for testing for convergence
    - Ensure full search direction does not deviate too much from naive one
    - Update test to use LBFGSLineSearch since this is now stable

--- ase/constraints.py
+++ ase/constraints.py
@@ -1758,6 +1758,7 @@ class ExpCellFilter(UnitCellFilter):
         if (self.mask != 1.0).any():
             virial *= self.mask
 
+        deform_grad_log_force_naive = virial.copy()
         Y = np.zeros((6,6))
         Y[0:3,0:3] = self.deform_grad_log
         Y[3:6,3:6] = self.deform_grad_log
@@ -1768,23 +1769,28 @@ class ExpCellFilter(UnitCellFilter):
             deform_grad_log_force[i1,i2] = ff
             deform_grad_log_force[i2,i1] = ff
 
-        if self.constant_volume:
-            dglf = deform_grad_log_force.trace()
-            np.fill_diagonal(deform_grad_log_force, np.diag(deform_grad_log_force) - dglf / 3.0)
+        # check for reasonable alignment between naive and exact search directions
+        if (np.sum(deform_grad_log_force*deform_grad_log_force_naive) /
+            np.sqrt(np.sum(deform_grad_log_force**2) * np.sum(deform_grad_log_force_naive**2)) > 0.8):
+            deform_grad_log_force = deform_grad_log_force_naive
 
+        # Cauchy stress used for convergence testing
+        convergence_crit_stress = -(virial/volume)
+        if self.constant_volume:
+            # apply constraint to force
+            dglf_trace = deform_grad_log_force.trace()
+            np.fill_diagonal(deform_grad_log_force, np.diag(deform_grad_log_force) - dglf_trace / 3.0)
+            # apply constraint to Cauchy stress used for convergence testing
+            ccs_trace = convergence_crit_stress.trace()
+            np.fill_diagonal(convergence_crit_stress, np.diag(convergence_crit_stress) - ccs_trace / 3.0)
+
+        # pack gradients into vector
         natoms = len(self.atoms)
         forces = np.zeros((natoms + 3, 3))
         forces[:natoms] = atoms_forces
         forces[natoms:] = deform_grad_log_force
 
-        # set for convergence testing purposes
-        conv_crit_stress = -(virial/volume)
-        if self.constant_volume:
-            # cofactor matrix
-            d_det_F = np.linalg.inv(self.deform_grad).T * np.linalg.det(self.deform_grad)
-            d_det_F_hat = d_det_F / np.sqrt(np.sum(d_det_F**2))
-            conv_crit_stress += np.sum(virial/volume * d_det_F_hat) * d_det_F_hat
-        self.stress = full_3x3_to_voigt_6_stress(conv_crit_stress)
+        self.stress = full_3x3_to_voigt_6_stress(convergence_crit_stress)
 
         return forces
 
