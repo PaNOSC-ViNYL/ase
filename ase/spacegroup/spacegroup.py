8958351efaf5eb4f93f140444e90aa6d4408f368
Ask Hjorth Larsen <asklarsen@gmail.com>
2018-10-31 19:50:34 +0100

    call spglib correctly

--- ase/spacegroup/spacegroup.py
+++ ase/spacegroup/spacegroup.py
@@ -812,10 +812,12 @@ def get_spacegroup(atoms, symprec=1e-5):
     except ImportError:
         from pyspglib import spglib  # For versions 1.8.x or before
 
-    sg = spglib.get_spacegroup(atoms.get_cell(),
-                               atoms.get_scaled_positions(),
-                               atoms.get_atomic_numbers(),
+    sg = spglib.get_spacegroup((atoms.get_cell(),
+                                atoms.get_scaled_positions(),
+                                atoms.get_atomic_numbers()),
                                symprec=symprec)
+    if sg is None:
+        raise RuntimeError('Spacegroup not found')
     sg_no = int(sg[sg.find('(') + 1:sg.find(')')])
     return Spacegroup(sg_no)
 
