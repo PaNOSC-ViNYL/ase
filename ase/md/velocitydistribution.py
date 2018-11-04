bb982be3a102cfa2e94b280b0e2935ab9c768b0b
Jakob Schiotz <schiotz@fysik.dtu.dk>
2018-11-04 14:13:55 +0100

    Remove illegal escape sequence causing syntax error on Python 3.7.0.

--- ase/md/velocitydistribution.py
+++ ase/md/velocitydistribution.py
@@ -231,7 +231,7 @@ def PhononHarmonics(atoms,
     force_constants: ndarray of size 3N x 3N
         Force constants for the the structure represented by atoms in eV/Å²
     temp: float
-        Temperature in eV (T \* units.kB)
+        Temperature in eV (T * units.kB)
     rng: Random number generator
         RandomState or other random number generator, e.g., np.random.rand
     quantum: bool
