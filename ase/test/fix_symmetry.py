b5c30a89c773b6545b71bbc453becc29ceef196e
James Kermode <james.kermode@gmail.com>
2018-11-05 12:04:56 +0000

    TST: give up on avoiding code duplication and inline example as test

--- ase/test/fix_symmetry.py
+++ ase/test/fix_symmetry.py
@@ -1,11 +1,61 @@
-import os
+# inserted from ,,/../doc/ase/fix_symmetry_example.py
+
+from __future__ import print_function
 import sys
+import numpy as np
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
-                '../../doc/ase/')))
-import fix_symmetry_example
+from ase.atoms import Atoms
+from ase.calculators.lj import LennardJones
+from ase.spacegroup.symmetrize import FixSymmetry, check
+from ase.optimize import BFGS
+from ase.constraints import UnitCellFilter
 
-assert fix_symmetry_example.d_init6["number"] == 229
-assert fix_symmetry_example.d_init8["number"] == 99
-assert fix_symmetry_example.d_unsym["number"] == 1
-assert fix_symmetry_example.d_sym["number"] == 229
+# setup an fcc Al cell
+a = 2.0/np.sqrt(3.0)
+at_prim = Atoms('Al2', positions=[[0,0,0],[a/2.0, a/2.0, a/2.0]],
+                cell=[[a,0,0],[0,a,0],[0,0,a]], pbc=[True, True, True])
+
+# without symmetrization
+at_unsym = at_prim * [2,2,2]
+at_unsym.positions[0,0] += 1.0e-7 # break symmetry by 1e-7
+at_init = at_unsym.copy()
+
+calc = LennardJones()
+at_unsym.set_calculator(calc)
+
+at_cell = UnitCellFilter(at_unsym)
+
+dyn = BFGS(at_cell)
+print("Energy", at_unsym.get_potential_energy())
+dyn.run(fmax=0.001)
+print("Energy", at_unsym.get_potential_energy())
+
+# with symmetrization
+at_sym = at_prim * [2,2,2]
+at_sym.positions[0,0] += 1.0e-7 # break symmetry by 1e-7
+
+at_sym.set_calculator(LennardJones())
+at_sym.set_constraint(FixSymmetry(at_sym))
+
+at_cell = UnitCellFilter(at_sym)
+
+dyn = BFGS(at_cell)
+print("Energy", at_sym.get_potential_energy())
+dyn.run(fmax=0.001)
+print("Energy", at_sym.get_potential_energy())
+
+print("position difference", np.linalg.norm(at_unsym.get_positions()-at_sym.get_positions()))
+
+print("initial symmetry at 1e-6")
+d_init6 = check(at_init, 1.0e-6)
+print("initial symmetry at 1e-8")
+d_init8 = check(at_init, 1.0e-8)
+print("unsym symmetry")
+d_unsym = check(at_unsym)
+print("sym symmetry")
+d_sym = check(at_sym)
+
+assert d_init6["number"] == 229
+assert d_init8["number"] == 99
+assert d_unsym["number"] == 1
+assert d_sym["number"] == 229
