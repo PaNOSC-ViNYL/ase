da5b8b7235c9f3e448c0ad9c98c9c40833504584
Jakob Schiotz <schiotz@fysik.dtu.dk>
2018-11-21 10:27:06 +0100

    ASE also works with Python 3.7; this should be visible on PyPI.
    The info in setup.py generates the metadata on PyPI.org

--- setup.py
+++ setup.py
@@ -92,4 +92,5 @@ setup(name='ase',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
           'Topic :: Scientific/Engineering :: Physics'])
